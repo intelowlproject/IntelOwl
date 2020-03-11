14adcae05b44e11feef4096f4660eedcf237f5cf
Eshaan Bansal <eshaan7bansal@gmail.com>
2020-03-11 20:42:23 +0530

    Added tests | Hunter.io API integration

--- api_app/tests.py
+++ api_app/tests.py
@@ -9,7 +9,7 @@ from api_app.script_analyzers import general
 from api_app.script_analyzers.file_analyzers import file_info, pe_info, doc_info, pdf_info, vt2_scan, intezer_scan, \
     cuckoo_scan, yara_scan, vt3_scan, strings_info, rtf_info
 from api_app.script_analyzers.observable_analyzers import abuseipdb, shodan, fortiguard, maxmind, greynoise, googlesf, otx, \
-    talos, tor, circl_pssl, circl_pdns, robtex_ip, robtex_fdns, robtex_rdns, vt2_get, ha_get, vt3_get, misp, dnsdb
+    talos, tor, circl_pssl, circl_pdns, robtex_ip, robtex_fdns, robtex_rdns, vt2_get, ha_get, vt3_get, misp, dnsdb, hunter
 
 from api_app import crons
 from api_app.models import Job
@@ -84,7 +84,7 @@ class ApiTests(TestCase):
         analyzers_requested = ["TorProject", "AbuseIPDB", "Shodan", "MaxMindGeoIP", "CIRCLPassiveSSL",
                                "GreyNoiseAlpha", "GoogleSafebrowsing", "Robtex_IP_Query",
                                "Robtex_Reverse_PDNS_Query", "TalosReputation", "OTXQuery",
-                               "VirusTotal_Get_v2_Observable", "HybridAnalysis_Get_Observable"]
+                               "VirusTotal_Get_v2_Observable", "HybridAnalysis_Get_Observable", "Hunter"]
         observable_name = os.environ.get("TEST_IP", "")
         md5 = hashlib.md5(observable_name.encode('utf-8')).hexdigest()
         api_request_result = self.client.send_observable_analysis_request(md5, analyzers_requested,
@@ -165,6 +165,10 @@ class IPAnalyzersTests(TestCase):
 
     def test_shodan(self):
         report = shodan.run("Shodan", self.job_id, self.observable_name, self.observable_classification, {})
+        self.assertEqual(report.get('success', False), True)
+
+    def test_hunter(self):
+        report = hunter.run("Hunter", self.job_id, self.observable_name, self.observable_classification, {})
         self.assertEqual(report.get('success', False), True)
 
     def test_maxmind(self):

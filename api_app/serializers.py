124241a6061cc6a5a991220e2ed6f7570de08eec
Eshaan Bansal <eshaan7bansal@gmail.com>
2020-05-12 23:59:53 +0530

    Added Tag model | custom fields in admin view

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
-from api_app.models import Job
+from api_app.models import Job, Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
 
 
 class JobSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+    tags_id = serializers.PrimaryKeyRelatedField(
+        many=True, write_only=True, queryset=Tag.objects.all()
+    )
+
     class Meta:
         model = Job
-        fields = '__all__'
+        fields = "__all__"
+        extra_kwargs = {"tags": {"required": False}}
+
+    def create(self, validated_data):
+        tags = validated_data.pop("tags_id", None)
+        job = Job.objects.create(**validated_data)
+        if tags:
+            job.tags.set(tags)
+
+        return job

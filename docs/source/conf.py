ba3a8316d13068f52f87462ba6f9a4368e4c8e73
Matteo Lodi <m.lodi@certego.net>
2020-07-02 12:18:26 +0200

    changed version number + upgraded Django version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Matteo Lodi"
 author = "Matteo Lodi"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------

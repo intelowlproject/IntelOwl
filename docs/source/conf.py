e0c10cbb316d218694cbff1f80ce71cf81f1ded4
Eshaan Bansal <eshaan7bansal@gmail.com>
2020-09-20 20:14:45 +0530

    bump version number to v1.7.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Matteo Lodi"
 author = "Matteo Lodi"
 
 # The full version, including alpha/beta/rc tags
-release = "1.6.1"
+release = "1.7.0"
 
 
 # -- General configuration ---------------------------------------------------

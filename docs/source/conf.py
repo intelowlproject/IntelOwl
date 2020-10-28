da757ba13e3f6c8a81917f12b61e0198e0431f8c
Eshaan Bansal <eshaan7bansal@gmail.com>
2020-10-28 23:54:13 +0530

    prep for release: bump intelowl version to v1.8.0 in .env, docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Matteo Lodi"
 author = "Matteo Lodi"
 
 # The full version, including alpha/beta/rc tags
-release = "1.7.1"
+release = "1.8.0"
 
 
 # -- General configuration ---------------------------------------------------

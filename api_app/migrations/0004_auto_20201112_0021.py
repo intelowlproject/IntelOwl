4ebdc4e509dbebc7ff1e23420c54c9d0e9400b6d
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2020-11-12 12:09:21 +0100

    added file_name max_length to 512

--- api_app/migrations/0004_auto_20201112_0021.py
+++ api_app/migrations/0004_auto_20201112_0021.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api_app', '0003_auto_20201020_1406'),
+        ("api_app", "0003_auto_20201020_1406"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='job',
-            name='file_name',
-            field=models.CharField(blank=True, max_length=64),
+            model_name="job",
+            name="file_name",
+            field=models.CharField(blank=True, max_length=512),
         )
     ]

e72f4a738a25442845792fa14c2227c602c4aff3
0ssigeno <simone.berni2@studio.unibo.it>
2021-01-07 10:53:34 +0100

    Updated quark version and fixed rules

--- api_app/script_analyzers/yara_repo_downloader.sh
+++ api_app/script_analyzers/yara_repo_downloader.sh
@@ -60,10 +60,14 @@ rm generic_anomalies.yar general_cloaking.yar thor_inverse_matches.yar yara_mixe
 
 # Download rules for quark-engine analyzer
 cd /opt/deploy
-svn export https://github.com/quark-engine/quark-engine/tags/v20.08/quark/rules quark-rules
-
+git clone https://github.com/quark-engine/quark-rules quark-rules
+# this is done to lock the version since the repo does not have tags.
+cd quark-rules
+git reset --hard b2cca423ed3c0786e6e41b2cb277676ed66d7854
 # chown directories
 chown -R www-data:www-data /opt/deploy/yara /opt/deploy/quark-rules
+
+cd /opt/deploy
 
 # Clone dictionaries for dnstwist analyzer
 svn export https://github.com/elceef/dnstwist/tags/20201022/dictionaries dnstwist-dictionaries

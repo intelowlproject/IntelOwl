426eb4d193d7e46639f504818948833ef6495097
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-02-14 01:16:32 +0530

    chore: small changes

--- api_app/signal_handlers.py
+++ api_app/signal_handlers.py
@@ -1,8 +1,23 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_migrate
 from django.dispatch import receiver
 from django.conf import settings
 from django.contrib.auth.models import Group
 from guardian.shortcuts import assign_perm
+
+
+@receiver(post_migrate)
+def create_DefaultGlobal_group(*args, **kwargs):
+    grp, created = Group.objects.get_or_create(name="DefaultGlobal")
+    if created:
+        # view/add/delete permissions for Job model
+        assign_perm("api_app.view_job", grp)
+        assign_perm("api_app.add_job", grp)
+        assign_perm("api_app.delete_job", grp)
+        # view/add/change permissions for Tag model
+        assign_perm("api_app.view_tag", grp)
+        assign_perm("api_app.add_tag", grp)
+        assign_perm("api_app.change_tag", grp)
+        grp.save()
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -15,15 +30,5 @@ def save_profile(sender, instance, created, **kwargs):
         return
 
     if created:
-        grp, grp_created = Group.objects.get_or_create(name="DefaultGlobal")
-        if grp_created:
-            # view/add/delete permissions for Job model
-            assign_perm("api_app.view_job", grp)
-            assign_perm("api_app.add_job", grp)
-            assign_perm("api_app.delete_job", grp)
-            # view/add/change permissions for Tag model
-            assign_perm("api_app.view_tag", grp)
-            assign_perm("api_app.add_tag", grp)
-            assign_perm("api_app.change_tag", grp)
-            grp.save()
+        grp, _ = Group.objects.get_or_create(name="DefaultGlobal")
         instance.groups.add(grp)

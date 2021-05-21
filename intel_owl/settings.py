9dfd8de6f3a7204e71534eaa2ba85fabb0add8dd
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-05-21 16:11:07 +0530

    Fixes #404, #454: Bump durin to 0.3.0, test_auth, fix bug in Dockerfile_nginx (#455)
    * Fixes #404: remove apk update line in Dockerfile_nginx
    * Fixes #454: durin to 0.3.0, related changes, test_auth

--- intel_owl/settings.py
+++ intel_owl/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
+    # default
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -44,7 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.postgres",
-    "memoize",
+    # DRF
     "rest_framework",
     "durin",
     "guardian",

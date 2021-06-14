57e13c3e24abe6b166a42494d3926c1ff8507b71
Sarthak Khattar <sarthakoct@gmail.com>
2021-06-14 17:51:40 +0530

    registered models in admin interface

--- api_app/analyzers_manager/admin.py
+++ api_app/analyzers_manager/admin.py
@@ -1,3 +1,47 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from guardian.admin import GuardedModelAdmin
+
+from api_app.analyzers_manager.models import Analyzer, AnalyzerReport, Secret
+
+
+class AnalyzerAdminView(GuardedModelAdmin):
+    list_display = (
+        "name",
+        "analyzer_type",
+        "disabled",
+        "description",
+        "python_module",
+        "config",
+    )
+    search_fields = ("name", "analyzer_type", "description", "disabled")
+
+
+class AnalyzerReportAdminView(GuardedModelAdmin):
+    list_display = (
+        "analyzer",
+        "job",
+        "status",
+        "report",
+        "errors",
+        "start_time",
+        "end_time",
+    )
+    search_fields = ("analyzer", "job", "status")
+
+
+class SecretAdminView(GuardedModelAdmin):
+    list_display = (
+        "name",
+        "env_variable_key",
+        "datatype",
+        "required",
+        "default",
+        "description",
+    )
+    search_fields = ("name", "description")
+
+
+admin.site.register(Analyzer, AnalyzerAdminView)
+admin.site.register(AnalyzerReport, AnalyzerReportAdminView)
+admin.site.register(Secret, SecretAdminView)

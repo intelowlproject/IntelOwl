96ac2b13959807fb911bda564db5572482c40626
Shubham Pandey <shubhampcpandey13@gmail.com>
2021-06-22 21:44:57 +0530

    Remove unused cache set

--- api_app/connectors_manager/helpers.py
+++ api_app/connectors_manager/helpers.py
@@ -1,7 +1,6 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
 
-from django.core.cache import cache
 from cache_memoize import cache_memoize
 
 from .serializers import ConnectorConfigSerializer
@@ -10,6 +9,4 @@ from .serializers import ConnectorConfigSerializer
 @cache_memoize(100)
 def get_verified_connector_config():
     success, config = ConnectorConfigSerializer.read_and_verify_config()
-    if success:
-        cache.set("verified_connectors_config", config)
     return config

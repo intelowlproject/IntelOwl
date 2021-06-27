9033837ecbff9e89ec053c94cc7426e0a07169d3
Sarthak Khattar <sarthakoct@gmail.com>
2021-06-28 02:22:08 +0530

    import fix in darksearch analyzer

--- api_app/analyzers_manager/observable_analyzers/darksearch.py
+++ api_app/analyzers_manager/observable_analyzers/darksearch.py
@@ -1,7 +1,7 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
 
-from api_app.script_analyzers.classes import ObservableAnalyzer
+from api_app.analyzers_manager.classes import ObservableAnalyzer
 
 
 class DarkSearchQuery(ObservableAnalyzer):

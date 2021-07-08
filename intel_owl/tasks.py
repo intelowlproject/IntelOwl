bd5d484c61b7b7544aca36712c57679c93ab38a5
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-07-08 16:02:18 +0530

    intelowl.tasks: use controller modules, refactor

--- intel_owl/tasks.py
+++ intel_owl/tasks.py
@@ -6,14 +6,15 @@ import importlib
 from celery import shared_task
 from guardian import utils
 
+from django.utils.module_loading import import_string
+
 from intel_owl.celery import app
 from api_app import crons
 from api_app.models import Job
 from api_app.analyzers_manager.classes import BaseAnalyzerMixin
-from api_app.analyzers_manager.utils import set_report_and_cleanup, set_failed_analyzer
-from api_app.connectors_manager.classes import BaseConnectorMixin
-from api_app.connectors_manager.utils import set_failed_connector
-from api_app.connectors_manager.general import start_connectors
+from api_app.analyzers_manager import controller as analyzers_controller
+from api_app.connectors_manager.classes import Connector
+from api_app.connectors_manager import controller as connectors_controller
 
 from api_app.analyzers_manager.file_analyzers import yara_scan
 from api_app.analyzers_manager.observable_analyzers import (
@@ -59,34 +60,42 @@ def yara_updater():
     yara_scan.YaraScan.yara_update_repos()
 
 
-@app.task(name="run_analyzer", soft_time_limit=500)
-def run_analyzer(cls_path, *args):
-    try:
-        # FIXME: we can replace this with `django.utils.module_loading`
-        # ref to django docs: https://cutt.ly/HzF2rcO
-        path_parts = cls_path.split(".")
-        typ = path_parts[0]
-        modname = ".".join(path_parts[1:-1])
-        clsname = path_parts[-1]
-        modpath = f"api_app.analyzers_manager.{typ}.{modname}"
-        mod = importlib.import_module(modpath)
-        if not mod:
-            raise Exception(f"Module: {cls_path} couldn't be imported")
+@app.task(name="start_analyzers", soft_time_limit=100)
+def start_analyzers(
+    job_id: int,
+    analyzers_to_execute: list,
+    runtime_configuration: dict,
+    celery_kwargs: dict = {},
+):
+    analyzers_controller.start_analyzers(
+        job_id, analyzers_to_execute, runtime_configuration, celery_kwargs
+    )
 
-        cls: BaseAnalyzerMixin = getattr(mod, clsname)
-        if not cls:
+
+@app.task(name="run_analyzer", soft_time_limit=500)
+def run_analyzer(job_id: int, config_dict: dict, **kwargs):
+    try:
+        cls_path: str = analyzers_controller.build_import_path(
+            config_dict["python_module"],
+            observable_analyzer=(
+                config_dict["type"] == "observable"
+                or config_dict["type"] == "file"
+                and config_dict["run_hash"]
+            ),
+        )
+        try:
+            klass: Connector = import_string(cls_path)
+        except ImportError:
             raise Exception(f"Class: {cls_path} couldn't be imported")
 
-        instance: BaseAnalyzerMixin = cls(*args)
-        instance.start()
-        set_report_and_cleanup(
-            instance.analyzer_name,
-            instance.job_id,
+        instance: BaseAnalyzerMixin = klass(
+            config_dict=config_dict, job_id=job_id, **kwargs
         )
+        instance.start()
+        analyzers_controller.job_cleanup(instance.job_id)
 
         # fire connectors when job finishes with success
-        job_object = Job.object_by_job_id(instance.job_id)
-        if job_object.status == "reported_without_fails":
+        if instance._job.status == "reported_without_fails":
             app.send_task(
                 "on_job_success",
                 args=[
@@ -94,34 +103,31 @@ def run_analyzer(cls_path, *args):
                 ],
             )
     except Exception as e:
-        set_failed_analyzer(args[0], args[1], str(e))
+        analyzers_controller.set_failed_analyzer(job_id, config_dict["name"], str(e))
 
 
 @app.task(name="on_job_success", soft_time_limit=500)
-def on_job_success(_job_id):
-    start_connectors(_job_id)
+def on_job_success(job_id: int):
+    # run all connectors
+    connectors_task_id_map = connectors_controller.start_connectors(job_id)
+    # update connectors_to_execute field
+    job = Job.objects.only("id", "connectors_to_execute").get(pk=job_id)
+    job.connectors_to_execute = list(connectors_task_id_map.keys())
+    job.save(update_fields=["connectors_to_execute"])
 
 
 @app.task(name="run_connector", soft_time_limit=500)
-def run_connector(cls_path, *args):
+def run_connector(job_id: int, config_dict: dict, **kwargs):
     try:
-        # FIXME: we can replace this with `django.utils.module_loading`
-        # ref to django docs: https://cutt.ly/HzF2rcO
-        path_parts = cls_path.split(".")
-        typ = path_parts[0]
-        modname = ".".join(path_parts[1:-1])
-        clsname = path_parts[-1]
-        modpath = f"api_app.connectors_manager.{typ}.{modname}"
-        mod = importlib.import_module(modpath)
-        if not mod:
-            raise Exception(f"Module: {cls_path} couldn't be imported")
-
-        cls: BaseConnectorMixin = getattr(mod, clsname)
-        if not cls:
+        cls_path: str = connectors_controller.build_import_path(
+            config_dict["python_module"]
+        )
+        try:
+            klass: Connector = import_string(cls_path)
+        except ImportError:
             raise Exception(f"Class: {cls_path} couldn't be imported")
 
-        instance: BaseConnectorMixin = cls(*args)
+        instance: Connector = klass(config_dict=config_dict, job_id=job_id, **kwargs)
         instance.start()
     except Exception as e:
-        set_failed_connector(args[0], args[1], str(e))
-        pass
+        connectors_controller.set_failed_connector(job_id, config_dict["name"], str(e))

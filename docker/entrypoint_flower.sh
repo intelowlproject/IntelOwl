0cd172929422c132484970c815deb8729329403b
0ssigeno <simone.berni2@studio.unibo.it>
2021-07-08 17:04:33 +0200

    Flower + fixes celery

---
+++ docker/entrypoint_flower.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+until cd /opt/deploy/intel_owl
+do
+    echo "Waiting for server volume..."
+done
+
+USER="${FLOWER_USER:=flower}"
+PASSWORD="${FLOWER_PWD:=flower}"
+echo "------------------------------"
+env
+echo "DEBUG:  ${DEBUG}"
+echo "DJANGO_TEST_SERVER: ${DJANGO_TEST_SERVER}"
+echo "BROKER: ${CELERY_BROKER_URL}"
+echo "------------------------------"
+
+CMD="/usr/local/bin/celery  -A intel_owl.celery --broker $CELERY_BROKER_URL flower --broker_api=http://guest:guest@rabbitmq:15672/api/"
+
+if [[ $DEBUG == "True" ]] && [[ $DJANGO_TEST_SERVER == "True" ]];
+then
+    ${CMD} --debug
+else
+  if [[ ! -f ".htpasswd" ]]; then
+    htpasswd -cb .htpasswd ${USER} ${PASSWORD}
+  fi
+    ${CMD}
+fi

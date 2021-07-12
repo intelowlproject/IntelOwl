9a02295df037638600510f6992427c2160cc51dc
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-07-12 18:58:56 +0530

    [fixes] analyzer: darksearch

--- api_app/analyzers_manager/observable_analyzers/darksearch.py
+++ api_app/analyzers_manager/observable_analyzers/darksearch.py
@@ -1,12 +1,22 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
 
+from tests.mock_utils import patch, if_mock
+
 from api_app.analyzers_manager.classes import ObservableAnalyzer
 
 
+@if_mock(
+    [
+        patch(
+            "darksearch.Client.search",
+            side_effect=lambda *args, **kwargs: [
+                {"total": 1, "last_page": 0, "data": ["test"]}
+            ],
+        )
+    ]
+)
 class DarkSearchQuery(ObservableAnalyzer):
-    name: str = "DarkSearchQuery"
-
     def set_params(self, params):
         self.num_pages = int(params.get("pages", 5))
         self.proxies = params.get("proxies", None)
@@ -16,13 +26,13 @@ class DarkSearchQuery(ObservableAnalyzer):
 
         c = Client(proxies=self.proxies)
         responses = c.search(self.observable_name, pages=self.num_pages)
-        report = {
+        result = {
             "total": responses[0]["total"],
             "total_pages": responses[0]["last_page"],
             "requested_pages": self.num_pages,
             "data": [],
         }
         for resp in responses:
-            report["data"].extend(resp["data"])
+            result["data"].extend(resp["data"])
 
-        return report
+        return result

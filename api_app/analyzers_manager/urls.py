3242bfa8458e1e85e89597f69d84dca522235146
Shubham Pandey <shubhampcpandey13@gmail.com>
2021-07-26 00:04:39 +0530

    Add plugin viewset, kill running plugin

--- api_app/analyzers_manager/urls.py
+++ api_app/analyzers_manager/urls.py
@@ -1,10 +1,22 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
 
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from .views import AnalyzerListAPI
+from .views import AnalyzerActionViewSet, AnalyzerListAPI
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(
+    r"job/(?P<job_id>\d+)/analyzer/(?P<name>\w+)",
+    AnalyzerActionViewSet,
+    basename="analyzer",
+)
 
 urlpatterns = [
     path("get_analyzer_configs", AnalyzerListAPI.as_view()),
+    # Viewsets
+    path(r"", include(router.urls)),
 ]
--- api_app/core/models.py
+++ api_app/core/models.py
@@ -13,6 +13,7 @@ class Statuses(Enum):
     PENDING = 1
     RUNNING = 2
     SUCCESS = 3
+    KILLED = 4
 
 
 class AbstractReport(models.Model):

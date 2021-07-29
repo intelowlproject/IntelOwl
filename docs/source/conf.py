111a13c6b90404d0474924742bd9298f2f292623
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-07-29 16:25:55 +0530

    version: bump to v2.5.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ copyright = "2020, Matteo Lodi"
 author = "Matteo Lodi"
 
 # The full version, including alpha/beta/rc tags
-release = "v2.4.2"
+release = "v2.5.0"
 
 
 # -- General configuration ---------------------------------------------------

4efda5b10ea7cb5e96d6d386f97d1b31848724ea
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-08-09 20:33:19 +0530

    VTv3: fix python_module pointer

--- api_app/analyzers_manager/file_analyzers/vt/vt3_scan.py
+++ api_app/analyzers_manager/file_analyzers/vt/vt3_scan.py
@@ -3,7 +3,9 @@
 
 
 from api_app.analyzers_manager.classes import FileAnalyzer
-from ..observable_analyzers.vt.vt3_base import VirusTotalv3AnalyzerMixin
+from api_app.analyzers_manager.observable_analyzers.vt.vt3_base import (
+    VirusTotalv3AnalyzerMixin,
+)
 
 from tests.mock_utils import patch, if_mock_connections, MockResponse
 

1618688f952bb5796683647ced224eac57ab4cf8
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-08-31 20:36:15 +0530

    fixes and adjusts

--- intel_owl/settings.py
+++ intel_owl/settings.py
@@ -92,6 +92,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.staticfiles",
     "django.contrib.postgres",
     # celery, elasticsearch
     "django_celery_results",
@@ -250,6 +251,14 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static_intel/"),)
 
 
 INFO_OR_DEBUG_LEVEL = "DEBUG" if DEBUG else "INFO"

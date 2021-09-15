dead47e6770e8e3fae9dd62b47cb83d729c60487
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-09-15 20:32:15 +0530

    release: v3.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ copyright = "2020, Matteo Lodi"
 author = "Matteo Lodi"
 
 # The full version, including alpha/beta/rc tags
-release = "v3.0.0"
+release = "v3.0.1"
 
 
 # -- General configuration ---------------------------------------------------
--- intel_owl/settings.py
+++ intel_owl/settings.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
 # DRF Spectacular
 SPECTACULAR_SETTINGS = {
     "TITLE": "IntelOwl API specification",
-    "VERSION": "2.5.0",
+    "VERSION": "3.0.1",
 }
 
 # Django-Rest-Durin

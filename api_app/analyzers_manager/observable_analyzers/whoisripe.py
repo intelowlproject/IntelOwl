bfd5ce8fecdf9afa112d6bae48e6fb0b3a2b357b
0x0elliot <wolfwithahat@protonmail.com>
2021-11-14 20:19:07 +0530

    Added WHOIS look up analyser using RIPE API.

---
+++ api_app/analyzers_manager/observable_analyzers/whoisripe.py
@@ -0,0 +1,35 @@
+# This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
+# See the file 'LICENSE' for copying permission.
+
+import requests
+
+
+from api_app.analyzers_manager import classes
+
+from tests.mock_utils import if_mock_connections, patch, MockResponse
+
+class Whoisripeapi(classes.ObservableAnalyzer):
+    url: str = "https://rest.db.ripe.net/search.json"
+
+    def run(self):
+        if self.observable_classification == self.ObservableTypes.IP:
+            params = {
+                "query-string" : self.observable_name
+            }
+
+            response = requests.get(self.url, params = params)
+            response.raise_for_status()
+
+            return response.json()
+        
+    @classmethod
+    def _monkeypatch(cls):
+        patches = [
+            if_mock_connections(
+                patch(
+                    "requests.get",
+                    return_value=MockResponse({}, 200),
+                ),
+            )
+        ]
+        return super()._monkeypatch(patches=patches)    

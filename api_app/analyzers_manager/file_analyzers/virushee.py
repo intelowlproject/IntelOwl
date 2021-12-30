f370d098280e8e3167a54d3a7d0cb12e08e7bcd2
uzair-ali10 <the.uzairali10@gmail.com>
2021-12-30 22:19:51 +0530

    Force scan and Refactor

--- api_app/analyzers_manager/file_analyzers/virushee.py
+++ api_app/analyzers_manager/file_analyzers/virushee.py
@@ -12,7 +12,6 @@ from tests.mock_utils import MockResponse, if_mock_connections, patch
 
 logger = logging.getLogger(__name__)
 
-
 class Virushee(FileAnalyzer):
     """Analyze a file against Virushee API"""
 
@@ -21,44 +20,44 @@ class Virushee(FileAnalyzer):
         self.max_tries = 30
         self.poll_distance = 10
         self.request_url = "https://api.virushee.com/"
+        self.to_force_scan = params.get("force_scan", False)
 
     def run(self):
         binary = self.read_file_bytes()
         if not binary:
             raise AnalyzerRunException("File is empty")
+        logger.info(f"FORCEE SCAN VAR: {self.to_force_scan}")
+        if self.to_force_scan:
+            task_id = self.__upload_file(binary)
+            result = self.__poll_status(task_id)
+            return result
         hash_exist = self.is_hash_avaliable()
+        if hash_exist:
+            return hash_exist
         task_id = self.__upload_file(binary)
         result = self.__poll_status(task_id)
         return result
 
-    def is_hash_avaliable(self) -> bool:
-        api_endpoint = f"file/hash/{self.md5}"
-        api_url = self.request_url + api_endpoint
+    def is_hash_avaliable(self):
+        logger.info(f"CECKING IF HASH AVB")
+        api_url = f"{self.request_url}file/hash/{self.md5}"
         response = self.session.get(api_url)
         if response.status_code == 200:
-            return True
+            json_response = response.json()
+            return json_response
         return False
 
     def __upload_file(self, binary) -> str:
+        logger.info(f"UPLOADING FILE FOR ANALYSIS")
         name_to_send = self.filename if self.filename else self.md5
         files = {"file": (name_to_send, binary)}
         upload_url = self.request_url + "file/upload"
-        response = self.session.post(upload_url, files=files)
-        if response.status_code == 422:
-            raise AnalyzerRunException(
-                "Virushee API returned an error: " + response.text
-            )
-        elif response.status_code == 200:
-            logger.info("HASH FOUND")
-            raise AnalyzerRunException(
-                "Virushee API returned an error: " + str(response.status_code)
-            )
-        elif response.status_code == 201:
-            return response.json()["task"]
-        else:
-            raise AnalyzerRunException(
-                "Virushee API returned an error: " + str(response.status_code)
-            )
+        try:
+            response = self.session.post(upload_url, files=files)
+            response.raise_for_status()
+        except requests.RequestException as error:
+            raise AnalyzerRunException(error)
+        return response.json()["task"]
 
     def __poll_status(self, task_id: str) -> dict:
         for chance in range(self.max_tries):

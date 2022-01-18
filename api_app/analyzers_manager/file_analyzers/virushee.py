8cc590472da784b978df24307bf268b0ea1b5572
Eshaan Bansal <eshaan7bansal@gmail.com>
2022-01-18 14:47:35 +0530

    fixups

--- api_app/analyzers_manager/file_analyzers/virushee.py
+++ api_app/analyzers_manager/file_analyzers/virushee.py
@@ -14,68 +14,75 @@ from tests.mock_utils import MockResponse, if_mock_connections, patch
 logger = logging.getLogger(__name__)
 
 
-class Virushee(FileAnalyzer):
+class VirusheeFileUpload(FileAnalyzer):
     """Analyze a file against Virushee API"""
 
+    max_tries = 30
+    poll_distance = 10
+    base_url = "https://api.virushee.com"
+
     def set_params(self, params):
-        self.session = requests.Session()
+        self.__to_force_scan = params.get("force_scan", False)
+        self.__session = requests.Session()
         api_key = self._secrets["api_key_name"]
         if not api_key:
-            logger.info(
-                f"{self.__repr__()}, (md5: {self.md5}) -> Continuing w/o API key.."
-            )
+            logger.info(f"{self.__repr__()} -> Continuing w/o API key..")
         else:
-            self.session.headers["Authorization"] = f"Bearer {api_key}"
-        self.max_tries = 30
-        self.poll_distance = 10
-        self.request_url = "https://api.virushee.com/"
-        self.to_force_scan = params.get("force_scan", False)
+            self.__session.headers["X-API-Key"] = api_key
 
     def run(self):
         binary = self.read_file_bytes()
         if not binary:
             raise AnalyzerRunException("File is empty")
-        if not self.to_force_scan:
+        if not self.__to_force_scan:
             hash_result = self.__check_report_for_hash()
             if hash_result:
                 return hash_result
         task_id = self.__upload_file(binary)
-        result = self.__poll_status(task_id)
+        result = self.__poll_status_and_result(task_id)
         return result
 
     def __check_report_for_hash(self) -> Optional[dict]:
-        api_url = f"{self.request_url}file/hash/{self.md5}"
+        response_json = None
         try:
-            response = requests.get(api_url)
+            response = self.__session.get(f"{self.base_url}/file/hash/{self.md5}")
+            if response.status_code == 404:  # hash not found in db
+                return response_json
             response.raise_for_status()
-        except requests.RequestException:
-            return None
+            response_json = response.json()
+        except requests.RequestException as exc:
+            raise AnalyzerRunException(str(exc))
 
-    def __upload_file(self, binary) -> str:
+        return response_json
+
+    def __upload_file(self, binary: bytes) -> str:
         name_to_send = self.filename if self.filename else self.md5
         files = {"file": (name_to_send, binary)}
-        upload_url = self.request_url + "file/upload"
         try:
-            response = self.session.post(upload_url, files=files)
+            response = self.__session.post(f"{self.base_url}/file/upload", files=files)
             response.raise_for_status()
-        except requests.RequestException as error:
-            raise AnalyzerRunException(error)
+        except requests.RequestException as exc:
+            raise AnalyzerRunException(str(exc))
         return response.json()["task"]
 
-    def __poll_status(self, task_id: str) -> dict:
-        request_url = f"{self.request_url}file/task/{task_id}"
-        chance = 0
-        while True:
-            logger.info(f"Polling #try{chance+1}")
-            time.sleep(self.poll_distance)
+    def __poll_status_and_result(self, task_id: str) -> dict:
+        response_json = None
+        url = f"{self.base_url}/file/task/{task_id}"
+        for chance in range(self.max_tries):
+            logger.info(f"Polling try#{chance+1}")
             try:
-                response = self.session.get(request_url)
+                response = self.__session.get(url)
                 response.raise_for_status()
             except requests.RequestException as exc:
-                raise AnalyzerRunException(exc)
+                raise AnalyzerRunException(str(exc))
             if response.status_code == 202:
+                time.sleep(self.poll_distance)
                 continue
-            return response.json()
+            elif response.status_code == 200:
+                response_json = response.json()
+                break
+
+        return response_json
 
     @classmethod
     def _monkeypatch(cls):
@@ -83,7 +90,7 @@ class Virushee(FileAnalyzer):
             if_mock_connections(
                 patch(
                     "requests.Session.get",
-                    return_value=MockResponse({"message": "hash_found"}, 200),
+                    return_value=MockResponse({"message": "hash_not_found"}, 404),
                 ),
                 patch(
                     "requests.Session.post",
@@ -97,9 +104,7 @@ class Virushee(FileAnalyzer):
                 ),
                 patch(
                     "requests.Session.get",
-                    return_value=MockResponse(
-                        {"message": "invalid_analysis_task"}, 404
-                    ),
+                    return_value=MockResponse({"result": "test"}, 200),
                 ),
             )
         ]

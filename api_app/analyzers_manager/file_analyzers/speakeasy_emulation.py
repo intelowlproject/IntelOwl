05c403eb7ebb5d8428d9eb10f6359e476820e6ed
Lionel <lionel.prat9@gmail.com>
2022-02-23 12:23:00 +0100

    Add shellcode option to floss, speakeasy and capa (#902)
    * Add shellcode option to floss, speakeasy and capa
    
    * fix black lint
    
    * Update Usage.md
    
    Co-authored-by: mlodic <30625432+mlodic@users.noreply.github.com>

--- api_app/analyzers_manager/file_analyzers/speakeasy_emulation.py
+++ api_app/analyzers_manager/file_analyzers/speakeasy_emulation.py
@@ -4,6 +4,7 @@
 import logging
 
 import speakeasy
+import speakeasy.winenv.arch as e_arch
 
 from api_app.analyzers_manager.classes import FileAnalyzer
 
@@ -11,11 +12,23 @@ logger = logging.getLogger(__name__)
 
 
 class SpeakEasy(FileAnalyzer):
+    def set_params(self, params):
+        self.raw_offset = params.get("raw_offset", 0x0)
+        self.arch = params.get("arch", "x64")
+        self.shellcode = params.get("shellcode", False)
+
     def run(self):
         results = {}
         s = speakeasy.Speakeasy()
-        m = s.load_module(self.filepath)
-        s.run_module(m)
+        if self.shellcode:
+            arch = e_arch.ARCH_AMD64
+            if self.arch == "x86":
+                arch = e_arch.ARCH_X86
+            sc_addr = s.load_shellcode(self.filepath, arch)
+            s.run_shellcode(sc_addr, offset=self.raw_offset or 0)
+        else:
+            m = s.load_module(self.filepath)
+            s.run_module(m)
         results = s.get_report()
 
         return results

4c239c1e5ad16189142ae239232c4433a7b2190f
Ibrahim Akgul <loginit@gmail.com>
2022-04-29 03:46:41 +0300

    Update docguard_get.py

--- api_app/analyzers_manager/observable_analyzers/docguard_get.py
+++ api_app/analyzers_manager/observable_analyzers/docguard_get.py
@@ -2,12 +2,10 @@
 # See the file 'LICENSE' for copying permission.
 
 import logging
-import time
-
 import requests
 
 from api_app.analyzers_manager import classes
-from api_app.exceptions import AnalyzerConfigurationException, AnalyzerRunException
+from api_app.exceptions import AnalyzerRunException
 from tests.mock_utils import MockResponse, if_mock_connections, patch
 
 logger = logging.getLogger(__name__)

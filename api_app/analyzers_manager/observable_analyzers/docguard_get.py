3057e8982df4aea03b71b5ee737c140bfcb495f7
Ibrahim Akgul <loginit@gmail.com>
2022-04-29 04:33:14 +0300

    Update docguard_get.py

--- api_app/analyzers_manager/observable_analyzers/docguard_get.py
+++ api_app/analyzers_manager/observable_analyzers/docguard_get.py
@@ -19,7 +19,7 @@ class DocGuard_Hash(classes.ObservableAnalyzer):
     @property
     def hash_type(self):
         hash_lengths = {32: "md5", 64: "sha256"}
-        hash_type = hash_lengths.get(len(self.observable_name), None)
+        hash_type = hash_lengths.get(len(self.observable_name))
         if not hash_type:
             raise AnalyzerRunException(
                 f"Given Hash: '{hash}' is not supported."

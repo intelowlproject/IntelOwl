cab43129b2713020496388dea772fc9e0805d479
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2022-06-29 18:45:50 +0200

    test weak

--- tests/api_app/test_api.py
+++ tests/api_app/test_api.py
@@ -112,7 +112,7 @@ class ApiViewTests(TestCase):
     def test_analyze_file__pcap(self):
         # with noone, only the PCAP analyzers should be executed
         analyzers_requested = []
-        file_name = "example.exe"
+        file_name = "example.pcap"
         uploaded_file, md5 = self.__get_test_file(file_name)
         file_mimetype = "application/vnd.tcpdump.pcap"
         data = {

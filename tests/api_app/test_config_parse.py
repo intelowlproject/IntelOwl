fddc13ce8b4a033099a6099b8eee25276fdb5057
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-07-27 14:49:01 +0530

    minor

--- tests/api_app/test_config_parse.py
+++ tests/api_app/test_config_parse.py
@@ -1,7 +1,5 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
-import random
-from hashlib import md5
 from unittest.mock import Mock, patch
 
 from django.test import TestCase
@@ -10,9 +8,11 @@ from api_app.core.serializers import AbstractConfigSerializer
 
 
 class ConfigParseTests(TestCase):
-    @patch("api_app.core.serializers.AbstractConfigSerializer._md5_config_file")
+    @patch(
+        "api_app.core.serializers.AbstractConfigSerializer._md5_config_file", new=Mock()
+    )
     @patch("api_app.core.serializers.AbstractConfigSerializer._read_config")
-    def test_extends(self, mock_read_config: Mock, mock_md5_config_file: Mock):
+    def test_extends(self, mock_read_config: Mock):
         sample_analyzer_config = {
             "Shodan_Honeyscore": {
                 "type": "observable",
@@ -52,23 +52,60 @@ class ConfigParseTests(TestCase):
         }
 
         pre_parsed_sample_config = {
-            "Shodan_Honeyscore": sample_analyzer_config["Shodan_Honeyscore"],
+            "Shodan_Honeyscore": {
+                "type": "observable",
+                "python_module": "shodan.Shodan",
+                "description": "scan an IP against Shodan Honeyscore API",
+                "disabled": False,
+                "external_service": True,
+                "leaks_info": False,
+                "observable_supported": ["ip"],
+                "config": {"soft_time_limit": 30, "queue": "default"},
+                "secrets": {
+                    "api_key_name": {
+                        "env_var_key": "SHODAN_KEY",
+                        "description": "",
+                        "required": True,
+                    }
+                },
+                "params": {
+                    "shodan_analysis": {
+                        "value": "honeyscore",
+                        "type": "str",
+                        "description": "",
+                    }
+                },
+            },
             "Shodan_Search": {
-                **sample_analyzer_config["Shodan_Honeyscore"],
-                **sample_analyzer_config["Shodan_Search"],
+                "type": "observable",
+                "python_module": "shodan.Shodan",
+                "disabled": False,
+                "external_service": True,
+                "leaks_info": False,
+                "observable_supported": ["ip"],
+                "config": {"soft_time_limit": 30, "queue": "default"},
+                "secrets": {
+                    "api_key_name": {
+                        "env_var_key": "SHODAN_KEY",
+                        "description": "",
+                        "required": True,
+                    }
+                },
+                "description": "scan an IP against Shodan Search API",
+                "params": {
+                    "shodan_analysis": {
+                        "value": "search",
+                        "type": "str",
+                        "description": "",
+                    }
+                },
             },
         }
-        pre_parsed_sample_config["Shodan_Search"].pop("extends")
 
         mock_read_config.return_value = sample_analyzer_config
-        mock_md5_config_file.return_value = md5(
-            str(random.random()).encode("utf-8")
-        ).hexdigest()
-        parsed_config = AbstractConfigSerializer.read_and_verify_config()
+        # _refresh ensures that cache isn't used, if any
+        parsed_config = AbstractConfigSerializer.read_and_verify_config(_refresh=True)
 
         mock_read_config.return_value = pre_parsed_sample_config
-        mock_md5_config_file.return_value = md5(
-            str(random.random()).encode("utf-8")
-        ).hexdigest()
-        expected_config = AbstractConfigSerializer.read_and_verify_config()
+        expected_config = AbstractConfigSerializer.read_and_verify_config(_refresh=True)
         self.assertDictEqual(parsed_config, expected_config)

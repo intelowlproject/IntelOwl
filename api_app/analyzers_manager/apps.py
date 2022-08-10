febcfcc2cc8da314f323261189c5eafc925638a7
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-08-10 14:37:24 +0530

    bugfix

--- api_app/analyzers_manager/apps.py
+++ api_app/analyzers_manager/apps.py
@@ -1,5 +1,6 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
+import sys
 
 from django.apps import AppConfig
 from django.conf import settings
@@ -11,9 +12,11 @@ class AnalyzersManagerConfig(AppConfig):
     name = "api_app.analyzers_manager"
 
     def ready(self):
-        from .serializers import AnalyzerConfigSerializer  # to avoid import issue
-
         set_permissions(settings.DEFAULT_CACHE)
-        # we "greedy cache" the config at start of application
-        # because it is an expensive operation
-        AnalyzerConfigSerializer.read_and_verify_config()
+
+        if "migrate" not in sys.argv:
+            from .serializers import AnalyzerConfigSerializer  # to avoid import issue
+
+            # we "greedy cache" the config at start of application
+            # because it is an expensive operation
+            AnalyzerConfigSerializer.read_and_verify_config()

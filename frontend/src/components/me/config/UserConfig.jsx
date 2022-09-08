bcfc22c66b55d59eea828015633f6fc86b58e22c
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-09-08 14:36:19 +0530

    Link to org config from user config

--- frontend/src/components/me/config/UserConfig.jsx
+++ frontend/src/components/me/config/UserConfig.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container } from "reactstrap";
 import useTitle from "react-use/lib/useTitle";
+import { Link } from "react-router-dom";
 import ConfigContainer from "./ConfigContainer";
 
 export default function UserConfig() {
@@ -14,8 +15,11 @@ export default function UserConfig() {
     <Container>
       <h4>Your plugin configuration</h4>
       <span className="text-muted">
-        Note: Your plugin configuration overrides your organization&apos;s
-        configuration (if any).
+        Note: Your plugin configuration overrides your{" "}
+        <Link to="/me/organization/config">
+          organization&apos;s configuration
+        </Link>{" "}
+        (if any).
       </span>
       <ConfigContainer filterFunction={(item) => !item.organization} />
     </Container>

e53273d96008f59e5ef1c3891bd2b24102f5d9f5
0x0elliot <wolfwithahat@protonmail.com>
2022-09-08 16:53:33 +0530

    Adding the handling of analyzer/connector report numbers differently on the frontend when playbooks are run

--- frontend/src/components/jobs/result/utils/JobOverview.jsx
+++ frontend/src/components/jobs/result/utils/JobOverview.jsx
@@ -9,20 +9,32 @@ import { JobInfoCard, JobIsRunningAlert, JobActionsBar } from "./sections";
 import { StatusIcon } from "../../../common";
 
 export default function JobOverview({ isRunningJob, job, refetch }) {
+  let AnalyzerDenominator = job.analyzers_requested?.length || "all";
+  let ConnectorDenominator = job.connectors_requested?.length || "all";
+
+  if (job.playbooks_to_execute?.length > 0) {
+    AnalyzerDenominator = job.analyzers_to_execute?.length;
+    if (job.connectors_to_execute?.length === 0) {
+      ConnectorDenominator = "None";
+    } else {
+      ConnectorDenominator = job.connectors_to_execute?.length;
+    }
+  }
+
   const tabTitles = React.useMemo(
     () => [
       <div className="d-flex-center">
         <strong>Analyzers Report</strong>
         <Badge className="ms-2">
           {job.analyzers_to_execute?.length} /&nbsp;
-          {job.analyzers_requested?.length}
+          {AnalyzerDenominator}
         </Badge>
       </div>,
       <div className="d-flex-center">
         <strong>Connectors Report</strong>
         <Badge className="ms-2">
           {job.connectors_to_execute?.length} /&nbsp;
-          {job.connectors_requested?.length}
+          {ConnectorDenominator}
         </Badge>
       </div>,
     ],

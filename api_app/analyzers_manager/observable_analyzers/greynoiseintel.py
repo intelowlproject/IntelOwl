82397af1ea6de8851934c68cc8641a5e28e4f714
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2022-09-16 08:58:09 +0200

    adjust

--- api_app/analyzers_manager/observable_analyzers/greynoiseintel.py
+++ api_app/analyzers_manager/observable_analyzers/greynoiseintel.py
@@ -25,29 +25,23 @@ class GreyNoiseAnalyzer(classes.ObservableAnalyzer):
         self.max_records_to_retrieve = int(params.get("max_records_to_retrieve", 500))
 
     def run(self):
+        api_key = self._secrets["api_key_name"]
         if self.api_version == "v2":
-            try:
-                api_key = self._secrets["api_key_name"]
-                session = GreyNoise(
-                    api_key=api_key, integration_name="greynoise-intelowl-v1.0"
-                )
-                response = session.ip(self.observable_name)
-                response |= session.riot(self.observable_name)
-            except Exception as e:
-                raise AnalyzerRunException(f"GreyNoise Exception: {e}")
+            session = GreyNoise(
+                api_key=api_key, integration_name="greynoise-intelowl-v1.0"
+            )
+            response = session.ip(self.observable_name)
+            response |= session.riot(self.observable_name)
         elif self.api_version == "v3":
-            try:
-                api_key = self._secrets["api_key_name"]
-                params = {
-                    "integration_name": "greynoise-community-intelowl-v1.0",
-                    "offering": "Community",
-                }
-                if api_key:
-                    params["api_key"] = api_key
-                session = GreyNoise(**params)
-                response = session.ip(self.observable_name)
-            except Exception as e:
-                raise AnalyzerRunException(f"GreyNoise Exception: {e}")
+            # this allows to use this service without an API key set
+            if not api_key:
+                api_key = ""
+            session = GreyNoise(
+                api_key=api_key,
+                integration_name="greynoise-community-intelowl-v1.0",
+                offering="Community",
+            )
+            response = session.ip(self.observable_name)
         else:
             raise AnalyzerRunException(
                 "Invalid API Version. " "Supported are: v2 (paid), v3 (community)"

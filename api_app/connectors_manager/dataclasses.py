1ee567347d4d10f81cd3eab7f70d292a8bb5db73
0x0elliot <wolfwithahat@protonmail.com>
2022-10-02 04:00:25 +0530

    Fixing connector checks during CI checks

--- api_app/connectors_manager/dataclasses.py
+++ api_app/connectors_manager/dataclasses.py
@@ -65,7 +65,7 @@ class ConnectorConfig(AbstractConfig):
         return [
             connector
             for connector in connectors_to_execute
-            if connector_dataclass.get(connector).is_ready_to_use
+            if connector_dataclass.get(connector).is_ready_to_use and settings.STAGE_CI
         ]
 
     @classmethod

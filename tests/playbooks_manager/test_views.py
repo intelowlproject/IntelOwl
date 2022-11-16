703875f4c3916bdfa481b4f17fc733ce77d9d27d
0x0elliot <wolfwithahat@protonmail.com>
2022-11-17 00:45:38 +0530

    Finishing up test cases

--- tests/playbooks_manager/test_views.py
+++ tests/playbooks_manager/test_views.py
@@ -1,17 +1,47 @@
+from django.conf import settings
 from django.test import TransactionTestCase
 
-from api_app.models import Job
+from api_app.analyzers_manager.serializers import AnalyzerConfigSerializer
 from api_app.playbooks_manager.dataclasses import PlaybookConfig
 from api_app.playbooks_manager.serializers import CachedPlaybooksSerializer
 from api_app.playbooks_manager.views import _cache_playbook
+from api_app.serializers import ObservableAnalysisSerializer
 
 
 class PlaybookViewTestCase(TransactionTestCase):
+    def setUp(self):
+        self.analyzer_serializer_class = ObservableAnalysisSerializer
+        self.superuser = settings.AUTH_USER_MODEL.objects.create_superuser(
+            username="test", email="test@intelowl.com", password="test"
+        )
 
-    playbook_name = ""
+        analyzers_used = ["Classic_DNS"]
+        data = {
+            "observables": [
+                ["url", "google.com"],
+            ],
+            "analyzers_requested": analyzers_used,
+            "connectors_requested": [],
+            "tlp": "WHITE",
+            "runtime_configuration": {},
+            "tags_labels": [],
+        }
+
+        self.supports = ["ip", "domain", "url"]
+
+        serializer = self.serializer_class(data=data, many=True)
+        serializer.is_valid(raise_exception=True)
+
+        serialized_data = serializer.validated_data
+        [data.pop("runtime_configuration", {}) for data in serialized_data]
+
+        self.test_jobs = serializer.save(
+            user=self.superuser,
+        )
 
     def test_cache_config(self):
-        job = Job.objects.first()
+        AnalyzerConfigSerializer
+        job = self.test_job
         planned_name = "TEST_NEW_PLAYBOOK"
         planned_description = "This is a test description"
         data = {
@@ -25,7 +55,7 @@ class PlaybookViewTestCase(TransactionTestCase):
 
         self.assertEqual(planned_description, playbook.get("description"))
 
-        self.assertNotEqual(playbook.get("supports"), [])
+        self.assertListEqual(playbook.get("supports"), self.supports)
 
         self.assertEqual(
             playbook.get("default"), True

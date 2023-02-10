1abde85a67f0958d1a3473d5a575e775c5f73882
0ssigeno <s.berni@certego.net>
2023-02-10 14:52:58 +0100

    Fixing
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- intel_owl/settings/certego.py
+++ intel_owl/settings/certego.py
@@ -2,5 +2,7 @@
 HOST_URI = "http://localhost"
 HOST_NAME = "IntelOwl"
 CERTEGO_SAAS = {
-    "USER_ACCESS_SERIALIZER": "authentication.serializers.UserAccessSerializer"
+    "USER_ACCESS_SERIALIZER": "authentication.serializers.UserAccessSerializer",
+    "HOST_URI": HOST_URI,
+    "HOST_NAME": HOST_NAME
 }

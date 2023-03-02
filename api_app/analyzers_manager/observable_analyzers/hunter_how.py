316193614c2075b9626f8008b370b7a1cd549cde
shivam <shivam@ubuntu.myguest.virtualbox.org>
2023-03-02 17:57:12 +0530

    precommits

--- api_app/analyzers_manager/observable_analyzers/hunter_how.py
+++ api_app/analyzers_manager/observable_analyzers/hunter_how.py
@@ -22,6 +22,7 @@ class Hunter_How(classes.ObservableAnalyzer):
             self.query = f'ip="{self.observable_name}"'
         elif self.observable_classification == self.ObservableTypes.DOMAIN:
             self.query = f'domain="{self.observable_name}"'
+
         self.encoded_query = base64.urlsafe_b64encode(
             self.query.encode("utf-8")
         ).decode("ascii")
@@ -30,15 +31,18 @@ class Hunter_How(classes.ObservableAnalyzer):
         self.start_time = params.get("start_time", "")
         self.end_time = params.get("end_time", "")
 
+        self.parameters = {
+            "api-key": self.__api_key,
+            "query": self.encoded_query,
+            "page": self.page,
+            "page_size": self.page_size,
+            "start_time": self.start_time,
+            "end_time": self.end_time,
+        }
+
     def run(self):
         try:
-            api_url = f"?api-key={self.__api_key}"
-            query_url = f"&query={self.encoded_query}"
-            page_url = f"&page={self.page}&page_size={self.page_size}"
-            time_url = f"&start_time={self.start_time}&end_time={self.end_time}"
-            params_url = api_url + query_url + page_url + time_url
-
-            response_ip = requests.get(self.base_url + params_url)
+            response_ip = requests.get(self.base_url, params=self.parameters)
             response_ip.raise_for_status()
 
         except requests.RequestException as e:

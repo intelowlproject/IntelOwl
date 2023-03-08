1d60086a6f89d804062d739bcfb9a072cac10ae8
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2023-03-08 10:37:07 +0100

    adjusted AWS SQS integration

--- intel_owl/settings/aws.py
+++ intel_owl/settings/aws.py
@@ -1,6 +1,8 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
 
+import sys
+
 from intel_owl import secrets
 
 # AWS settings
@@ -9,6 +11,14 @@ if not AWS_IAM_ACCESS:
     AWS_ACCESS_KEY_ID = secrets.get_secret("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = secrets.get_secret("AWS_SECRET_ACCESS_KEY")
 
-AWS_SECRETS = secrets.get_secret("AWS_SECRETS", False) == "True"
-AWS_SQS = secrets.get_secret("AWS_SQS", False) == "True"
 AWS_REGION = secrets.get_secret("AWS_REGION", "eu-central-1")
+AWS_USER_NUMBER = secrets.get_secret("AWS_USER_NUMBER")
+
+AWS_SECRETS = secrets.get_secret("AWS_SECRETS", False) == "True"
+
+AWS_SQS = secrets.get_secret("AWS_SQS", False) == "True"
+if AWS_SQS:
+    if not AWS_USER_NUMBER:
+        print("you must specify the USER NUMBER")
+        sys.exit(4)
+    BROKER_URL = "sqs://"

38d8723c6eed75bbae378c74518be7bb3f4d45d0
Kyle Taylor <kyletaylored@gmail.com>
2023-03-15 13:05:25 -0500

    Remove unnecessary directory context

--- docker/scripts/install_logrotate.sh
+++ docker/scripts/install_logrotate.sh
@@ -1,4 +1,1 @@
-# Get the directory path of the script (including symbolic link)
-dir="$(dirname "$(readlink -f "$0")")"
-
-sudo cp -r "$dir/logrotate" /etc/logrotate.d
+sudo cp -r logrotate /etc/logrotate.d

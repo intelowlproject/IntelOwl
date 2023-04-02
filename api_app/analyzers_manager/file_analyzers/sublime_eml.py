fd1f63fcfadad678d3ab1c02814997f62e97d220
wnyyyy <viniberizzi@gmail.com>
2023-04-02 15:08:09 -0400

    Added Sublime EML analyzer
    added sublime rules download
    
    add sublime rules download
    
    add sublime eml analyzer
    
    Update documentation for Sublime EML
    
    final fixes

---
+++ api_app/analyzers_manager/file_analyzers/sublime_eml.py
@@ -0,0 +1,22 @@
+# This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
+# See the file 'LICENSE' for copying permission.
+import sublime
+
+from api_app.analyzers_manager.classes import FileAnalyzer
+from api_app.exceptions import AnalyzerRunException
+
+
+class SublimeEML(FileAnalyzer):
+    def run(self):
+        client = sublime.Sublime()
+        raw_message_eml = sublime.util.load_eml(self.filepath)
+        rules, queries = sublime.util.load_yml_path(
+            "/opt/deploy/sublime_eml/detection-rules"
+        )
+
+        try:
+            response = client.analyze_raw_message(raw_message_eml, rules, queries)
+        except Exception as e:
+            raise AnalyzerRunException(e)
+
+        return response

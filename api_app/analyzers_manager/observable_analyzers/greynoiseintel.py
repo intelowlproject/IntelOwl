4912d4fdd12edf2208cecab1a201cc0b95c06d5f
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2023-05-15 11:26:28 +0200

    adjustment for greynoise

--- api_app/analyzers_manager/observable_analyzers/greynoiseintel.py
+++ api_app/analyzers_manager/observable_analyzers/greynoiseintel.py
@@ -1,11 +1,16 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
 
+import logging
+
 from greynoise import GreyNoise
+from greynoise.exceptions import NotFound, RateLimitError, RequestFailure
 
 from api_app.analyzers_manager import classes
 from api_app.analyzers_manager.exceptions import AnalyzerRunException
 from tests.mock_utils import if_mock_connections, patch
+
+logger = logging.getLogger(__name__)
 
 
 class GreyNoiseAnalyzer(classes.ObservableAnalyzer):
@@ -15,24 +20,39 @@ class GreyNoiseAnalyzer(classes.ObservableAnalyzer):
     _api_key_name: str
 
     def run(self):
-        if self.greynoise_api_version == "v2":
-            session = GreyNoise(
-                api_key=self._api_key_name, integration_name="greynoise-intelowl-v1.0"
-            )
-            response = session.ip(self.observable_name)
-            response |= session.riot(self.observable_name)
-        elif self.greynoise_api_version == "v3":
-            # this allows to use this service without an API key set
-            session = GreyNoise(
-                api_key=self._api_key_name,
-                integration_name="greynoise-community-intelowl-v1.0",
-                offering="Community",
-            )
-            response = session.ip(self.observable_name)
-        else:
-            raise AnalyzerRunException(
-                "Invalid API Version. " "Supported are: v2 (paid), v3 (community)"
-            )
+        response = {}
+        try:
+            if self.greynoise_api_version == "v2":
+                session = GreyNoise(
+                    api_key=self._api_key_name,
+                    integration_name="greynoise-intelowl-v1.0",
+                )
+                response = session.ip(self.observable_name)
+                response |= session.riot(self.observable_name)
+
+            elif self.greynoise_api_version == "v3":
+                # this allows to use this service without an API key set
+                session = GreyNoise(
+                    api_key=self._api_key_name,
+                    integration_name="greynoise-community-intelowl-v1.0",
+                    offering="Community",
+                )
+                response = session.ip(self.observable_name)
+            else:
+                raise AnalyzerRunException(
+                    "Invalid API Version. Supported are: v2 (paid), v3 (community)"
+                )
+        except RateLimitError as e:
+            self.report.errors.append(e)
+            self.report.save()
+            raise AnalyzerRunException(f"Rate limit error: {e}")
+        except RequestFailure as e:
+            self.report.errors.append(e)
+            self.report.save()
+            raise AnalyzerRunException(f"Request failure error: {e}")
+        except NotFound as e:
+            logger.info(f"not found error for {self.observable_name} :{e}")
+            response["not_found"] = True
 
         return response
 

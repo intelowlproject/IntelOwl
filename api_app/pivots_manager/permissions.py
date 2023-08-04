4986e867d9a00522e469c472732652f57127eef4
0ssigeno <s.berni@certego.net>
2023-08-04 15:05:11 +0200

    Deepsource
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- api_app/pivots_manager/permissions.py
+++ api_app/pivots_manager/permissions.py
@@ -2,7 +2,8 @@ from rest_framework.permissions import BasePermission
 
 
 class PivotOwnerPermission(BasePermission):
-    def has_object_permission(self, request, view, obj):
+    @staticmethod
+    def has_object_permission(request, view, obj):
         return (
             obj.starting_job.user.pk == request.user.pk
             and obj.ending_job.user.pk == request.user.pk

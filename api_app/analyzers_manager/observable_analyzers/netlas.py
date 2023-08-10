6043873c4485a3d2ddf9734156c69f940d7db318
deep <deeppoharkar21@gmail.com>
2023-08-10 09:20:45 +0530

    add mockresponse for netlas

--- api_app/analyzers_manager/observable_analyzers/netlas.py
+++ api_app/analyzers_manager/observable_analyzers/netlas.py
@@ -36,11 +36,62 @@ class Netlas(classes.ObservableAnalyzer):
 
     @classmethod
     def _monkeypatch(cls):
+        example_response = {
+            "@timestamp": "2023-07-06T14:53:32",
+            "ip": {"gte": "8.8.8.0", "lte": "8.8.8.255"},
+            "related_nets": [
+                {
+                    "country": "US",
+                    "address": "1600 Amphitheatre Parkway",
+                    "city": "Mountain View",
+                    "created": "2014-03-14",
+                    "range": "8.8.8.0 - 8.8.8.255",
+                    "description": "Google LLC",
+                    "handle": "NET-8-8-8-0-1",
+                    "organization": "Google LLC (GOGL)",
+                    "name": "LVLT-GOGL-8-8-8",
+                    "start_ip": "8.8.8.0",
+                    "cidr": ["8.8.8.0/24"],
+                    "net_size": 255,
+                    "state": "CA",
+                    "postal_code": "94043",
+                    "updated": "2014-03-14",
+                    "end_ip": "8.8.8.255",
+                }
+            ],
+            "net": {
+                "country": "US",
+                "address": "100 CenturyLink Drive",
+                "city": "Monroe",
+                "created": "1992-12-01",
+                "range": "8.0.0.0 - 8.127.255.255",
+                "description": "Level 3 Parent, LLC",
+                "handle": "NET-8-0-0-0-1",
+                "organization": "Level 3 Parent, LLC (LPL-141)",
+                "name": "LVLT-ORG-8-8",
+                "start_ip": "8.0.0.0",
+                "cidr": ["8.0.0.0/9"],
+                "net_size": 8388607,
+                "state": "LA",
+                "postal_code": "71203",
+                "updated": "2018-04-23",
+                "end_ip": "8.127.255.255",
+            },
+            "asn": {
+                "number": ["15169"],
+                "registry": "arin",
+                "country": "US",
+                "name": "GOOGLE",
+                "cidr": "8.8.8.0/24",
+                "updated": "1992-12-01",
+            },
+        }
+
         patches = [
             if_mock_connections(
                 patch(
                     "requests.get",
-                    return_value=MockUpResponse({}, 200),
+                    return_value=MockUpResponse(example_response, 200),
                 ),
             )
         ]

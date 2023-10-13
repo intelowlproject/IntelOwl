983f148d30a627291f6b7ca2980bfc1e6f1af592
Daniele Rosetti <d.rosetti@certego.net>
2023-10-13 11:10:41 +0200

    added job type in the job tables

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,29 +1,95 @@
-// app specific
-export const JOB_TYPE_COLOR_MAP = {
+export const JOB_TYPE = Object.freeze({
+  FILE: "file",
+  OBSERVABLE: "observable",
+});
+export const FILE_MIME_TYPES = Object.freeze({
+  // IMPORTANT! in case you update this Object remember to update also the blackend
+  WSCRIPT: "application/w-script-file",
+  JAVASCRIPT1: "application/javascript",
+  JAVASCRIPT2: "application/x-javascript",
+  JAVASCRIPT3: "text/javascript",
+  VB_SCRIPT: "application/x-vbscript",
+  IQY: "text/x-ms-iqy",
+  APK: "application/vnd.android.package-archive",
+  DEX: "application/x-dex",
+  ONE_NOTE: "application/onenote",
+  ZIP1: "application/zip",
+  ZIP2: "multipart/x-zip",
+  JAVA: "application/java-archive",
+  RTF1: "text/rtf",
+  RTF2: "application/rtf",
+  DOS: "application/x-dosexec",
+  SHARED_LIB: "application/x-sharedlib",
+  EXE: "application/x-executable",
+  ELF: "application/x-elf",
+  OCTET: "application/octet-stream",
+  PCAP: "application/vnd.tcpdump.pcap",
+  PDF: "application/pdf",
+  HTML: "text/html",
+  PUB: "application/x-mspublisher",
+  EXCEL_MACRO1: "application/vnd.ms-excel.addin.macroEnabled",
+  EXCEL_MACRO2: "application/vnd.ms-excel.sheet.macroEnabled.12",
+  EXCEL1: "application/vnd.ms-excel",
+  EXCEL2: "application/excel",
+  DOC: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  XML1: "application/xml",
+  XML2: "text/xml",
+  ENCRYPTED: "application/encrypted",
+  PLAIN: "text/plain",
+  CSV: "text/csv",
+  PPTX: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+  WORD1: "application/msword",
+  WORD2:
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  POWERPOINT: "application/vnd.ms-powerpoint",
+  OFFICE: "application/vnd.ms-office",
+  BINARY: "application/x-binary",
+  MAC1: "application/x-macbinary",
+  MAC2: "application/mac-binary",
+  MAC3: "application/x-mach-binary",
+  COMPRESS1: "application/x-zip-compressed",
+  COMPRESS2: "application/x-compressed",
+  OUTLOOK: "application/vnd.ms-outlook",
+  EML: "message/rfc822",
+  PKCS7: "application/pkcs7-signature",
+  XPKCS7: "application/x-pkcs7-signature",
+  MIXED: "multipart/mixed",
+  X_SHELLSCRIPT: "text/x-shellscript",
+});
+export const OBSERVABLE_CLASSIFICATION = Object.freeze({
+  IP: "ip",
+  URL: "url",
+  DOMAIN: "domain",
+  HASH: "hash",
+  GENERIC: "generic",
+});
+// colors
+export const JOB_TYPE_COLOR_MAP = Object.freeze({
   file: "#ed896f",
   observable: "#42796f",
-};
-export const TLP_COLOR_MAP = {
+});
+export const TLP_COLOR_MAP = Object.freeze({
   CLEAR: "#FFFFFF",
   GREEN: "#33FF00",
   AMBER: "#FFC000",
   RED: "#FF0033",
-};
-export const OBSERVABLE_CLASSIFICATION_COLOR_MAP = {
+});
+
+export const OBSERVABLE_CLASSIFICATION_COLOR_MAP = Object.freeze({
   ip: "#9aa66c",
   url: "#7da7d3",
   domain: "#8070ed",
   hash: "#ed896f",
   generic: "#733010",
-};
-export const TLP_DESCRIPTION_MAP = {
+});
+export const TLP_DESCRIPTION_MAP = Object.freeze({
   CLEAR: "TLP: use all analyzers",
   GREEN: "TLP: disable analyzers that could impact privacy",
   AMBER:
     "TLP: disable analyzers that could impact privacy and limit access to my organization",
   RED: "TLP: disable analyzers that could impact privacy, limit access to my organization and do not use any external service",
-};
-export const JOB_STATUS_COLOR_MAP = {
+});
+export const JOB_STATUS_COLOR_MAP = Object.freeze({
   pending: "light",
   running: "secondary",
   analyzers_running: "secondary",
@@ -40,29 +106,24 @@ export const JOB_STATUS_COLOR_MAP = {
   reported_with_fails: "warning",
   reported_without_fails: "success",
   failed: "danger",
-};
-export const REPORT_STATUS_COLOR_MAP = {
+});
+export const REPORT_STATUS_COLOR_MAP = Object.freeze({
   pending: "light",
   running: "secondary",
   killed: "gray",
   success: "success",
   failed: "danger",
-};
-export const STATUS_COLORMAP = {
+});
+export const STATUS_COLORMAP = Object.freeze({
   ...JOB_STATUS_COLOR_MAP,
   ...REPORT_STATUS_COLOR_MAP,
-};
+});
 export const TLP_CHOICES = Object.keys(TLP_COLOR_MAP);
-export const OBSERVABLE_TYPES = Object.keys(
-  OBSERVABLE_CLASSIFICATION_COLOR_MAP,
-);
 
-export const scanTypes = {
+export const scanTypes = Object.freeze({
   playbooks: "Playbooks",
   analyzers_and_connectors: "Analyzers/Connectors",
-};
-
-export const ALL_CLASSIFICATIONS = OBSERVABLE_TYPES.concat("file");
+});
 
 export const HACKER_MEME_STRING =
   "LoOk At YoU hAcKeR a PaThEtIc CrEaTuRe Of MeAt AnD bOnE";

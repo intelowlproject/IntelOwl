c31204d971b959c56d6475e95e44469c8182f800
0ssigeno <s.berni@certego.net>
2023-10-19 12:36:15 +0200

    Fix signal
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- api_app/playbooks_manager/signals.py
+++ api_app/playbooks_manager/signals.py
@@ -1,7 +1,10 @@
+from typing import Type
+
 from django.core.exceptions import ValidationError
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
+from api_app.pivots_manager.models import PivotConfig
 from api_app.playbooks_manager.models import PlaybookConfig
 
 
@@ -31,16 +34,18 @@ def m2m_changed_pivots_playbook_config(
     instance: PlaybookConfig,
     action: str,
     reverse,
-    model,
+    model: Type[PivotConfig],
     pk_set,
     using,
     *args,
     **kwargs,
 ):
     if action == "pre_add":
-        valid_objects = model.objects.filter(pk__in=pk_set).valid(
-            instance.analyzers.all(), instance.connectors.all()
+        valid_pks = (
+            model.objects.filter(pk__in=pk_set)
+            .valid(instance.analyzers.all(), instance.connectors.all())
+            .values_list("pk", flat=True)
         )
-        wrong_pks = ", ".join([pk for pk in pk_set if pk not in valid_objects])
+        wrong_pks = ", ".join([str(pk) for pk in pk_set if pk not in valid_pks])
         if wrong_pks:
             raise ValidationError(f"You can't set pivots {wrong_pks}")

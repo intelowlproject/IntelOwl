e984157821051b7873af242741dd6a97899d9b9d
Martina Carella <m.carella@certego.net>
2023-10-26 10:38:00 +0200

    fixed playbook options

--- frontend/tests/components/scan/ScanForm.test.jsx
+++ frontend/tests/components/scan/ScanForm.test.jsx
@@ -225,6 +225,10 @@ jest.mock("../../../src/stores", () => ({
       checkPluginHealth: () => {},
     }),
   ),
+  useOrganizationStore: jest.fn((state) =>
+  state({
+    pluginsState: {},
+  }))
 }));
 // mock RecentScans component
 jest.mock("../../../src/components/scan/utils/RecentScans", () =>

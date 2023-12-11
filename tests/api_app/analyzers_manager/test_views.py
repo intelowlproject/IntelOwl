1a49880a2fec3cd2da066e5fdcacf6bc6c00dced
Daniele Rosetti <d.rosetti@certego.net>
2023-12-11 09:55:43 +0100

    fix test

--- tests/api_app/analyzers_manager/test_views.py
+++ tests/api_app/analyzers_manager/test_views.py
@@ -66,7 +66,7 @@ class AnalyzerConfigViewSetTestCase(
         result = response.json()
         self.assertIn("errors", result)
         self.assertIn("detail", result["errors"])
-        self.assertEqual(result["errors"]["detail"], "Not implemented")
+        self.assertEqual(result["errors"]["detail"], "No healthcheck implemented")
 
 
 class AnalyzerActionViewSetTests(CustomViewSetTestCase, PluginActionViewsetTestCase):

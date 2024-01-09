43871ce0b148447ee6a5655de81bc8d5ea592b70
0ssigeno <s.berni@certego.net>
2024-01-09 08:40:58 +0100

    Fix
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- api_app/queryset.py
+++ api_app/queryset.py
@@ -130,6 +130,8 @@ class AbstractConfigQuerySet(CleanOnCreateQuerySet):
 
 
 class JobQuerySet(CleanOnCreateQuerySet, SendToBiQuerySet):
+
+    @classmethod
     def _get_bi_serializer_class(cls):
         from api_app.serializers import JobBISerializer
 

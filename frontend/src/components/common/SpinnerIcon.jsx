0fae7a0427c5386a92bb47ef5602e4a772f090a1
Daniele Rosetti <d.rosetti@certego.net>
2024-01-12 09:26:58 +0100

    restored old comment

--- frontend/src/components/common/SpinnerIcon.jsx
+++ frontend/src/components/common/SpinnerIcon.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Spinner } from "reactstrap";
 
+// we can't delete this function because IconButton expects Icon as a function
 export function SpinnerIcon() {
   return <Spinner type="border" size="sm" className="text-darker" />;
 }

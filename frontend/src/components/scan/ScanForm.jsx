473df73a2807cd70f203fc8c6a6f1d1701ba48ac
Martina Carella <m.carella@certego.net>
2024-01-23 11:48:24 +0100

    prettier

--- frontend/src/components/scan/ScanForm.jsx
+++ frontend/src/components/scan/ScanForm.jsx
@@ -330,8 +330,8 @@ export default function ScanForm() {
           currentAnalyzer.isDisabled === nextAnalyzer.isDisabled
             ? 0
             : currentAnalyzer.isDisabled
-            ? 1
-            : -1,
+              ? 1
+              : -1,
         ),
     [analyzersGrouped, formik.values.classification],
   );
@@ -363,8 +363,8 @@ export default function ScanForm() {
           currentConnector.isDisabled === nextConnector.isDisabled
             ? 0
             : currentConnector.isDisabled
-            ? 1
-            : -1,
+              ? 1
+              : -1,
         ),
     [connectors],
   );

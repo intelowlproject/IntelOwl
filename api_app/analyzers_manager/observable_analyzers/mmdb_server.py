08bec3a7e0f8f22809ab48e9d034b2b74f1d041e
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2024-01-29 17:16:27 +0100

    fix monkeypatch

--- api_app/analyzers_manager/observable_analyzers/mmdb_server.py
+++ api_app/analyzers_manager/observable_analyzers/mmdb_server.py
@@ -39,48 +39,44 @@ class MmdbServer(classes.ObservableAnalyzer):
                     "requests.get",
                     return_value=MockUpResponse(
                         {
-                            (
-                                {
-                                    "country": {"iso_code": "BE"},
-                                    "meta": {
-                                        "description": {"en": "Geo Open MMDB database"},
-                                        "build_db": "2022-02-05 11:37:33",
-                                        "db_source": "GeoOpen-Country",
-                                        "nb_nodes": 1159974,
-                                    },
-                                    "ip": "188.65.220.25",
-                                    "country_info": {
-                                        "Country": "Belgium",
-                                        "Alpha-2 code": "BE",
-                                        "Alpha-3 code": "BEL",
-                                        "Numeric code": "56",
-                                        "Latitude (average)": "50.8333",
-                                        "Longitude (average)": "4",
-                                    },
-                                },
-                                {
-                                    "country": {
-                                        "iso_code": "BE",
-                                        "AutonomousSystemNumber": "49677",
-                                        "ASO": "MAEHDROS-AS",
-                                    },
-                                    "meta": {
-                                        "description": {"en": "Geo Open MMDB database"},
-                                        "build_db": "2022-02-06 10:30:25",
-                                        "db_source": "GeoOpen-Country-ASN",
-                                        "nb_nodes": 1159815,
-                                    },
-                                    "ip": "188.65.220.25",
-                                    "country_info": {
-                                        "Country": "Belgium",
-                                        "Alpha-2 code": "BE",
-                                        "Alpha-3 code": "BEL",
-                                        "Numeric code": "56",
-                                        "Latitude (average)": "50.8333",
-                                        "Longitude (average)": "4",
-                                    },
-                                },
-                            )
+                            "country": {"iso_code": "BE"},
+                            "meta": {
+                                "description": {"en": "Geo Open MMDB database"},
+                                "build_db": "2022-02-05 11:37:33",
+                                "db_source": "GeoOpen-Country",
+                                "nb_nodes": 1159974,
+                            },
+                            "ip": "188.65.220.25",
+                            "country_info": {
+                                "Country": "Belgium",
+                                "Alpha-2 code": "BE",
+                                "Alpha-3 code": "BEL",
+                                "Numeric code": "56",
+                                "Latitude (average)": "50.8333",
+                                "Longitude (average)": "4",
+                            },
+                        },
+                        {
+                            "country": {
+                                "iso_code": "BE",
+                                "AutonomousSystemNumber": "49677",
+                                "ASO": "MAEHDROS-AS",
+                            },
+                            "meta": {
+                                "description": {"en": "Geo Open MMDB database"},
+                                "build_db": "2022-02-06 10:30:25",
+                                "db_source": "GeoOpen-Country-ASN",
+                                "nb_nodes": 1159815,
+                            },
+                            "ip": "188.65.220.25",
+                            "country_info": {
+                                "Country": "Belgium",
+                                "Alpha-2 code": "BE",
+                                "Alpha-3 code": "BEL",
+                                "Numeric code": "56",
+                                "Latitude (average)": "50.8333",
+                                "Longitude (average)": "4",
+                            },
                         },
                         200,
                     ),

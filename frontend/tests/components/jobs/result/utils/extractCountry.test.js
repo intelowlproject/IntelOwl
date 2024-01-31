78878b9bbed2653bd604f06aea505afb0ddd7558
Daniele Rosetti <d.rosetti@certego.net>
2024-01-31 12:26:30 +0100

    updated flag extraction in job report

--- frontend/tests/components/jobs/result/utils/extractCountry.test.js
+++ frontend/tests/components/jobs/result/utils/extractCountry.test.js
@@ -134,41 +134,69 @@ describe("extractCountry test", () => {
     expect(country.countryName).toBe("Sweden");
   });
 
-  test("AbuseIPBD with no data and Maxmind report", () => {
+  test("Ip2Location report", () => {
     const job = {
       id: 2,
       analyzer_reports: [
         {
-          name: "AbuseIPDB",
-          process_time: 0.07,
-          status: "SUCCESS",
-          report: {},
-        },
-        {
-          name: "Maxmind",
+          name: "Ip2location",
           process_time: 0.07,
           status: "SUCCESS",
           end_time: "2024-01-15T14:56:32.328332Z",
           report: {
-            data: {
-              names: {
-                de: "Schweden",
-                en: "Sweden",
-                es: "Suecia",
-                fr: "Suède",
-                ja: "スウェーデン王国",
-                ru: "Швеция",
-                "pt-BR": "Suécia",
-                "zh-CN": "瑞典",
-              },
-              iso_code: "SE",
-              geoname_id: 2661886,
-              is_in_european_union: true,
-              // other keys (locations, continent, registered_country) unsued
-            },
+            as:"Google LLC",
+            ip:"8.8.8.8",
+            asn:"15169",
+            is_proxy:false,
+            latitude:37.38605,
+            zip_code:"94035",
+            city_name:"Mountain View",
+            longitude:-122.08385,
+            time_zone:"-08:00",
+            region_name:"California",
+            country_code:"IT",
+            country_name:"Italy",
           },
         },
       ],
+      connector_reports: [],
+      pivot_reports: [],
+      visualizer_reports: [],
+      is_sample: false,
+      md5: "",
+      observable_name: "8.8.8.8",
+      observable_classification: "ip",
+      file_name: "",
+      file_mimetype: "",
+      status: "reported_without_fails",
+      runtime_configuration: {},
+      received_request_time: "2024-01-15T14:56:32.019310Z",
+      finished_analysis_time: "2024-01-15T14:56:32.353326Z",
+      process_time: null,
+      tlp: "CLEAR",
+      errors: [],
+      warnings: [],
+      scan_mode: 2,
+      scan_check_time: "1 00:00:00",
+      sent_to_bi: false,
+      playbook_requested: "",
+      playbook_to_execute: "",
+      analyzers_requested: ["Ip2location"],
+      connectors_requested: [],
+      analyzers_to_execute: ["Ip2location"],
+      connectors_to_execute: [],
+      visualizers_to_execute: [],
+    };
+
+    const country = extractCountry(job);
+    expect(country.countryCode).toBe("IT");
+    expect(country.countryName).toBe("Italy");
+  });
+
+  test("no reports", () => {
+    const job = {
+      id: 2,
+      analyzer_reports: [],
       connector_reports: [],
       pivot_reports: [],
       visualizer_reports: [],
@@ -191,19 +219,19 @@ describe("extractCountry test", () => {
       sent_to_bi: false,
       playbook_requested: "",
       playbook_to_execute: "",
-      analyzers_requested: ["AbuseIPDB", "Maxmind"],
+      analyzers_requested: ["AbuseIPDB"],
       connectors_requested: [],
-      analyzers_to_execute: ["AbuseIPDB", "Maxmind"],
+      analyzers_to_execute: ["AbuseIPDB"],
       connectors_to_execute: [],
       visualizers_to_execute: [],
     };
 
     const country = extractCountry(job);
     expect(country.countryCode).toBe("");
-    expect(country.countryName).toBe("Sweden");
+    expect(country.countryName).toBe("");
   });
 
-  test("AbuseIPBD and Maxmind reports", () => {
+  test("empty reports", () => {
     const job = {
       id: 2,
       analyzer_reports: [
@@ -211,6 +239,67 @@ describe("extractCountry test", () => {
           name: "AbuseIPDB",
           process_time: 0.07,
           status: "SUCCESS",
+          end_time: "2024-01-15T14:56:32.328332Z",
+          report: {},
+        },
+        {
+          name: "Maxmind",
+          process_time: 0.07,
+          status: "SUCCESS",
+          end_time: "2024-01-15T14:56:32.328332Z",
+          report: {},
+        },
+        {
+          name: "Ip2location",
+          process_time: 0.07,
+          status: "SUCCESS",
+          end_time: "2024-01-15T14:56:32.328332Z",
+          report: {},
+        },
+      ],
+      connector_reports: [],
+      pivot_reports: [],
+      visualizer_reports: [],
+      is_sample: false,
+      md5: "",
+      observable_name: "8.8.8.8",
+      observable_classification: "ip",
+      file_name: "",
+      file_mimetype: "",
+      status: "reported_without_fails",
+      runtime_configuration: {},
+      received_request_time: "2024-01-15T14:56:32.019310Z",
+      finished_analysis_time: "2024-01-15T14:56:32.353326Z",
+      process_time: null,
+      tlp: "CLEAR",
+      errors: [],
+      warnings: [],
+      scan_mode: 2,
+      scan_check_time: "1 00:00:00",
+      sent_to_bi: false,
+      playbook_requested: "",
+      playbook_to_execute: "",
+      analyzers_requested: ["AbuseIPDB"],
+      connectors_requested: [],
+      analyzers_to_execute: ["AbuseIPDB"],
+      connectors_to_execute: [],
+      visualizers_to_execute: [],
+    };
+
+    const country = extractCountry(job);
+    expect(country.countryCode).toBe("");
+    expect(country.countryName).toBe("");
+  });
+
+  test("all reports", () => {
+    const job = {
+      id: 2,
+      analyzer_reports: [
+        {
+          name: "AbuseIPDB",
+          process_time: 0.07,
+          status: "SUCCESS",
+          end_time: "2024-01-15T14:56:32.328332Z",
           report: {
             data: {
               isp: "Microsoft Corporation",
@@ -222,8 +311,8 @@ describe("extractCountry test", () => {
               ipAddress: "20.190.181.3",
               ipVersion: 4,
               usageType: "Data Center/Web Hosting/Transit",
-              countryCode: "IT",
-              countryName: "Italy",
+              countryCode: "US",
+              countryName: "United States of America",
               totalReports: 0,
               isWhitelisted: null,
               lastReportedAt: null,
@@ -255,7 +344,27 @@ describe("extractCountry test", () => {
               geoname_id: 2661886,
               is_in_european_union: true,
               // other keys (locations, continent, registered_country) unsued
-            },
+            }
+          },
+        },
+        {
+          name: "Ip2location",
+          process_time: 0.07,
+          status: "SUCCESS",
+          end_time: "2024-01-15T14:56:32.328332Z",
+          report: {
+            as:"Google LLC",
+            ip:"8.8.8.8",
+            asn:"15169",
+            is_proxy:false,
+            latitude:37.38605,
+            zip_code:"94035",
+            city_name:"Mountain View",
+            longitude:-122.08385,
+            time_zone:"-08:00",
+            region_name:"California",
+            country_code:"IT",
+            country_name:"Italy",
           },
         },
       ],
@@ -264,7 +373,7 @@ describe("extractCountry test", () => {
       visualizer_reports: [],
       is_sample: false,
       md5: "",
-      observable_name: "1.1.1.1",
+      observable_name: "8.8.8.8",
       observable_classification: "ip",
       file_name: "",
       file_mimetype: "",
@@ -281,67 +390,16 @@ describe("extractCountry test", () => {
       sent_to_bi: false,
       playbook_requested: "",
       playbook_to_execute: "",
-      analyzers_requested: ["AbuseIPDB", "Maxmind"],
+      analyzers_requested: ["AbuseIPDB"],
       connectors_requested: [],
-      analyzers_to_execute: ["AbuseIPDB", "Maxmind"],
+      analyzers_to_execute: ["AbuseIPDB"],
       connectors_to_execute: [],
       visualizers_to_execute: [],
     };
 
     const country = extractCountry(job);
-    expect(country.countryCode).toBe("IT");
-    expect(country.countryName).toBe("Italy");
+    expect(country.countryCode).toBe("US");
+    expect(country.countryName).toBe("United States of America");
   });
 
-  test("AbuseIPBD with no data and Maxmind with no data", () => {
-    const job = {
-      id: 2,
-      analyzer_reports: [
-        {
-          name: "AbuseIPDB",
-          process_time: 0.07,
-          status: "SUCCESS",
-          report: {},
-        },
-        {
-          name: "Maxmind",
-          process_time: 0.07,
-          status: "SUCCESS",
-          end_time: "2024-01-15T14:56:32.328332Z",
-          report: {},
-        },
-      ],
-      connector_reports: [],
-      pivot_reports: [],
-      visualizer_reports: [],
-      is_sample: false,
-      md5: "",
-      observable_name: "1.1.1.1",
-      observable_classification: "ip",
-      file_name: "",
-      file_mimetype: "",
-      status: "reported_without_fails",
-      runtime_configuration: {},
-      received_request_time: "2024-01-15T14:56:32.019310Z",
-      finished_analysis_time: "2024-01-15T14:56:32.353326Z",
-      process_time: null,
-      tlp: "CLEAR",
-      errors: [],
-      warnings: [],
-      scan_mode: 2,
-      scan_check_time: "1 00:00:00",
-      sent_to_bi: false,
-      playbook_requested: "",
-      playbook_to_execute: "",
-      analyzers_requested: ["AbuseIPDB", "Maxmind"],
-      connectors_requested: [],
-      analyzers_to_execute: ["AbuseIPDB", "Maxmind"],
-      connectors_to_execute: [],
-      visualizers_to_execute: [],
-    };
-
-    const country = extractCountry(job);
-    expect(country.countryCode).toBe("");
-    expect(country.countryName).toBe("");
-  });
 });

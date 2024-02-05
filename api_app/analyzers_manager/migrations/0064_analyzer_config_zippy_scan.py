29283400cba34cae08b59d2725bccd5af4bb598b
0ssigeno <s.berni@certego.net>
2024-02-05 09:28:33 +0100

    Fix migration
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- api_app/analyzers_manager/migrations/0064_analyzer_config_zippy_scan.py
+++ api_app/analyzers_manager/migrations/0064_analyzer_config_zippy_scan.py
@@ -59,7 +59,7 @@ values = [
         "value": "ensembled",
         "updated_at": "2024-01-31T21:43:33.344103Z",
         "owner": None,
-        "analyzer_config": 151,
+        "analyzer_config": "Zippy_scan",
         "connector_config": None,
         "visualizer_config": None,
         "ingestor_config": None,
@@ -83,7 +83,10 @@ def _get_real_obj(Model, field, value):
             value = other_model.objects.get_or_create(**real_vals)[0]
         # it is just the primary key serialized
         else:
-            value = other_model.objects.get(pk=value)
+            if isinstance(value, int):
+                value = other_model.objects.get(pk=value)
+            else:
+                value = other_model.objects.get(name=value)
     return value
 
 

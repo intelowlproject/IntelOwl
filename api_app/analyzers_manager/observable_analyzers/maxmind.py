06341b18eeebf7acb53a3dc02a0eec7f0b82ce36
g4ze <bhaiyajionline17@gmail.com>
2024-02-12 05:04:33 +0530

    maxmind_update

--- api_app/analyzers_manager/observable_analyzers/maxmind.py
+++ api_app/analyzers_manager/observable_analyzers/maxmind.py
@@ -33,9 +33,7 @@ class Maxmind(classes.ObservableAnalyzer):
         for db in db_names:
             try:
                 db_location = _get_db_location(db)
-                if not os.path.isfile(db_location) and not self._update_db(
-                    db, self._api_key_name
-                ):
+                if not self._update_db(db, self._api_key_name):
                     raise AnalyzerRunException(
                         f"failed extraction of maxmind db {db},"
                         " reached max number of attempts"
@@ -79,64 +77,68 @@ class Maxmind(classes.ObservableAnalyzer):
 
         db_location = _get_db_location(db)
         try:
-            db_name_wo_ext = db[:-5]
-            logger.info(f"starting download of db {db_name_wo_ext} from maxmind")
-            url = (
-                "https://download.maxmind.com/app/geoip_download?edition_id="
-                f"{db_name_wo_ext}&license_key={api_key}&suffix=tar.gz"
-            )
-            r = requests.get(url)
-            if r.status_code >= 300:
-                raise AnalyzerRunException(
-                    f"failed request for new maxmind db {db_name_wo_ext}."
-                    f" Status code: {r.status_code}"
+            if (not os.path.isfile(db_location)) or (
+                datetime.datetime.now().timestamp() - os.path.getmtime(db_location)
+                > 86400
+            ):
+                db_name_wo_ext = db[:-5]
+                logger.info(f"starting download of db {db_name_wo_ext} from maxmind")
+                url = (
+                    "https://download.maxmind.com/app/geoip_download?edition_id="
+                    f"{db_name_wo_ext}&license_key={api_key}&suffix=tar.gz"
                 )
-
-            tar_db_path = f"/tmp/{db_name_wo_ext}.tar.gz"
-            with open(tar_db_path, "wb") as f:
-                f.write(r.content)  # lgtm [py/clear-text-storage-sensitive-data]
-
-            tf = tarfile.open(tar_db_path)
-            directory_to_extract_files = settings.MEDIA_ROOT
-            tf.extractall(str(directory_to_extract_files))
-
-            today = datetime.datetime.now().date()
-            counter = 0
-            directory_found = False
-            downloaded_db_path = ""
-            # this is because we do not know the exact date of the db we downloaded
-            while counter < 10 or not directory_found:
-                date_to_check = today - datetime.timedelta(days=counter)
-                formatted_date = date_to_check.strftime("%Y%m%d")
-                downloaded_db_path = (
-                    f"{directory_to_extract_files}/"
-                    f"{db_name_wo_ext}_{formatted_date}/{db}"
-                )
-                try:
-                    os.rename(downloaded_db_path, db_location)
-                except FileNotFoundError:
-                    logger.debug(
-                        f"{downloaded_db_path} not found move to the day before"
+                r = requests.get(url)
+                if r.status_code >= 300:
+                    raise AnalyzerRunException(
+                        f"failed request for new maxmind db {db_name_wo_ext}."
+                        f" Status code: {r.status_code}"
                     )
-                    counter += 1
-                else:
-                    directory_found = True
-                    shutil.rmtree(
+
+                tar_db_path = f"/tmp/{db_name_wo_ext}.tar.gz"
+                with open(tar_db_path, "wb") as f:
+                    f.write(r.content)  # lgtm [py/clear-text-storage-sensitive-data]
+
+                tf = tarfile.open(tar_db_path)
+                directory_to_extract_files = settings.MEDIA_ROOT
+                tf.extractall(str(directory_to_extract_files))
+
+                today = datetime.datetime.now().date()
+                counter = 0
+                directory_found = False
+                downloaded_db_path = ""
+                # this is because we do not know the exact date of the db we downloaded
+                while counter < 10 or not directory_found:
+                    date_to_check = today - datetime.timedelta(days=counter)
+                    formatted_date = date_to_check.strftime("%Y%m%d")
+                    downloaded_db_path = (
                         f"{directory_to_extract_files}/"
-                        f"{db_name_wo_ext}_{formatted_date}"
+                        f"{db_name_wo_ext}_{formatted_date}/{db}"
                     )
+                    try:
+                        os.rename(downloaded_db_path, db_location)
+                    except FileNotFoundError:
+                        logger.debug(
+                            f"{downloaded_db_path} not found move to the day before"
+                        )
+                        counter += 1
+                    else:
+                        directory_found = True
+                        shutil.rmtree(
+                            f"{directory_to_extract_files}/"
+                            f"{db_name_wo_ext}_{formatted_date}"
+                        )
 
-            if directory_found:
-                logger.info(f"maxmind directory found {downloaded_db_path}")
-            else:
-                return False
+                if directory_found:
+                    logger.info(f"maxmind directory found {downloaded_db_path}")
+                else:
+                    return False
 
-            logger.info(f"ended download of db {db_name_wo_ext} from maxmind")
-            return True
+                logger.info(f"ended download of db {db_name_wo_ext} from maxmind")
+                return True
 
         except Exception as e:
             logger.exception(e)
-        return False
+        return True
 
     @classmethod
     def update(cls) -> bool:

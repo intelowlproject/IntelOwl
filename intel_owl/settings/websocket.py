096687076dca612e5b68c5683034c70f14623c1d
0ssigeno <s.berni@certego.net>
2024-02-15 12:19:44 +0100

    Fixes
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- intel_owl/settings/websocket.py
+++ intel_owl/settings/websocket.py
@@ -1,14 +1,17 @@
 from intel_owl import secrets
+import socket
 
 websockets_url = secrets.get_secret("WEBSOCKETS_URL", None)
 if not websockets_url:
-    raise RuntimeError("Unable to configure websockets")
-ASGI_APPLICATION = "intel_owl.asgi.application"
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [f"{websockets_url}/0"],
+    if socket.gethostname() in ["uwsgi", "daphne"]:
+        raise RuntimeError("Unable to configure websockets")
+else:
+    ASGI_APPLICATION = "intel_owl.asgi.application"
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [f"{websockets_url}/0"],
+            },
         },
-    },
-}
+    }

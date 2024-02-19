e85be95d513a1ce9cc296c0e94239e562ec218cf
0ssigeno <s.berni@certego.net>
2024-02-19 10:49:27 +0100

    Celery autoreload
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- docker/entrypoints/celery_ingestor.sh
+++ docker/entrypoints/celery_ingestor.sh
@@ -4,5 +4,11 @@ until cd /opt/deploy/intel_owl
 do
     echo "Waiting for server volume..."
 done
-
-/usr/local/bin/celery -A intel_owl.celery worker -n worker_ingestor --uid www-data --gid www-data --time-limit=40000 --pidfile= -Ofair -Q ingestor,broadcast,config -E --autoscale=1,15 --without-gossip
+ARGUMENTS="-A intel_owl.celery worker -n worker_ingestor --uid www-data --gid www-data --time-limit=40000 --pidfile= -Ofair -Q ingestor,broadcast,config -E --autoscale=1,15 --without-gossip"
+if [[ $DEBUG == "True" ]] && [[ $DJANGO_TEST_SERVER == "True" ]];
+then
+    echo "Running celery with autoreload"
+    python3 manage.py celery_reload -c "$ARGUMENTS"
+else
+  /usr/local/bin/celery $ARGUMENTS
+fi
--- docker/entrypoints/celery_long.sh
+++ docker/entrypoints/celery_long.sh
@@ -4,5 +4,11 @@ until cd /opt/deploy/intel_owl
 do
     echo "Waiting for server volume..."
 done
-
-/usr/local/bin/celery -A intel_owl.celery worker -n worker_long --uid www-data --gid www-data --time-limit=40000 --pidfile= -Ofair -Q long,broadcast,config -E --without-gossip
+ARGUMENTS="-A intel_owl.celery worker -n worker_long --uid www-data --gid www-data --time-limit=40000 --pidfile= -Ofair -Q long,broadcast,config -E --without-gossip"
+if [[ $DEBUG == "True" ]] && [[ $DJANGO_TEST_SERVER == "True" ]];
+then
+    echo "Running celery with autoreload"
+    python3 manage.py celery_reload -c "$ARGUMENTS"
+else
+  /usr/local/bin/celery $ARGUMENTS
+fi

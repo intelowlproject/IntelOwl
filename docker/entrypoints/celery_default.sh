bf1cb60aac57ea9731705079f6e296b2ca49d741
0ssigeno <s.berni@certego.net>
2024-02-19 09:17:47 +0100

    Adjust for ubuntu 22
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- docker/entrypoints/celery_default.sh
+++ docker/entrypoints/celery_default.sh
@@ -4,5 +4,9 @@ until cd /opt/deploy/intel_owl
 do
     echo "Waiting for server volume..."
 done
-
-/usr/local/bin/celery -A intel_owl.celery worker -n worker_default --uid www-data --gid www-data --time-limit=10000 --pidfile= -Ofair -Q default,broadcast,config -E --without-gossip
+if [[ $DEBUG == "True" ]] && [[ $DJANGO_TEST_SERVER == "True" ]];
+then
+    /usr/local/bin/celery -A intel_owl.celery worker -n worker_default --uid www-data --gid www-data --time-limit=10000 --pidfile= -Ofair -Q default,broadcast,config -E --without-gossip
+else
+  /usr/local/bin/celery -A intel_owl.celery worker -n worker_default --uid www-data --gid www-data --time-limit=10000 --pidfile= -Ofair -Q default,broadcast,config -E --without-gossip
+fi

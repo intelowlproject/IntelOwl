48b3272e3ca325fb390a6855953cabcaae73865c
0ssigeno <s.berni@certego.net>
2024-02-19 10:49:32 +0100

    Celery autoreload
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- docker/entrypoints/celery_default.sh
+++ docker/entrypoints/celery_default.sh
@@ -4,9 +4,11 @@ until cd /opt/deploy/intel_owl
 do
     echo "Waiting for server volume..."
 done
+ARGUMENTS="-A intel_owl.celery worker -n worker_default --uid www-data --gid www-data --time-limit=10000 --pidfile= -Ofair -Q default,broadcast,config -E --without-gossip"
 if [[ $DEBUG == "True" ]] && [[ $DJANGO_TEST_SERVER == "True" ]];
 then
-    /usr/local/bin/celery -A intel_owl.celery worker -n worker_default --uid www-data --gid www-data --time-limit=10000 --pidfile= -Ofair -Q default,broadcast,config -E --without-gossip
+    echo "Running celery with autoreload"
+    python3 manage.py celery_reload -c "$ARGUMENTS"
 else
-  /usr/local/bin/celery -A intel_owl.celery worker -n worker_default --uid www-data --gid www-data --time-limit=10000 --pidfile= -Ofair -Q default,broadcast,config -E --without-gossip
+  /usr/local/bin/celery $ARGUMENTS
 fi

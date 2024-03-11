a972284736f95b3ffdd6386271db6d9faf8496a3
Daniele Rosetti <d.rosetti@certego.net>
2024-03-11 10:47:38 +0100

    removed redis cache setting

--- intel_owl/settings/cache.py
+++ intel_owl/settings/cache.py
@@ -35,8 +35,6 @@ class DatabaseCacheExtended(DatabaseCache):
         return self.get_many([row[0] for row in rows], version=version)
 
 
-DJANGO_REDIS_IGNORE_EXCEPTIONS = True
-
 CACHES = {
     "default": {
         "BACKEND": "intel_owl.settings.cache.DatabaseCacheExtended",

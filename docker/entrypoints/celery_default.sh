df7f1e7635a5ba1ca864ee7149a70e246835521c
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2024-03-19 13:01:49 +0100

    concurrency 1 for stage ci

--- docker/entrypoints/celery_default.sh
+++ docker/entrypoints/celery_default.sh
@@ -4,7 +4,17 @@ until cd /opt/deploy/intel_owl
 do
     echo "Waiting for server volume..."
 done
-ARGUMENTS="-A intel_owl.celery worker -n worker_default --uid www-data --gid www-data --time-limit=10000 --pidfile= -Ofair -Q default,broadcast,config -E --without-gossip"
+
+echo "environment: $STAGE"
+if [ "$STAGE" = "ci" ]
+then
+   worker_number=1
+else
+   # default is prod
+   worker_number=4
+fi
+
+ARGUMENTS="-A intel_owl.celery worker -n worker_default --uid www-data --gid www-data --time-limit=10000 --pidfile= -c $worker_number -Ofair -Q default,broadcast,config -E --without-gossip"
 if [[ $DEBUG == "True" ]] && [[ $DJANGO_TEST_SERVER == "True" ]];
 then
     echo "Running celery with autoreload"

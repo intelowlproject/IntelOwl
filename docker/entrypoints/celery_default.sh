fc8632d2fe163fdfea0ac31fd0ec3805001e0780
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2024-03-19 13:02:18 +0100

    concurrency 1 for stage ci

--- docker/entrypoints/celery_default.sh
+++ docker/entrypoints/celery_default.sh
@@ -11,7 +11,7 @@ then
    worker_number=1
 else
    # default is prod
-   worker_number=4
+   worker_number=8
 fi
 
 ARGUMENTS="-A intel_owl.celery worker -n worker_default --uid www-data --gid www-data --time-limit=10000 --pidfile= -c $worker_number -Ofair -Q default,broadcast,config -E --without-gossip"

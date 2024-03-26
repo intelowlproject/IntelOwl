8707f5eb66eca230abb8ecc9c3f0a5843f0315e3
0ssigeno <s.berni@certego.net>
2024-03-26 08:55:54 +0100

    Fix user without membership
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- api_app/investigations_manager/models.py
+++ api_app/investigations_manager/models.py
@@ -50,7 +50,7 @@ class Investigation(OwnershipAbstractModel, ListCachable):
             and (
                 user.has_membership()
                 and self.owner.has_membership()
-                and user.organization == self.owner.organization
+                and user.membership.organization == self.owner.mermbership.organization
             )
             # or same user
         ) or user == self.owner:

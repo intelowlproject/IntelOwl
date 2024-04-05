25405b103ba1774e84a0b0125d5569407698c2f4
Moon Patel <moonpatel2003@gmail.com>
2024-04-05 22:29:17 +0530

    added permhash analyzer and updated project-requirements.txt

---
+++ api_app/analyzers_manager/file_analyzers/permhash.py
@@ -0,0 +1,52 @@
+# This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
+# See the file 'LICENSE' for copying permission.
+
+import os
+
+from permhash import functions as permhash
+
+from api_app.analyzers_manager.classes import FileAnalyzer
+
+
+class Permhash(FileAnalyzer):
+    """
+    Create permissions hash of APK, Chrome extensions,
+    Android manifest and Chrome extension manifest files.
+    """
+
+    def run(self):
+        result = {}
+        _, file_extension = os.path.splitext(self.filepath)
+
+        file_extension = file_extension[1:]
+
+        hash_val = ""
+
+        if file_extension == "apk":
+            hash_val = permhash.permhash_apk(self.filepath)
+        elif file_extension == "xml":
+            hash_val = permhash.permhash_apk_manifest(self.filepath)
+        elif file_extension == "crx":
+            hash_val = permhash.permhash_crx(self.filepath)
+        elif file_extension == "json":
+            hash_val = permhash.permhash_crx_manifest(self.filepath)
+        else:
+            result["error"] = "Invalid file extension."
+
+        # permhash returns False if for some reason the hash value can't be found
+        if hash_val:
+            result["hash"] = hash_val
+        else:
+            result["error"] = "Could not find permissions in the file."
+
+        return result
+
+    @classmethod
+    def update(cls) -> bool:
+        pass
+
+    @classmethod
+    def _monkeypatch(cls):
+        patches = []
+
+        return super()._monkeypatch(patches=patches)

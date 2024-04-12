19c1db61a91438ed3abf5cf19aadfa357a591a7c
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2024-04-12 20:05:24 +0200

    support for Elastic8 and removal of support for older versions (#2262)
    * support for elastic 8
    
    * elastic8
    
    * elastic8

--- intel_owl/settings/elasticsearch.py
+++ intel_owl/settings/elasticsearch.py
@@ -1,56 +1,32 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
 
-# Elastic Search Configuration
-from ssl import create_default_context
-
 from elasticsearch import Elasticsearch
 
 from intel_owl import secrets
-from intel_owl.settings import CONFIG_ROOT
 
 ELASTICSEARCH_BI_ENABLED = (
     secrets.get_secret("ELASTICSEARCH_BI_ENABLED", False) == "True"
 )
 if ELASTICSEARCH_BI_ENABLED:
     ELASTICSEARCH_BI_HOST = secrets.get_secret("ELASTICSEARCH_BI_HOST").split(",")
-    ELASTICSEARCH_SSL_CERTIFICATE_PATH = CONFIG_ROOT / secrets.get_secret(
-        "ELASTICSEARCH_SSL_CERTIFICATE_FILE_NAME", "elasticsearch.crt"
-    )
     ELASTICSEARCH_BI_INDEX = secrets.get_secret("ELASTICSEARCH_BI_INDEX")
-    if (
-        not ELASTICSEARCH_BI_HOST
-        or not ELASTICSEARCH_SSL_CERTIFICATE_PATH
-        or not ELASTICSEARCH_BI_INDEX
-    ):
-        print("Elasticsearch not correctly configured")
-    elif not ELASTICSEARCH_SSL_CERTIFICATE_PATH.exists():
-        print(
-            f"Elasticsearch certificate {ELASTICSEARCH_SSL_CERTIFICATE_PATH}"
-            " not found"
-        )
-    else:
-        elastic_ssl_context = create_default_context(
-            cafile=str(ELASTICSEARCH_SSL_CERTIFICATE_PATH)
-        )
-        from importlib.metadata import version
 
-        v = version("elasticsearch")
-        major_version = int(v.split(".")[0])
-        if major_version < 8:
-            ELASTICSEARCH_CLIENT = Elasticsearch(
-                ELASTICSEARCH_BI_HOST,
-                ssl_context=elastic_ssl_context,
-                scheme="https",
-                maxsize=20,
-                max_retries=10,
-                retry_on_timeout=True,
-                timeout=30,
-                sniff_on_connection_fail=True,
-                sniff_timeout=30,
-            )
-        else:
-            raise RuntimeError(f"Elastic version {v} is not supported at the moment.")
+    if not ELASTICSEARCH_BI_HOST or not ELASTICSEARCH_BI_INDEX:
+        print("Elasticsearch not correctly configured")
+
+    else:
+        ELASTICSEARCH_CLIENT = Elasticsearch(
+            ELASTICSEARCH_BI_HOST,
+            maxsize=20,
+            max_retries=10,
+            retry_on_timeout=True,
+            timeout=30,
+            sniff_on_connection_fail=True,
+            sniff_timeout=30,
+        )
+        if not ELASTICSEARCH_CLIENT.ping():
+            print("ELASTICSEARCH client configuration did not connect correctly")
 
 ELASTICSEARCH_DSL_ENABLED = (
     secrets.get_secret("ELASTICSEARCH_DSL_ENABLED", False) == "True"

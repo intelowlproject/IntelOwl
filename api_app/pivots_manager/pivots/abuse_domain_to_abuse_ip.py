9f2a23674ed61fe2ea9a521171ad66191fdbcff8
Cristina Ascari <c.ascari@certego.net>
2024-04-17 11:42:11 +0200

    updated AbuseDomainToIp should_run

--- api_app/pivots_manager/pivots/abuse_domain_to_abuse_ip.py
+++ api_app/pivots_manager/pivots/abuse_domain_to_abuse_ip.py
@@ -2,25 +2,27 @@ import logging
 from typing import Any, Optional, Tuple
 
 from api_app.pivots_manager.classes import Pivot
+from api_app.playbooks_manager.models import PlaybookConfig
 
 logger = logging.getLogger(__name__)
 
 
 class AbuseDomainToAbuseIp(Pivot):
     def should_run(self) -> Tuple[bool, Optional[str]]:
-        for x in self.related_reports:
-            if (
-                x.status == self.report_model.Status.SUCCESS.value
-                and len(x.report["resolutions"]) > 0
-            ):
-                value = x.report["resolutions"][0]["data"]
-                if value:
-                    self.value = value
-                    result = True
-                    break
-        else:
-            self.value = None
-            result = False
+        playbook = PlaybookConfig.objects.filter(name="Abuse_Domain").first()
+        self.value = None
+        result = False
+        if self._job.playbook_requested == playbook:
+            for x in self.related_reports:
+                if (
+                    x.status == self.report_model.Status.SUCCESS.value
+                    and len(x.report["resolutions"]) > 0
+                ):
+                    value = x.report["resolutions"][0]["data"]
+                    if value:
+                        self.value = value
+                        result = True
+                        break
         return (
             result,
             f"Necessary reports{'' if result else ' do not'} have success status "

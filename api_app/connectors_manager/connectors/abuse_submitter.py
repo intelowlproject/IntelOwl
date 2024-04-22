e46f3787e8ef99c98d92146130a431558322a92b
Cristina Ascari <c.ascari@certego.net>
2024-04-22 16:39:08 +0200

    updates email sender and abuse submitter

---
+++ api_app/connectors_manager/connectors/abuse_submitter.py
@@ -0,0 +1,14 @@
+from api_app.connectors_manager.connectors.email_sender import EmailSender
+
+
+class AbuseSubmitter(EmailSender):
+    @property
+    def subject(self) -> str:
+        return f"Take down domain {self._job.parent_job.parent_job.observable_name}"
+
+    @property
+    def body(self) -> str:
+        return (
+            f"Domain {self._job.parent_job.parent_job.observable_name} "
+            f"has been reported as malicious. We request you to take it down."
+        )

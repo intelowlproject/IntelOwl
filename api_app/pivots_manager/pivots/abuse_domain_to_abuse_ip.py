53b27b7e9ecefad2218daed072436c7fa864619a
Cristina Ascari <95929371+cristinaascari@users.noreply.github.com>
2024-04-24 10:54:13 +0200

    Update should_run
    Co-authored-by: Simone Berni <simone.berni2@studio.unibo.it>

--- api_app/pivots_manager/pivots/abuse_domain_to_abuse_ip.py
+++ api_app/pivots_manager/pivots/abuse_domain_to_abuse_ip.py
@@ -9,25 +9,9 @@ logger = logging.getLogger(__name__)
 
 class AbuseDomainToAbuseIp(Pivot):
     def should_run(self) -> Tuple[bool, Optional[str]]:
-        playbook = PlaybookConfig.objects.filter(name="Abuse_Domain").first()
-        self.value = None
-        result = False
-        if self._job.playbook_requested == playbook:
-            for x in self.related_reports:
-                if (
-                    x.status == self.report_model.Status.SUCCESS.value
-                    and len(x.report["resolutions"]) > 0
-                ):
-                    value = x.report["resolutions"][0]["data"]
-                    if value:
-                        self.value = value
-                        result = True
-                        break
-        return (
-            result,
-            f"Necessary reports{'' if result else ' do not'} have success status "
-            f"{'and' if result else 'or'} needed data",
-        )
+        if valid_report := self.related_reports.filter(status=self.report_model.Status.SUCCESS.value).first():
+            self._value = valid_report.report["resolutions"] # etc
+             result = True
 
     def get_value_to_pivot_to(self) -> Any:
         return self.value

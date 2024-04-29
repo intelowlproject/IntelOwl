eae53696e4c770a6938f06bf48b710024e4633d8
Cristina Ascari <c.ascari@certego.net>
2024-04-29 15:43:17 +0200

    Fix linters

--- api_app/connectors_manager/connectors/abuse_submitter.py
+++ api_app/connectors_manager/connectors/abuse_submitter.py
@@ -4,11 +4,15 @@ from api_app.connectors_manager.connectors.email_sender import EmailSender
 class AbuseSubmitter(EmailSender):
     @property
     def subject(self) -> str:
-        return f"Takedown domain request for {self._job.parent_job.parent_job.observable_name}"
+        return (
+            "Takedown domain request for "
+            f"{self._job.parent_job.parent_job.observable_name}"
+        )
 
     @property
     def body(self) -> str:
         return (
             f"Domain {self._job.parent_job.parent_job.observable_name} "
-            "has been detected as malicious by our team. We kindly request you to take it down as soon as possible."
+            "has been detected as malicious by our team. We kindly request you to take "
+            "it down as soon as possible."
         )

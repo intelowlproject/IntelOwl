b877e19aa950cea0b52cf0af3707415151d727de
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2024-05-06 17:22:54 +0200

    fixed migration

--- api_app/analyzers_manager/migrations/0082_analyzer_config_ip2whois.py
+++ api_app/analyzers_manager/migrations/0082_analyzer_config_ip2whois.py
@@ -6,40 +6,42 @@ from django.db.models.fields.related_descriptors import (
 )
 
 plugin = {
-    'python_module': {
-        'health_check_schedule': None,
-        'update_schedule': None,
-        'module': 'ip2whois.Ip2whois',
-        'base_path': 'api_app.analyzers_manager.observable_analyzers'
+    "python_module": {
+        "health_check_schedule": None,
+        "update_schedule": None,
+        "module": "ip2whois.Ip2whois",
+        "base_path": "api_app.analyzers_manager.observable_analyzers",
     },
-    'name': 'IP2WHOIS',
-    'description': '[API Docs](https://www.ip2location.io/ip2whois-documentation) IP2Location.io IP2WHOIS Domain WHOIS API helps users to obtain domain information and WHOIS record by using a domain name.',
-    'disabled': False,
-    'soft_time_limit': 60,
-    'routing_key': 'default',
-    'health_check_status': True,
-    'type': 'observable',
-    'docker_based': False,
-    'maximum_tlp': 'RED',
-    'observable_supported': ['domain'],
-    'supported_filetypes': [],
-    'run_hash': False,
-    'run_hash_type': '',
-    'not_supported_filetypes': [],
-    'model': 'analyzers_manager.AnalyzerConfig'
-    }
+    "name": "IP2WHOIS",
+    "description": "[API Docs](https://www.ip2location.io/ip2whois-documentation)"
+    " IP2Location.io IP2WHOIS Domain WHOIS API helps users to obtain"
+    " domain information and WHOIS record by using a domain name.",
+    "disabled": False,
+    "soft_time_limit": 60,
+    "routing_key": "default",
+    "health_check_status": True,
+    "type": "observable",
+    "docker_based": False,
+    "maximum_tlp": "RED",
+    "observable_supported": ["domain"],
+    "supported_filetypes": [],
+    "run_hash": False,
+    "run_hash_type": "",
+    "not_supported_filetypes": [],
+    "model": "analyzers_manager.AnalyzerConfig",
+}
 
 params = [
     {
-        'python_module': {
-            'module': 'ip2whois.Ip2whois',
-            'base_path': 'api_app.analyzers_manager.observable_analyzers'
+        "python_module": {
+            "module": "ip2whois.Ip2whois",
+            "base_path": "api_app.analyzers_manager.observable_analyzers",
         },
-        'name': 'api_key_name',
-        'type': 'str',
-        'description': 'IP2WHOIS API key name.',
-        'is_secret': True,
-        'required': True
+        "name": "api_key_name",
+        "type": "str",
+        "description": "IP2WHOIS API key name.",
+        "is_secret": True,
+        "required": True,
     }
 ]
 
@@ -76,6 +78,7 @@ def _get_real_obj(Model, field, value):
         value = [_get_obj(Model, other_model, val) for val in value]
     return value
 
+
 def _create_object(Model, data):
     mtm, no_mtm = {}, {}
     for field, value in data.items():
@@ -96,10 +99,11 @@ def _create_object(Model, data):
                 attribute.set(value)
         return False
     return True
-    
+
+
 def migrate(apps, schema_editor):
     Parameter = apps.get_model("api_app", "Parameter")
-    PluginConfig = apps.get_model("api_app", "PluginConfig")    
+    PluginConfig = apps.get_model("api_app", "PluginConfig")
     python_path = plugin.pop("model")
     Model = apps.get_model(*python_path.split("."))
     if not Model.objects.filter(name=plugin["name"]).exists():
@@ -111,25 +115,17 @@ def migrate(apps, schema_editor):
                 _create_object(PluginConfig, value)
 
 
-
 def reverse_migrate(apps, schema_editor):
     python_path = plugin.pop("model")
     Model = apps.get_model(*python_path.split("."))
     Model.objects.get(name=plugin["name"]).delete()
 
 
-
 class Migration(migrations.Migration):
     atomic = False
     dependencies = [
-        ('api_app', '0062_alter_parameter_python_module'),
-        ("analyzers_manager", "0080_remove_dns0_names_analyzer"),
+        ("api_app", "0062_alter_parameter_python_module"),
+        ("analyzers_manager", "0081_adjust_abusix"),
     ]
 
-    operations = [
-        migrations.RunPython(
-            migrate, reverse_migrate
-        )
-    ]
-        
-        
+    operations = [migrations.RunPython(migrate, reverse_migrate)]

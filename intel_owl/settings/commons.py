94188d1068e92a5d792c527651dc933217adc0d5
0ssigeno <s.berni@certego.net>
2024-05-06 11:48:32 +0200

    Removed import
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- intel_owl/settings/commons.py
+++ intel_owl/settings/commons.py
@@ -1,6 +1,5 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
-import os
 from pathlib import Path, PosixPath
 
 from ._util import get_secret

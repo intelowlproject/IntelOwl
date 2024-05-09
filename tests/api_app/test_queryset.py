f84364ce5a39c0af09d3af0c65d78e6439905f33
0ssigeno <s.berni@certego.net>
2024-05-09 17:52:12 +0200

    Fix + test for runtime config
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- tests/api_app/test_queryset.py
+++ tests/api_app/test_queryset.py
@@ -276,6 +276,11 @@ class ParameterQuerySetTestCase(CustomTestCase):
 
         # user value
         self.assertEqual(param.value, "myperfecttest1")
+        param = Parameter.objects.annotate_value_for_user(ac, self.user, {param.name: "runtime_test"}).get(
+            pk=param.pk
+        )
+        print(param.runtime_value)
+        self.assertEqual(param.value, "runtime_test")
 
         pc.delete()
         pc2.delete()

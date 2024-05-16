b5c1a3e51b727f2d467bb62cc1c1bb643e93c684
0ssigeno <s.berni@certego.net>
2024-05-16 16:18:55 +0200

    Phoneinfoga
    Signed-off-by: 0ssigeno <s.berni@certego.net>

--- api_app/analyzers_manager/observable_analyzers/phoneinfoga_scan.py
+++ api_app/analyzers_manager/observable_analyzers/phoneinfoga_scan.py
@@ -1,11 +1,25 @@
 import logging
+from enum import Enum
+from typing import Dict, List
 
 import requests
 
 from api_app.analyzers_manager import classes
+from api_app.analyzers_manager.exceptions import AnalyzerConfigurationException
 from tests.mock_utils import MockUpResponse
 
 logger = logging.getLogger(__name__)
+
+
+class SCANNER_NAMES(Enum):
+    LOCAL = "local"
+    NUM_VERIFY = "numverify"
+    GOOGLECSE = "googlecse"
+    OVH = "ovh"
+
+    @classmethod
+    def values(cls):
+        return list(map(lambda c: c.value, cls))
 
 
 class Phoneinfoga(classes.ObservableAnalyzer, classes.DockerBasedAnalyzer):
@@ -17,7 +31,8 @@ class Phoneinfoga(classes.ObservableAnalyzer, classes.DockerBasedAnalyzer):
         pass
 
     observable_name: str
-    scanner_name: str
+    scanners: List[str]
+    googlecse_max_results: int = 10
     name: str = "phoneinfoga"
     # here is a list of pre declared api keys, user can put
     # values as per their required scanner, by default it is null
@@ -25,27 +40,41 @@ class Phoneinfoga(classes.ObservableAnalyzer, classes.DockerBasedAnalyzer):
     _NUMVERIFY_API_KEY: str = ""
     _GOOGLECSE_CX: str = ""
     _GOOGLE_API_KEY: str = ""
+
     url = "http://phoneinfoga:5000"
 
+    def config(self, runtime_configuration: Dict):
+        super().config(runtime_configuration)
+        for scanner in self.scanners:
+            if scanner not in SCANNER_NAMES.values():
+                raise AnalyzerConfigurationException(
+                    f"Scanner {scanner} not supported. Choices are {', '.join(SCANNER_NAMES.values())}"
+                )
+
     def run(self):
-        url: str = f"{self.url}/api/v2/scanners/{self.scanner_name}/run"
-        response = requests.post(
-            url,
-            headers={
-                "Content-Type": "application/json",
-                "accept": "application/json",
-            },
-            json={
-                "number": self.observable_name,
-                "options": {
-                    "NUMVERIFY_API_KEY": self._NUMVERIFY_API_KEY,
+        result = {}
+        for scanner in self.scanners:
+            url: str = f"{self.url}/api/v2/scanners/{scanner}/run"
+            options = {}
+            if scanner == SCANNER_NAMES.NUM_VERIFY.value:
+                options["NUMVERIFY_API_KEY"] = self._NUMVERIFY_API_KEY
+            elif scanner == SCANNER_NAMES.GOOGLECSE.value:
+                options = {
                     "GOOGLECSE_CX": self._GOOGLECSE_CX,
                     "GOOGLE_API_KEY": self._GOOGLE_API_KEY,
+                    "GOOGLECSE_MAX_RESULTS": self.googlecse_max_results,
+                }
+            response = requests.post(
+                url,
+                headers={
+                    "Content-Type": "application/json",
+                    "accept": "application/json",
                 },
-            },
-        )
-        response.raise_for_status()
-        return response.json()
+                json={"number": self.observable_name, "options": options},
+            )
+            response.raise_for_status()
+            result[scanner] = response.json()
+        return result
 
     @staticmethod
     def mocked_docker_analyzer_post(*args, **kwargs):

a098cc6eaf9e5068841ef9b25ffd61003efaecb5
g4ze <bhaiyajionline@gmail.com>
2024-05-18 05:09:12 +0530

    add params

--- api_app/analyzers_manager/observable_analyzers/hudsonrock.py
+++ api_app/analyzers_manager/observable_analyzers/hudsonrock.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 import requests
 
@@ -14,11 +15,40 @@ class HudsonRock(classes.ObservableAnalyzer):
     This analyzer is a wrapper for hudson rock
     """
 
-    compromised_since: str
-    compromised_until: str
+    compromised_since: str = None  # for IP/DOMAIN
+    compromised_until: str = None  # for IP/DOMAIN
+    page: int = None  # for IP/LOGIN/DOMAIN
+    added_since: str = None  # for IP/DOMAIN
+    added_until: str = None  # for IP/DOMAIN
+    installed_software: bool = None  # for IP/LOGIN/DOMAIN
+    sort_by: str = None  # for LOGIN
+    domain_cred_type: str = None  # for DOMAIN
+    domain_filtered: str = None  # for DOMAIN
+    third_party_domains: bool = None  # for DOMAIN
+
     _api_key_name: str
 
     url = "https://cavalier.hudsonrock.com/api/json/v2"
+
+    def get_param_url(self, valid_params):
+        param_url = ""
+        params = {
+            "compromised_since": self.compromised_since,
+            "compromised_until": self.compromised_until,
+            "page": self.page,
+            "added_since": self.added_since,
+            "added_until": self.added_until,
+            "installed_software": self.installed_software,
+            "sortby": self.sort_by,
+            "type": self.domain_cred_type,
+            "filtered": self.domain_filtered,
+            "third_party_domains": self.third_party_domains,
+        }
+        for param, value in params.items():
+            if param in valid_params and value:
+                param_url += f"&{param}={value}"
+
+        return "?" + param_url
 
     def run(self):
         response = {}
@@ -27,7 +57,20 @@ class HudsonRock(classes.ObservableAnalyzer):
             "Content-Type": "application/json",
         }
         if self.observable_classification == self.ObservableTypes.IP:
-            url = self.url + "/search-by-ip"
+            url = (
+                self.url
+                + "/search-by-ip"
+                + self.get_param_url(
+                    [
+                        "compromised_since",
+                        "compromised_until",
+                        "page",
+                        "added_since",
+                        "added_until",
+                        "installed_software",
+                    ]
+                )
+            )
             response = requests.post(
                 url, headers=headers, json={"ip": self.observable_name}
             )
@@ -35,18 +78,36 @@ class HudsonRock(classes.ObservableAnalyzer):
         elif self.observable_classification == self.ObservableTypes.DOMAIN:
             url = (
                 self.url
-                + f"/search-by-domain?compromised_since={self.compromised_since}"
-                + f"&compromised_until={self.compromised_until}"
+                + "/search-by-domain"
+                + self.get_param_url(
+                    [
+                        "compromised_since",
+                        "compromised_until",
+                        "page",
+                        "added_since",
+                        "added_until",
+                        "installed_software" "type",
+                        "filtered",
+                        "third_party_domains",
+                    ]
+                )
             )
             response = requests.post(
                 url, headers=headers, json={"domains": [self.observable_name]}
             )
 
         elif self.observable_classification == self.ObservableTypes.GENERIC:
-            url = self.url + "/search-by-login"
-            response = requests.post(
-                url, headers=headers, json={"login": self.observable_name}
-            )
+            # checking for email
+            regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
+            if re.fullmatch(regex, self.observable_name):
+                url = (
+                    self.url
+                    + "/search-by-login"
+                    + self.get_param_url(["sortby", "page", "installed_software"])
+                )
+                response = requests.post(
+                    url, headers=headers, json={"login": self.observable_name}
+                )
         else:
             raise AnalyzerConfigurationException(
                 f"Invalid observable type {self.observable_classification}"

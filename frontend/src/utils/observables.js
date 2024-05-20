dde1dfd64c6b13c3ef637f501652a01a8437e3b1
Daniele Rosetti <55402684+drosetti@users.noreply.github.com>
2024-05-20 17:53:43 +0200

    Fixes frontend regex (#2329)
    * support phone numbers
    
    * moved phone number validation to E.164 format
    
    * removed dates from parsing as IP addresses
    
    * prettier

--- frontend/src/utils/observables.js
+++ frontend/src/utils/observables.js
@@ -3,6 +3,7 @@ import {
   IP_REGEX,
   URL_REGEX,
   HASH_REGEX,
+  PHONE_REGEX,
 } from "../constants/regexConst";
 import {
   ObservableClassifications,
@@ -10,32 +11,44 @@ import {
   InvalidTLD,
 } from "../constants/jobConst";
 
-const observableType2RegExMap = {
-  domain: DOMAIN_REGEX,
-  ip: IP_REGEX,
-  url: URL_REGEX,
-  hash: HASH_REGEX,
+// IMPORTANT, order matters! phone and date must be checked after ip or the numbers will be taken as IP addesses
+const observableType2Validation = {
+  domain: (obs) => DOMAIN_REGEX.test(obs),
+  ip: (obs) => IP_REGEX.test(obs),
+  phone: (obs) => PHONE_REGEX.test(obs),
+  date: (obs) => new Date(obs).toString() !== "Invalid Date",
+  url: (obs) => URL_REGEX.test(obs),
+  hash: (obs) => HASH_REGEX.test(obs),
 };
 
 /* Remove [] inside the string, remove any no-word characters at the end of the string */
 export function sanitizeObservable(observable) {
-  return observable
-    .replaceAll("[", "")
-    .replaceAll("]", "")
-    .trim()
-    .replace(/\W*$/, "")
-    .replace(/^\W/, "");
+  return (
+    observable
+      .replaceAll("[", "")
+      .replaceAll("]", "")
+      .trim()
+      .replace(/\W*$/, "")
+      /* ignore + at the start of the string to support phone number:
+    this could lead to a match problem in the loading observable feature */
+      .replace(/^(?!\+)\W/, "")
+  );
 }
 
 export function observableValidators(stringToValidate) {
   const sanitizedString = sanitizeObservable(stringToValidate);
 
   let stringClassification = "";
-  Object.entries(observableType2RegExMap).forEach(([typeName, typeRegEx]) => {
-    if (typeRegEx.test(sanitizedString)) {
-      stringClassification = typeName;
-    }
-  });
+  Object.entries(observableType2Validation).forEach(
+    ([typeName, typeValidationFunction]) => {
+      const validType = typeValidationFunction(sanitizedString);
+      if (validType) {
+        stringClassification = typeName;
+        if (stringClassification === "phone") stringClassification = null;
+        if (stringClassification === "date") stringClassification = null;
+      }
+    },
+  );
 
   // domain
   if (stringClassification === ObservableClassifications.DOMAIN) {

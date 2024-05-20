7a9a84a8ad5d8079e2904ef02716945196ac78c2
g4ze <bhaiyajionline@gmail.com>
2024-05-20 16:29:17 +0530

    cycat

--- api_app/analyzers_manager/observable_analyzers/cycat.py
+++ api_app/analyzers_manager/observable_analyzers/cycat.py
@@ -9,7 +9,7 @@ from tests.mock_utils import MockUpResponse, if_mock_connections, patch
 logger = logging.getLogger(__name__)
 
 
-class MmdbServer(classes.ObservableAnalyzer):
+class CyCat(classes.ObservableAnalyzer):
     """
     This analyzer is a wrapper for cycat api.
     """
@@ -19,23 +19,34 @@ class MmdbServer(classes.ObservableAnalyzer):
 
     url: str = "https://api.cycat.org"
 
+    def uuid_lookup(self, uuid: str):
+        logger.info(f"performing lookup on uuid: {uuid}")
+        response = requests.get(
+            self.url + "/lookup/" + uuid,
+            headers={"accept": "application/json"},
+        )
+        response.raise_for_status()
+        return response
+
     def run(self):
+        final_response = {}
         uuid_pattern = re.compile(
             r"\b[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\b",
             re.IGNORECASE,
         )
         if uuid_pattern.match(self.observable_name):
-            response = requests.get(
-                self.url + "/lookup/" + self.observable_name,
-                headers={"accept": "application/json"},
-            )
+            final_response = self.uuid_lookup(self.observable_name)
+
         else:
             response = requests.get(
                 self.url + "/search/" + self.observable_name,
                 headers={"accept": "application/json"},
             )
-        response.raise_for_status()
-        return response.json()
+            response.raise_for_status()
+            response = response.json()
+            for uuid in response:
+                final_response[uuid] = self.uuid_lookup(uuid)
+        return final_response
 
     @classmethod
     def _monkeypatch(cls):

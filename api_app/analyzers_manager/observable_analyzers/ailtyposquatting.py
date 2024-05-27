bf20a3810df2e4a07a65cf97fb168320e48eee47
g4ze <bhaiyajionline@gmail.com>
2024-05-27 21:02:39 +0530

    no files

--- api_app/analyzers_manager/observable_analyzers/ailtyposquatting.py
+++ api_app/analyzers_manager/observable_analyzers/ailtyposquatting.py
@@ -1,14 +1,10 @@
 import logging
 import math
-import os
-import shutil
 
 from ail_typo_squatting import runAll
 from ail_typo_squatting.dns_local.resolving import dnsResolving
-from django.conf import settings
 
 from api_app.analyzers_manager import classes
-from intel_owl.settings._util import set_permissions
 
 logger = logging.getLogger(__name__)
 
@@ -24,28 +20,20 @@ class AilTypoSquatting(classes.ObservableAnalyzer):
         pass
 
     def run(self):
-        reports_dir = settings.AILTYPO_REPORTS_PATH / f"ailtypo_{self.observable_name}"
-
         response = {}
-        os.mkdir(reports_dir)
-        set_permissions(reports_dir)
 
         resultList = []
         resultList = runAll(
             domain=self.observable_name,
             limit=math.inf,
             formatoutput="yara",
-            pathOutput=reports_dir,
-            verbose=False,
-            givevariations=False,
-            keeporiginal=False,
+            pathOutput=None,
         )
         response["algorithms"] = resultList
 
         if self._config.maximum_tlp == "CLEAR" and self.dns_resolving:
             response["dnsResolving"] = dnsResolving(
-                resultList, domain=self.observable_name, pathOutput=reports_dir
+                resultList, domain=self.observable_name, pathOutput=None
             )
 
-        shutil.rmtree(reports_dir)
         return response

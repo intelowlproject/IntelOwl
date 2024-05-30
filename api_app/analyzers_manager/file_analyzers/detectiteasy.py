9fc11c64a1e446489b0ea5d1f3c52c1b06a2ae3b
g4ze <bhaiyajionline@gmail.com>
2024-05-31 03:40:00 +0530

    tweeks

--- api_app/analyzers_manager/file_analyzers/detectiteasy.py
+++ api_app/analyzers_manager/file_analyzers/detectiteasy.py
@@ -1,4 +1,8 @@
+import logging
+
 from api_app.analyzers_manager.classes import DockerBasedAnalyzer, FileAnalyzer
+
+logger = logging.getLogger(__name__)
 
 
 class DetectItEasy(FileAnalyzer, DockerBasedAnalyzer):
@@ -10,18 +14,15 @@ class DetectItEasy(FileAnalyzer, DockerBasedAnalyzer):
     poll_distance: int = 3
 
     def run(self):
-        # construct a valid filename into which thug will save the result
         fname = str(self.filename).replace("/", "_").replace(" ", "_")
         # get the file to send
         binary = self.read_file_bytes()
-        # construct arguments, For example this corresponds to,
-        # apkid -j file.apk
         args = [f"@{fname}", "--json"]
         req_data = {
             "args": args,
         }
         req_files = {fname: binary}
-
+        logger.info(f"Running {self.analyzer_name} with args: {args}")
         report = self._docker_run(req_data, req_files, analyzer_name=self.analyzer_name)
         if not report:
             self.report.errors.append("DIE does not support the file")

aecfefcf5e7a226fc2cdc29c9d2f8ad6e4028de4
g4ze <bhaiyajionline@gmail.com>
2024-06-05 14:55:25 +0530

    bad query

--- api_app/analyzers_manager/observable_analyzers/dns/dns_malicious_detectors/adguard.py
+++ api_app/analyzers_manager/observable_analyzers/dns/dns_malicious_detectors/adguard.py
@@ -1,25 +1,15 @@
-import base64
 import logging
-from typing import Tuple
 from urllib.parse import urlparse
 
-import dns.message
 import requests
 
 from api_app.analyzers_manager import classes
-from tests.mock_utils import MockUpResponse, if_mock_connections, patch
-
-from ..dns_responses import malicious_detector_response
 
 logger = logging.getLogger(__name__)
 
 
 class AdGuard(classes.ObservableAnalyzer):
-    """Check if a domain is malicious by AdGuard public resolver.
-    AdGuard does not answer in the case a malicious domain is queried.
-    However, we need to perform another check to understand if that domain was blocked
-    by the resolver or if it just does not exist.
-    """
+    """Check if a domain is malicious by AdGuard public resolver."""
 
     HEADERS = {"Accept": "application/dns-json"}
 
@@ -34,21 +24,16 @@ class AdGuard(classes.ObservableAnalyzer):
         # for URLs we are checking the relative domain
         if self.observable_classification == self.ObservableTypes.URL:
             observable = urlparse(self.observable_name).hostname
-
-        query = dns.message.make_query("example.com", dns.rdatatype.A)
-        query_wire = query.to_wire()
-        query_base64url = base64.urlsafe_b64encode(query_wire).rstrip(b"=")
-
         params = {"dns": observable}
-        response = requests.get(
-            f"https://unfiltered.adguard-dns.com/dns-query?dns={query_base64url.decode()}",
+        response1 = requests.get(
+            "https://unfiltered.adguard-dns.com/dns-query",
+            params=params,
             headers={"Accept": "application/dns-json"},
         )
         response2 = requests.get(
-            f"https://dns.adguard-dns.com/dns-query?dns={query_base64url.decode()}",
+            "https://dns.adguard-dns.com/dns-query?",
+            params=params,
             headers={"Accept": "application/dns-json"},
         )
-        logger.info(f"AdGuard responsee: {response.text}")
-        logger.info(f"AdGuard responsee2: {response2.text}")
 
-        return (response.text, response2.text)
+        return {"response1": response1.text, "response2": response2.text}

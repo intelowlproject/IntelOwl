f877522ca85ac025c2e76e24af7e014036c23eac
g4ze <bhaiyajionline@gmail.com>
2024-06-13 17:45:15 +0530

    better code

--- api_app/analyzers_manager/observable_analyzers/dns/dns_malicious_detectors/spamhaus_wqs.py
+++ api_app/analyzers_manager/observable_analyzers/dns/dns_malicious_detectors/spamhaus_wqs.py
@@ -3,6 +3,7 @@ import logging
 import requests
 
 from api_app.analyzers_manager import classes
+from api_app.analyzers_manager.exceptions import AnalyzerRunException
 from tests.mock_utils import MockUpResponse, if_mock_connections, patch
 
 from ..dns_responses import malicious_detector_response
@@ -21,24 +22,23 @@ class SpamhausWQS(classes.ObservableAnalyzer):
         headers = {"Authorization": f"Bearer {self._api_key}"}
 
         if self.observable_classification == self.ObservableTypes.DOMAIN.value:
-            if len(self.observable_name.split(".")) > 2:
-                # "www.google.xyz.yzx..." to "google"
-                self.observable_name = self.observable_name.split(".")[1]
-            else:
-                # "google.com" to "google" or "google" to "google"
-                self.observable_name = self.observable_name.split(".")[0]
             response = requests.get(
                 url=f"{self.url}/DBL/{self.observable_name}", headers=headers
             )
-        if self.observable_classification == self.ObservableTypes.IP.value:
+        elif self.observable_classification == self.ObservableTypes.IP.value:
             response = requests.get(
                 url=f"{self.url}/AUTHBL/{self.observable_name}", headers=headers
             )
-        if response.json()["status"] == 404:
+
+        response = response.json()
+        if response["status"] == 200:
+            # 200 - Found - The record is listed
+            return malicious_detector_response(self.observable_name, True)
+        elif response["status"] == 404:
             # 404 - Not found - The record is not listed
             return malicious_detector_response(self.observable_name, False)
-        response.raise_for_status()
-        return malicious_detector_response(self.observable_name, True)
+        else:
+            raise AnalyzerRunException(f"result not expected: {response}")
 
     @classmethod
     def _monkeypatch(cls):

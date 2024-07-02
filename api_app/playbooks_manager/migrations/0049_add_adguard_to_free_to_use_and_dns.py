18d5bff73af02548a19325bc09de3ef0f6589dbb
g4ze <bhaiyajionline@gmail.com>
2024-07-02 15:32:42 +0530

    mign

--- api_app/playbooks_manager/migrations/0049_add_adguard_to_free_to_use_and_dns.py
+++ api_app/playbooks_manager/migrations/0049_add_adguard_to_free_to_use_and_dns.py
@@ -9,18 +9,26 @@ def migrate(apps, schema_editor):
     playbook_config = apps.get_model("playbooks_manager", "PlaybookConfig")
     AnalyzerConfig = apps.get_model("analyzers_manager", "AnalyzerConfig")
     pc = playbook_config.objects.get(name="FREE_TO_USE_ANALYZERS")
+    pc2 = playbook_config.objects.get(name="Dns")
     pc.analyzers.add(AnalyzerConfig.objects.get(name="AdGuard").id)
+    pc2.analyzers.add(AnalyzerConfig.objects.get(name="AdGuard").id)
     pc.full_clean()
     pc.save()
+    pc2.full_clean()
+    pc2.save()
 
 
 def reverse_migrate(apps, schema_editor):
     playbook_config = apps.get_model("playbooks_manager", "PlaybookConfig")
     AnalyzerConfig = apps.get_model("analyzers_manager", "AnalyzerConfig")
     pc = playbook_config.objects.get(name="FREE_TO_USE_ANALYZERS")
+    pc2 = playbook_config.objects.get(name="Dns")
     pc.analyzers.remove(AnalyzerConfig.objects.get(name="AdGuard").id)
+    pc2.analyzers.remove(AnalyzerConfig.objects.get(name="AdGuard").id)
     pc.full_clean()
     pc.save()
+    pc2.full_clean()
+    pc2.save()
 
 
 class Migration(migrations.Migration):

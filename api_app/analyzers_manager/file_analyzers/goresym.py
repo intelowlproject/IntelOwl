217cfe46f981a5ed54dfb6f02e43659bb9864f58
g4ze <bhaiyajionline@gmail.com>
2024-07-06 00:08:58 +0530

    tests

--- api_app/analyzers_manager/file_analyzers/goresym.py
+++ api_app/analyzers_manager/file_analyzers/goresym.py
@@ -62,83 +62,88 @@ class GoReSym(FileAnalyzer, DockerBasedAnalyzer):
     def mocked_docker_analyzer_get(*args, **kwargs):
         return MockUpResponse(
             {
-                "Version": "1.22.3",
-                "BuildId": """nwVuB9ChiwRxUV3uqogj/gqLuN_Lt0hiTuOBT4YDw
+                "report": {
+                    "Version": "1.22.3",
+                    "BuildId": """nwVuB9ChiwRxUV3uqogj
+                    /gqLuN_Lt0hiTuOBT4YDw
                 /7ArmhPs-KKm8m0kwm8Ud/RNgWKMZv5-p8k3r8fJCp""",
-                "Arch": "amd64",
-                "OS": "linux",
-                "TabMeta": {
-                    "VA": 8261824,
-                    "Version": "1.20",
-                    "Endianess": "LittleEndian",
-                    "CpuQuantum": 1,
-                    "CpuQuantumStr": "x86/x64/wasm",
-                    "PointerSize": 8,
-                },
-                "ModuleMeta": {
-                    "VA": 10005792,
-                    "TextVA": 4198400,
-                    "Types": 7045120,
-                    "ETypes": 8248647,
-                    "Typelinks": {"Data": 8251936, "Len": 1791, "Capacity": 1791},
-                    "ITablinks": {"Data": 8259104, "Len": 339, "Capacity": 339},
-                    "LegacyTypes": {"Data": 0, "Len": 0, "Capacity": 0},
-                },
-                "Types": None,
-                "Interfaces": None,
-                "BuildInfo": {
-                    "GoVersion": "go1.22.3",
-                    "Path": "github.com/g4ze/byoc/reverse-proxy",
-                    "Main": {
-                        "Path": "github.com/g4ze/byoc",
-                        "Version": "(devel)",
-                        "Sum": "",
-                        "Replace": None,
+                    "Arch": "amd64",
+                    "OS": "linux",
+                    "TabMeta": {
+                        "VA": 8261824,
+                        "Version": "1.20",
+                        "Endianess": "LittleEndian",
+                        "CpuQuantum": 1,
+                        "CpuQuantumStr": "x86/x64/wasm",
+                        "PointerSize": 8,
                     },
-                    "Deps": [
-                        {
-                            "Path": "github.com/joho/godotenv",
-                            "Version": "v1.5.1",
-                            "Sum": "h1:7eLL/+HRGLY0ldzfGMeQkb7vMd0as4CfYvUVzLqw0N0=",
+                    "ModuleMeta": {
+                        "VA": 10005792,
+                        "TextVA": 4198400,
+                        "Types": 7045120,
+                        "ETypes": 8248647,
+                        "Typelinks": {"Data": 8251936, "Len": 1791, "Capacity": 1791},
+                        "ITablinks": {"Data": 8259104, "Len": 339, "Capacity": 339},
+                        "LegacyTypes": {"Data": 0, "Len": 0, "Capacity": 0},
+                    },
+                    "Types": None,
+                    "Interfaces": None,
+                    "BuildInfo": {
+                        "GoVersion": "go1.22.3",
+                        "Path": "github.com/g4ze/byoc/reverse-proxy",
+                        "Main": {
+                            "Path": "github.com/g4ze/byoc",
+                            "Version": "(devel)",
+                            "Sum": "",
                             "Replace": None,
                         },
-                        {
-                            "Path": "github.com/lib/pq",
-                            "Version": "v1.10.9",
-                            "Sum": "h1:YXG7RB+JIjhP29X+OtkiDnYaXQwpS4JEWq7dtCCRUEw=",
-                            "Replace": None,
-                        },
-                    ],
-                    "Settings": [
-                        {"Key": "-buildmode", "Value": "exe"},
-                        {"Key": "-compiler", "Value": "gc"},
-                        {"Key": "CGO_ENABLED", "Value": "1"},
-                        {"Key": "CGO_CFLAGS", "Value": ""},
-                        {"Key": "CGO_CPPFLAGS", "Value": ""},
-                        {"Key": "CGO_CXXFLAGS", "Value": ""},
-                        {"Key": "CGO_LDFLAGS", "Value": ""},
-                        {"Key": "GOARCH", "Value": "amd64"},
-                        {"Key": "GOOS", "Value": "linux"},
-                        {"Key": "GOAMD64", "Value": "v1"},
-                        {"Key": "vcs", "Value": "git"},
-                        {
-                            "Key": "vcs.revision",
-                            "Value": "34e6cafd47a85a15e9aeedd63786a2ba72e5b301",
-                        },
-                        {"Key": "vcs.time", "Value": "2024-06-24T07:44:25Z"},
-                        {"Key": "vcs.modified", "Value": "true"},
-                    ],
-                },
-                "Files": None,
-                "UserFunctions": [
-                    {
-                        "Start": 7043712,
-                        "End": 7043758,
-                        "PackageName": "main",
-                        "FullName": "main.main.NewSingleHostReverseProxy.func1",
+                        "Deps": [
+                            {
+                                "Path": "github.com/joho/godotenv",
+                                "Version": "v1.5.1",
+                                "Sum": """h1:7eLL/
+                                +HRGLY0ldzfGMeQkb7vMd0as4CfYvUVzLqw0N0=""",
+                                "Replace": None,
+                            },
+                            {
+                                "Path": "github.com/lib/pq",
+                                "Version": "v1.10.9",
+                                "Sum": """h1:YXG7RB+JIjhP29X
+                                +OtkiDnYaXQwpS4JEWq7dtCCRUEw=""",
+                                "Replace": None,
+                            },
+                        ],
+                        "Settings": [
+                            {"Key": "-buildmode", "Value": "exe"},
+                            {"Key": "-compiler", "Value": "gc"},
+                            {"Key": "CGO_ENABLED", "Value": "1"},
+                            {"Key": "CGO_CFLAGS", "Value": ""},
+                            {"Key": "CGO_CPPFLAGS", "Value": ""},
+                            {"Key": "CGO_CXXFLAGS", "Value": ""},
+                            {"Key": "CGO_LDFLAGS", "Value": ""},
+                            {"Key": "GOARCH", "Value": "amd64"},
+                            {"Key": "GOOS", "Value": "linux"},
+                            {"Key": "GOAMD64", "Value": "v1"},
+                            {"Key": "vcs", "Value": "git"},
+                            {
+                                "Key": "vcs.revision",
+                                "Value": "34e6cafd47a85a15e9aeedd63786a2ba72e5b301",
+                            },
+                            {"Key": "vcs.time", "Value": "2024-06-24T07:44:25Z"},
+                            {"Key": "vcs.modified", "Value": "true"},
+                        ],
                     },
-                ],
-                "StdFunctions": None,
+                    "Files": None,
+                    "UserFunctions": [
+                        {
+                            "Start": 7043712,
+                            "End": 7043758,
+                            "PackageName": "main",
+                            "FullName": "main.main.NewSingleHostReverseProxy.func1",
+                        },
+                    ],
+                    "StdFunctions": None,
+                }
             },
             200,
         )

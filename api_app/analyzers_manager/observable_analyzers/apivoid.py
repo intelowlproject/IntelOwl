4c6fec6ecdd4e5ab8c9781f76cea49b38dad1353
g4ze <bhaiyajionline17@gmail.com>
2024-07-20 16:32:00 +0530

    tests

--- api_app/analyzers_manager/observable_analyzers/apivoid.py
+++ api_app/analyzers_manager/observable_analyzers/apivoid.py
@@ -48,55 +48,53 @@ class ApiVoidAnalyzer(classes.ObservableAnalyzer):
                     "requests.get",
                     return_value=MockUpResponse(
                         {
-                            {
-                                "data": {
-                                    "report": {
-                                        "ip": "2.57.122.0",
-                                        "version": "v4",
-                                        "blacklists": {
-                                            "engines": {
-                                                "0": {
-                                                    "engine": "0spam",
-                                                    "detected": False,
-                                                    "reference": "https://0spam.org/",
-                                                    "elapsed": "0.09",
-                                                },
+                            "data": {
+                                "report": {
+                                    "ip": "2.57.122.0",
+                                    "version": "v4",
+                                    "blacklists": {
+                                        "engines": {
+                                            "0": {
+                                                "engine": "0spam",
+                                                "detected": False,
+                                                "reference": "https://0spam.org/",
+                                                "elapsed": "0.09",
                                             },
-                                            "detections": 7,
-                                            "engines_count": 79,
-                                            "detection_rate": "9%",
-                                            "scantime": "1.35",
                                         },
-                                        "information": {
-                                            "reverse_dns": "",
-                                            "continent_code": "EU",
-                                            "continent_name": "Europe",
-                                            "country_code": "RO",
-                                            "country_name": "Romania",
-                                            "country_currency": "RON",
-                                            "country_calling_code": "40",
-                                            "region_name": "Bucuresti",
-                                            "city_name": "Bucharest",
-                                            "latitude": 44.432301,
-                                            "longitude": 26.10607,
-                                            "isp": "Pptechnology Limited",
-                                            "asn": "AS47890",
-                                        },
-                                        "anonymity": {
-                                            "is_proxy": False,
-                                            "is_webproxy": False,
-                                            "is_vpn": False,
-                                            "is_hosting": False,
-                                            "is_tor": False,
-                                        },
-                                        "risk_score": {"result": 100},
-                                    }
-                                },
-                                "credits_remained": 24.76,
-                                "estimated_queries": "309",
-                                "elapsed_time": "2.58",
-                                "success": True,
-                            }
+                                        "detections": 7,
+                                        "engines_count": 79,
+                                        "detection_rate": "9%",
+                                        "scantime": "1.35",
+                                    },
+                                    "information": {
+                                        "reverse_dns": "",
+                                        "continent_code": "EU",
+                                        "continent_name": "Europe",
+                                        "country_code": "RO",
+                                        "country_name": "Romania",
+                                        "country_currency": "RON",
+                                        "country_calling_code": "40",
+                                        "region_name": "Bucuresti",
+                                        "city_name": "Bucharest",
+                                        "latitude": 44.432301,
+                                        "longitude": 26.10607,
+                                        "isp": "Pptechnology Limited",
+                                        "asn": "AS47890",
+                                    },
+                                    "anonymity": {
+                                        "is_proxy": False,
+                                        "is_webproxy": False,
+                                        "is_vpn": False,
+                                        "is_hosting": False,
+                                        "is_tor": False,
+                                    },
+                                    "risk_score": {"result": 100},
+                                }
+                            },
+                            "credits_remained": 24.76,
+                            "estimated_queries": "309",
+                            "elapsed_time": "2.58",
+                            "success": True,
                         },
                         200,
                     ),

33a87a219dae23e1ec30605035fbf7d050f10cf5
g4ze <bhaiyajionline17@gmail.com>
2024-07-22 21:17:57 +0530

    domaincheck

--- api_app/analyzers_manager/observable_analyzers/domaincheckanalyzer.py
+++ api_app/analyzers_manager/observable_analyzers/domaincheckanalyzer.py
@@ -1,5 +1,25 @@
+import logging
+
 from domaincheck import domaincheck
 
-result = domaincheck.main(
-    ["google.com"], resolver="8.8.8.8", output_format="json", verbosity=4
-)
+from api_app.analyzers_manager import classes
+
+logger = logging.getLogger(__name__)
+
+
+class domainCheck(classes.ObservableAnalyzer):
+    resolver: str = "8.8.8.8"
+
+    def run(self):
+        logger.info(f"Running DomainCheck for {self.observable_name}")
+        if not self.resolver:
+            self.resolver = None
+        result = domaincheck.main(
+            [self.observable_name],
+            resolver=self.resolver,
+        )
+        logger.info(f"DomainCheck result: {result}")
+        return result
+
+    def update(self):
+        pass

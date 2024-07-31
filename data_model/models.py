1cf2e0a133389ad431be1cc709d22181624b9a5b
Cristina Ascari <c.ascari@certego.net>
2024-07-31 18:14:39 +0200

    updated data models

--- data_model/models.py
+++ data_model/models.py
@@ -1,4 +1,13 @@
+from django.contrib.postgres import fields as pg_fields
 from django.db import models
+
+
+class IETFReport(models.Model):
+    rrname = models.CharField()
+    rrtype = models.CharField()
+    rdata = pg_fields.ArrayField(models.CharField())
+    time_first = models.DateTimeField()
+    time_last = models.DateTimeField()
 
 
 class DomainDataModel(models.Model):
@@ -6,18 +15,30 @@ class DomainDataModel(models.Model):
     classification = models.CharField(
         null=True
     )  # HybridAnalysisObservable (verdict), BasicMaliciousDetector
+    ietf_report = models.ForeignKey(
+        IETFReport, on_delete=models.CASCADE, null=True
+    )  # pdns
+    tranco_rank = models.IntegerField(null=True)  # Tranco
+    related_url = models.URLField(
+        null=True
+    )  # Crowdsec (link), UrlHaus (external_references)
 
 
 class IPDataModel(models.Model):
     evaluation = models.CharField(null=True)
-    asn = models.IntegerField(null=True)  # BGPRanking
-    asn_rank = models.DecimalField(null=True)  # BGPRanking
-    circl_pssl_certificates = models.JSONField(null=True)  # CIRCL_PSSL
     classification = models.CharField(
         null=True
     )  # Crowdsec, GreyNoise, HybridAnalysisObservable (verdict), BasicMaliciousDetector
+    ietf_report = models.ForeignKey(
+        IETFReport, on_delete=models.CASCADE, null=True
+    )  # pdns
+    asn = models.IntegerField(null=True)  # BGPRanking
+    asn_rank = models.DecimalField(null=True)  # BGPRanking
+    circl_pssl_certificates = models.JSONField(null=True)  # CIRCL_PSSL
     behavior = models.CharField(null=True)  # Crowdsec
-    link = models.URLField(null=True)  # Crowdsec
+    related_url = models.URLField(
+        null=True
+    )  # Crowdsec (link), UrlHaus (external_references)
     noise = models.BooleanField(null=True)  # GreyNoise
     riot = models.BooleanField(null=True)  # GreyNoise
     org_name = models.CharField(null=True)  # GreyNoise
@@ -25,11 +46,15 @@ class IPDataModel(models.Model):
 
 
 class HashDataModel(models.Model):
-    classification_tags = models.CharField(
-        null=True
-    )  # HybridAnalysisFileAnalyzer (list?)
-    tags = models.CharField(null=True)  # HybridAnalysisFileAnalyzer (list?)
-    domains = models.CharField(null=True)  # HybridAnalysisFileAnalyzer (list?)
-    compromised_hosts = models.CharField(
-        null=True
-    )  # HybridAnalysisFileAnalyzer (list?)
+    classification_tags = pg_fields.ArrayField(
+        models.CharField(), null=True
+    )  # HybridAnalysisFileAnalyzer
+    tags = pg_fields.ArrayField(
+        models.CharField(), null=True
+    )  # HybridAnalysisFileAnalyzer
+    domains = pg_fields.ArrayField(
+        models.CharField(), null=True
+    )  # HybridAnalysisFileAnalyzer
+    compromised_hosts = pg_fields.ArrayField(
+        models.CharField(), null=True
+    )  # HybridAnalysisFileAnalyzer

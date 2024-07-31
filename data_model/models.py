604738b462eba53617050f4df2713db2c88bc954
Cristina Ascari <c.ascari@certego.net>
2024-07-31 12:58:53 +0200

    updated data models

--- data_model/models.py
+++ data_model/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class DomainDataModel(models.Model):
     evaluation = models.CharField(null=True)
-    classification = models.CharField(null=True)  # HybridAnalysisObservable (verdict)
+    classification = models.CharField(
+        null=True
+    )  # HybridAnalysisObservable (verdict), BasicMaliciousDetector
 
 
 class IPDataModel(models.Model):
@@ -13,9 +15,21 @@ class IPDataModel(models.Model):
     circl_pssl_certificates = models.JSONField(null=True)  # CIRCL_PSSL
     classification = models.CharField(
         null=True
-    )  # Crowdsec, GreyNoise, HybridAnalysisObservable (verdict)
+    )  # Crowdsec, GreyNoise, HybridAnalysisObservable (verdict), BasicMaliciousDetector
     behavior = models.CharField(null=True)  # Crowdsec
     link = models.URLField(null=True)  # Crowdsec
     noise = models.BooleanField(null=True)  # GreyNoise
     riot = models.BooleanField(null=True)  # GreyNoise
     org_name = models.CharField(null=True)  # GreyNoise
+    vx_family = models.CharField(null=True)  # HybridAnalysisObservable
+
+
+class HashDataModel(models.Model):
+    classification_tags = models.CharField(
+        null=True
+    )  # HybridAnalysisFileAnalyzer (list?)
+    tags = models.CharField(null=True)  # HybridAnalysisFileAnalyzer (list?)
+    domains = models.CharField(null=True)  # HybridAnalysisFileAnalyzer (list?)
+    compromised_hosts = models.CharField(
+        null=True
+    )  # HybridAnalysisFileAnalyzer (list?)

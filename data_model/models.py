210f0083e9a3a9abe7953a99408be54e161c1d09
Cristina Ascari <c.ascari@certego.net>
2024-08-26 09:56:56 +0200

    fix CharFiled max_length

--- data_model/models.py
+++ data_model/models.py
@@ -3,17 +3,18 @@ from django.db import models
 
 
 class IETFReport(models.Model):
-    rrname = models.CharField()
-    rrtype = models.CharField()
-    rdata = pg_fields.ArrayField(models.CharField())
+    rrname = models.CharField(max_length=100)
+    rrtype = models.CharField(max_length=100)
+    rdata = pg_fields.ArrayField(models.CharField(max_length=100))
     time_first = models.DateTimeField()
     time_last = models.DateTimeField()
 
 
 class DomainDataModel(models.Model):
-    evaluation = models.CharField(null=True)
+    evaluation = models.CharField(max_length=100, null=True)
     classification = models.CharField(
-        null=True
+        max_length=100,
+        null=True,
     )  # HybridAnalysisObservable (verdict), BasicMaliciousDetector,
     # GoogleSafeBrowsing, Crowdsec
     ietf_report = models.ForeignKey(
@@ -23,13 +24,16 @@ class DomainDataModel(models.Model):
     related_url = models.URLField(
         null=True
     )  # Crowdsec (link), UrlHaus (external_references)
-    threats = pg_fields.ArrayField(models.CharField(), null=True)  # GoogleSafeBrowsing
+    threats = pg_fields.ArrayField(
+        models.CharField(max_length=100), null=True
+    )  # GoogleSafeBrowsing
 
 
 class IPDataModel(models.Model):
-    evaluation = models.CharField(null=True)
+    evaluation = models.CharField(max_length=100, null=True)
     classification = models.CharField(
-        null=True
+        max_length=100,
+        null=True,
     )  # Crowdsec, GreyNoise, HybridAnalysisObservable (verdict),
     # BasicMaliciousDetector, GoogleSafeBrowsing
     ietf_report = models.ForeignKey(
@@ -38,38 +42,45 @@ class IPDataModel(models.Model):
     asn = models.IntegerField(null=True)  # BGPRanking, MaxMind
     asn_rank = models.DecimalField(null=True)  # BGPRanking
     certificates = models.JSONField(null=True)  # CIRCL_PSSL
-    behavior = models.CharField(null=True)  # Crowdsec
+    behavior = models.CharField(max_length=100, null=True)  # Crowdsec
     related_urls = pg_fields.ArrayField(
         models.URLField(), null=True
     )  # Crowdsec (link), UrlHaus (external_references)
     noise = models.BooleanField(null=True)  # GreyNoise
     riot = models.BooleanField(null=True)  # GreyNoise
-    org_name = models.CharField(null=True)  # GreyNoise
-    vx_family = models.CharField(null=True)  # HybridAnalysisObservable
-    country = models.CharField(null=True)  # MaxMind, AbuseIPDB
-    country_code = models.CharField(null=True)  # MaxMind, AbuseIPDB
-    registered_country = models.CharField(null=True)  # MaxMind, AbuseIPDB
-    registered_country_code = models.CharField(null=True)  # MaxMind, AbuseIPDB
-    isp = models.CharField(null=True)  # AbuseIPDB
-    threats = pg_fields.ArrayField(models.CharField(), null=True)  # GoogleSafeBrowsing
+    org_name = models.CharField(max_length=100, null=True)  # GreyNoise
+    vx_family = models.CharField(max_length=100, null=True)  # HybridAnalysisObservable
+    country = models.CharField(max_length=100, null=True)  # MaxMind, AbuseIPDB
+    country_code = models.CharField(max_length=100, null=True)  # MaxMind, AbuseIPDB
+    registered_country = models.CharField(
+        max_length=100, null=True
+    )  # MaxMind, AbuseIPDB
+    registered_country_code = models.CharField(
+        max_length=100, null=True
+    )  # MaxMind, AbuseIPDB
+    isp = models.CharField(max_length=100, null=True)  # AbuseIPDB
+    threats = pg_fields.ArrayField(
+        models.CharField(max_length=100), null=True
+    )  # GoogleSafeBrowsing
     is_anonymizer = models.BooleanField(null=True)  # TorProject, Crowdsec
     is_tor_exit_node = models.BooleanField(null=True)  # TorProject, Crowdsec
 
 
 class FileDataModel(models.Model):
     evaluation = models.CharField(
-        null=True
+        max_length=100,
+        null=True,
     )  # Cymru (found), Cuckoo (malscore), Intezer (verdict/sub_verdict),
     # Triage (analysis.score)
     classification_tags = pg_fields.ArrayField(
-        models.CharField(), null=True
+        models.CharField(max_length=100), null=True
     )  # HybridAnalysisFileAnalyzer
     tags = pg_fields.ArrayField(
-        models.CharField(), null=True
+        models.CharField(max_length=100), null=True
     )  # HybridAnalysisFileAnalyzer, MalwareBazaarFileAnalyzer, MwDB,
     # VirusTotalV3FileAnalyzer (report.data.tags)
     compromised_hosts = pg_fields.ArrayField(
-        models.CharField(), null=True
+        models.CharField(max_length=100), null=True
     )  # HybridAnalysisFileAnalyzer
     related_urls = pg_fields.ArrayField(
         models.URLField(), null=True
@@ -81,10 +92,11 @@ class FileDataModel(models.Model):
     # HybridAnalysisFileAnalyzer (domains),
     # VirusTotalV3FileAnalyzer (data.relationships.contacted_urls/contacted_domains)
     signatures = pg_fields.ArrayField(
-        models.CharField(), null=True
+        models.CharField(max_length=100), null=True
     )  # ClamAvFileAnalyzer, MalwareBazaarFileAnalyzer, Yara (report.list_el.match)
     family = models.CharField(
-        null=True
+        max_length=100,
+        null=True,
     )  # Intezer (family_name), Cuckoo, MwDB, Triage (analysis.family),
     # UnpacMe (results.malware_id.malware_family),
     # VirusTotalV3FileAnalyzer
@@ -93,7 +105,7 @@ class FileDataModel(models.Model):
         models.JSONField(), null=True
     )  # MalwareBazaarFileAnalyzer, Yaraify (report.data.tasks.static_result)
     comments = pg_fields.ArrayField(
-        models.CharField(), null=True
+        models.CharField(max_length=100), null=True
     )  # MalwareBazaarFileAnalyzer,
     # VirusTotalV3FileAnalyzer (data.relationships.comments)
     file_information = pg_fields.ArrayField(
@@ -102,11 +114,11 @@ class FileDataModel(models.Model):
     # (files), QuarkEngineAPK (crimes.confidence, threat_level, total_score)
     # RtfInfo (exploit_equation_editor, exploit_ole2link_vuln)
     related_threats = pg_fields.ArrayField(
-        models.CharField(), null=True
+        models.CharField(max_length=100), null=True
     )  # MalwareBazaarFileAnalyzer(?), QuarkEngineAPK (crimes.crime)
     stats = pg_fields.ArrayField(
         models.JSONField(), null=True
     )  # PdfInfo (peepdf_stats)
     pdfid_reports = pg_fields.ArrayField(models.JSONField(), null=True)  # PdfInfo
-    imphash = models.CharField(null=True)  # PeInfo
-    type = models.CharField(null=True)  # PeInfo
+    imphash = models.CharField(max_length=100, null=True)  # PeInfo
+    type = models.CharField(max_length=100, null=True)  # PeInfo

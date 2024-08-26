8e78524dfd21f5181d6195d2825d946e1ec80e97
Cristina Ascari <c.ascari@certego.net>
2024-08-26 17:43:29 +0200

    updates data models

--- data_model/models.py
+++ data_model/models.py
@@ -13,37 +13,46 @@ class IETFReport(models.Model):
 class BaseDataModel(models.Model):
     evaluation = models.CharField(
         max_length=100, null=True
-    )  # classification/verdict/found/score
+    )  # classification/verdict/found/score/malscore
+    # HybridAnalysisObservable (verdict), BasicMaliciousDetector (malicious),
+    # GoogleSafeBrowsing (malicious), Crowdsec (classifications),
+    # GreyNoise (classification), Cymru (found), Cuckoo (malscore),
+    # Intezer (verdict/sub_verdict), Triage (analysis.score),
+    # HybridAnalysisFileAnalyzer (classification_tags)
     related_urls = pg_fields.ArrayField(
         models.URLField(), null=True
     )  # link/external_references/permalink/domains
-    threats = pg_fields.ArrayField(
+    # Crowdsec (link), UrlHaus (external_references), BoxJs,
+    # Cuckoo (result_url/permalink), Intezer (link/analysis_url),
+    # MalwareBazaarFileAnalyzer (permalink/file_information.value), MwDB (permalink),
+    # StringsInfo (data), Triage (permalink), UnpacMe (permalink), XlmMacroDeobfuscator,
+    # Yara (report.list_el.url/rule_url), Yaraify (link),
+    # HybridAnalysisFileAnalyzer (domains),
+    # VirusTotalV3FileAnalyzer (data.relationships.contacted_urls/contacted_domains)
+    related_threats = pg_fields.ArrayField(
         models.CharField(max_length=100), null=True
     )  # threats/related_threats
-
-
-class DomainDataModel(models.Model):
-    evaluation = models.CharField(
+    # GoogleSafeBrowsing, QuarkEngineAPK (crimes.crime)
+    family = models.CharField(
         max_length=100, null=True
-    )  # HybridAnalysisObservable (verdict),
-    # BasicMaliciousDetector, GoogleSafeBrowsing, Crowdsec (classification)
+    )  # family/family_name/malware_family
+    # HybridAnalysisObservable, Intezer (family_name), Cuckoo, MwDB,
+    # Triage (analysis.family), UnpacMe (results.malware_id.malware_family),
+    # VirusTotalV3FileAnalyzer
+    # (attributes.last_analysis_results.list_el.results/attributes.names)
+    additional_info = (
+        models.JSONField()
+    )  # field for additional information related to a specific analyzer
+
+
+class DomainDataModel(BaseDataModel):
     ietf_report = models.ForeignKey(
         IETFReport, on_delete=models.CASCADE, null=True
     )  # pdns
     tranco_rank = models.IntegerField(null=True)  # Tranco
-    related_urls = pg_fields.ArrayField(
-        models.URLField(), null=True
-    )  # Crowdsec (link), UrlHaus (external_references)
-    threats = pg_fields.ArrayField(
-        models.CharField(max_length=100), null=True
-    )  # GoogleSafeBrowsing
 
 
-class IPDataModel(models.Model):
-    evaluation = models.CharField(
-        max_length=100, null=True
-    )  # HybridAnalysisObservable (verdict),
-    # Crowdsec, GreyNoise, BasicMaliciousDetector, GoogleSafeBrowsing (classification)
+class IPDataModel(BaseDataModel):
     ietf_report = models.ForeignKey(
         IETFReport, on_delete=models.CASCADE, null=True
     )  # pdns
@@ -51,13 +60,9 @@ class IPDataModel(models.Model):
     asn_rank = models.DecimalField(null=True)  # BGPRanking
     certificates = models.JSONField(null=True)  # CIRCL_PSSL
     behavior = models.CharField(max_length=100, null=True)  # Crowdsec
-    related_urls = pg_fields.ArrayField(
-        models.URLField(), null=True
-    )  # Crowdsec (link), UrlHaus (external_references)
     noise = models.BooleanField(null=True)  # GreyNoise
     riot = models.BooleanField(null=True)  # GreyNoise
     org_name = models.CharField(max_length=100, null=True)  # GreyNoise
-    family = models.CharField(max_length=100, null=True)  # HybridAnalysisObservable
     country = models.CharField(max_length=100, null=True)  # MaxMind, AbuseIPDB
     country_code = models.CharField(max_length=100, null=True)  # MaxMind, AbuseIPDB
     registered_country = models.CharField(
@@ -67,19 +72,11 @@ class IPDataModel(models.Model):
         max_length=100, null=True
     )  # MaxMind, AbuseIPDB
     isp = models.CharField(max_length=100, null=True)  # AbuseIPDB
-    threats = pg_fields.ArrayField(
-        models.CharField(max_length=100), null=True
-    )  # GoogleSafeBrowsing
     is_anonymizer = models.BooleanField(null=True)  # TorProject, Crowdsec
     is_tor_exit_node = models.BooleanField(null=True)  # TorProject, Crowdsec
 
 
-class FileDataModel(models.Model):
-    evaluation = models.CharField(
-        max_length=100,
-        null=True,
-    )  # Cymru (found), Cuckoo (malscore), Intezer (verdict/sub_verdict),
-    # Triage (analysis.score), HybridAnalysisFileAnalyzer (classification_tags)
+class FileDataModel(BaseDataModel):
     tags = pg_fields.ArrayField(
         models.CharField(max_length=100), null=True
     )  # HybridAnalysisFileAnalyzer, MalwareBazaarFileAnalyzer, MwDB,
@@ -87,25 +84,9 @@ class FileDataModel(models.Model):
     compromised_hosts = pg_fields.ArrayField(
         models.CharField(max_length=100), null=True
     )  # HybridAnalysisFileAnalyzer
-    related_urls = pg_fields.ArrayField(
-        models.URLField(), null=True
-    )  # Crowdsec (link), UrlHaus (external_references), BoxJs,
-    # Cuckoo (result_url/permalink), Intezer (link/analysis_url),
-    # MalwareBazaarFileAnalyzer (permalink/file_information.value), MwDB (permalink),
-    # StringsInfo (data), Triage (permalink), UnpacMe (permalink), XlmMacroDeobfuscator,
-    # Yara (report.list_el.url/rule_url), Yaraify (link),
-    # HybridAnalysisFileAnalyzer (domains),
-    # VirusTotalV3FileAnalyzer (data.relationships.contacted_urls/contacted_domains)
     signatures = pg_fields.ArrayField(
         models.CharField(max_length=100), null=True
     )  # ClamAvFileAnalyzer, MalwareBazaarFileAnalyzer, Yara (report.list_el.match)
-    family = models.CharField(
-        max_length=100,
-        null=True,
-    )  # Intezer (family_name), Cuckoo, MwDB, Triage (analysis.family),
-    # UnpacMe (results.malware_id.malware_family),
-    # VirusTotalV3FileAnalyzer
-    # (attributes.last_analysis_results.list_el.results/attributes.names)
     yara_rules = pg_fields.ArrayField(
         models.JSONField(), null=True
     )  # MalwareBazaarFileAnalyzer, Yaraify (report.data.tasks.static_result)
@@ -118,9 +99,6 @@ class FileDataModel(models.Model):
     )  # MalwareBazaarFileAnalyzer, OneNoteInfo
     # (files), QuarkEngineAPK (crimes.confidence, threat_level, total_score)
     # RtfInfo (exploit_equation_editor, exploit_ole2link_vuln)
-    related_threats = pg_fields.ArrayField(
-        models.CharField(max_length=100), null=True
-    )  # MalwareBazaarFileAnalyzer(?), QuarkEngineAPK (crimes.crime)
     stats = pg_fields.ArrayField(
         models.JSONField(), null=True
     )  # PdfInfo (peepdf_stats)

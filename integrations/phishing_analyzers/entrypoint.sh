e8b811e2f0325b6a7cd3f022822ab2b5a7dc4ee0
Federico Gibertoni <f.gibertoni@certego.net>
2024-09-13 17:46:20 +0200

    Fixed docker permissions

--- integrations/phishing_analyzers/entrypoint.sh
+++ integrations/phishing_analyzers/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-
+mkdir -p ${LOG_PATH}
+touch ${LOG_PATH}/gunicorn_access.log ${LOG_PATH}/gunicorn_errors.log
+chown -R ${USER}:${USER} . ${LOG_PATH}
 gunicorn 'app:app' \
     --bind '0.0.0.0:4005' \
     --log-level ${LOG_LEVEL} \
+    --user ${USER} \
     --access-logfile ${LOG_PATH}/gunicorn_access.log \
     --error-logfile ${LOG_PATH}/gunicorn_errors.log

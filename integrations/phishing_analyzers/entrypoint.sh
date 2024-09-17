d528b84001567c256f0fcee8b7666c015b00acb9
Federico Gibertoni <f.gibertoni@certego.net>
2024-09-17 14:41:09 +0200

    Restricted permissions and injection points

--- integrations/phishing_analyzers/entrypoint.sh
+++ integrations/phishing_analyzers/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-sudo /usr/bin/mkdir -p ${LOG_PATH}
-sudo /usr/bin/touch ${LOG_PATH}/gunicorn_access.log ${LOG_PATH}/gunicorn_errors.log
-sudo /usr/bin/chown -R ${USER}:${USER} . ${LOG_PATH}
+sudo /usr/bin/mkdir -p /var/log/intel_owl/phishing_analyzers
+sudo /usr/bin/touch /var/log/intel_owl/phishing_analyzers/gunicorn_access.log \
+      /var/log/intel_owl/phishing_analyzers/gunicorn_errors.log
+sudo /usr/bin/chown -R phishing-user:phishing-user \
+      /opt/deploy/phishing_analyzers /var/log/intel_owl/phishing_analyzers
 
 /usr/local/bin/gunicorn 'app:app' \
     --bind '0.0.0.0:4005' \
     --log-level ${LOG_LEVEL} \
-    --user ${USER} \
-    --group ${USER} \
-    --access-logfile ${LOG_PATH}/gunicorn_access.log \
-    --error-logfile ${LOG_PATH}/gunicorn_errors.log
+    --user phishing-user \
+    --group phishing-user \
+    --access-logfile /var/log/intel_owl/phishing_analyzers/gunicorn_access.log \
+    --error-logfile /var/log/intel_owl/phishing_analyzers/gunicorn_errors.log

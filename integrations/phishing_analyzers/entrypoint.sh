56eeeec2aefd3ed4e03efd7f45c9c6358c8dfcef
Federico Gibertoni <f.gibertoni@certego.net>
2024-09-17 12:07:19 +0200

    Ran entrypoint as unprivileged user using sudoers file

--- integrations/phishing_analyzers/entrypoint.sh
+++ integrations/phishing_analyzers/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-mkdir -p ${LOG_PATH}
-touch ${LOG_PATH}/gunicorn_access.log ${LOG_PATH}/gunicorn_errors.log
-chown -R ${USER}:${USER} . ${LOG_PATH}
-gunicorn 'app:app' \
+sudo /usr/bin/mkdir -p ${LOG_PATH}
+sudo /usr/bin/touch ${LOG_PATH}/gunicorn_access.log ${LOG_PATH}/gunicorn_errors.log
+sudo /usr/bin/chown -R ${USER}:${USER} . ${LOG_PATH}
+
+/usr/local/bin/gunicorn 'app:app' \
     --bind '0.0.0.0:4005' \
     --log-level ${LOG_LEVEL} \
     --user ${USER} \
+    --group ${USER} \
     --access-logfile ${LOG_PATH}/gunicorn_access.log \
     --error-logfile ${LOG_PATH}/gunicorn_errors.log

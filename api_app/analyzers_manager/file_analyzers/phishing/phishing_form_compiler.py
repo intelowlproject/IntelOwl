9211ddfcbeb33d6d37943f3bc9967faa7a2e060b
Federico Gibertoni <f.gibertoni@certego.net>
2024-10-09 10:00:11 +0200

    Added observable name extraction

--- api_app/analyzers_manager/file_analyzers/phishing/phishing_form_compiler.py
+++ api_app/analyzers_manager/file_analyzers/phishing/phishing_form_compiler.py
@@ -1,12 +1,25 @@
 import logging
+from typing import Dict
 
 from api_app.analyzers_manager.classes import DockerBasedAnalyzer, FileAnalyzer
+from api_app.analyzers_manager.exceptions import AnalyzerRunException
 
 logger = logging.getLogger(__name__)
 
 
 class PhishingFormCompiler(FileAnalyzer, DockerBasedAnalyzer):
-    def run(self) -> dict:
-        if not (target_site := self._job.parent_job.parent_job.observable_name):
+    target_site: str
+
+    def config(self, runtime_configuration: Dict):
+        super().config(runtime_configuration)
+
+        if not (hasattr(self._job, "pivot_parent")):
+            raise AnalyzerRunException(f"Analyzer {self.analyzer_name}")
+
+        self.target_site = self._job.pivot_parent.starting_job.observable_name
+        if not self.target_site:
             logger.info("Target site not found! Proceeding with only source code.")
-        print(target_site)
+
+    def run(self) -> dict:
+        logger.info(self.target_site)
+        return {}

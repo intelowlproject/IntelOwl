d5453e8967445ba4fadd1d922e2bd19cfe254227
Federico Gibertoni <f.gibertoni@certego.net>
2024-10-09 11:02:44 +0200

    Added docker based analyzer with request

--- api_app/analyzers_manager/file_analyzers/phishing/phishing_form_compiler.py
+++ api_app/analyzers_manager/file_analyzers/phishing/phishing_form_compiler.py
@@ -3,12 +3,25 @@ from typing import Dict
 
 from api_app.analyzers_manager.classes import DockerBasedAnalyzer, FileAnalyzer
 from api_app.analyzers_manager.exceptions import AnalyzerRunException
+from api_app.models import PythonConfig
 
 logger = logging.getLogger(__name__)
 
 
 class PhishingFormCompiler(FileAnalyzer, DockerBasedAnalyzer):
-    target_site: str
+    name: str = "PhishingFormCompiler"
+    url: str = "http://phishing_analyzers:4005/phishing_form_compiler"
+    max_tries: int = 20
+    poll_distance: int = 3
+
+    def __init__(
+        self,
+        config: PythonConfig,
+        **kwargs,
+    ):
+        super().__init__(config, **kwargs)
+        self.target_site: str = ""
+        self.html_source_code: str = ""
 
     def config(self, runtime_configuration: Dict):
         super().config(runtime_configuration)
@@ -17,9 +30,22 @@ class PhishingFormCompiler(FileAnalyzer, DockerBasedAnalyzer):
             raise AnalyzerRunException(f"Analyzer {self.analyzer_name}")
 
         self.target_site = self._job.pivot_parent.starting_job.observable_name
-        if not self.target_site:
-            logger.info("Target site not found! Proceeding with only source code.")
+        if self.target_site:
+            logger.info(f"Extracted {self.target_site} from parent job")
+        else:
+            logger.info(
+                "Target site from parent job not found! Proceeding with only source code."
+            )
+
+        self.html_source_code = self.read_file_bytes()
 
     def run(self) -> dict:
-        logger.info(self.target_site)
-        return {}
+        req_data: {} = {
+            "target": self.target_site,
+            "source_code": self.html_source_code,
+        }
+        logger.info(f"sending {req_data=} to {self.url}")
+        return self._docker_run(req_data)
+
+    def update(self) -> bool:
+        pass

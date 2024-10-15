dc12b5b89f871f098be2fbe7a752f247fe4d4586
Vignesh Skanda <agvskanda@gmail.com>
2024-10-15 11:41:05 +0530

    Update test_interfaces.py

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -1,41 +1,31 @@
 from django.db.models import QuerySet
-
 from api_app.interfaces import CreateJobsFromPlaybookInterface
 from api_app.playbooks_manager.models import PlaybookConfig
 from tests import CustomTestCase
 
-
 class CreateJobsFromPlaybookInterfaceTestCase(CustomTestCase):
-    class Test(CreateJobsFromPlaybookInterface):
+    class TestInterface(CreateJobsFromPlaybookInterface):
+        """Test implementation of CreateJobsFromPlaybookInterface."""
         def __init__(self, playbooks: QuerySet):
             self.playbooks_choice = playbooks
             self.name = "Test"
 
-    def test_validate_playbook_to_execute(self):
-        default_pc = PlaybookConfig.objects.create(
-            name="Playbook", type=["ip"], description="test"
-        )
-        a = self.Test(PlaybookConfig.objects.filter(pk=default_pc.pk))
-        try:
-            a.validate_playbooks(self.user)
-        except RuntimeError as e:
-            self.fail(e)
-        default_pc.delete()
-        pc_owned = PlaybookConfig.objects.create(
-            name="Playbook", type=["ip"], description="test", owner=self.user
-        )
-        a = self.Test(PlaybookConfig.objects.filter(pk=pc_owned.pk))
-        try:
-            a.validate_playbooks(self.user)
-        except RuntimeError as e:
-            self.fail(e)
+    def test_validate_playbooks(self):
+        """Test the validate_playbooks method under different ownership scenarios."""
+        def create_and_test_playbook(owner=None):
+            playbook = PlaybookConfig.objects.create(
+                name="Playbook", type=["ip"], description="test", owner=owner
+            )
+            interface = self.TestInterface(PlaybookConfig.objects.filter(pk=playbook.pk))
+            
+            if owner is None or owner == self.user:
+                interface.validate_playbooks(self.user)  # Should not raise an exception
+            else:
+                with self.assertRaises(RuntimeError):
+                    interface.validate_playbooks(self.user)
+            
+            playbook.delete()
 
-        pc_owned.delete()
-
-        pc_not_owned = PlaybookConfig.objects.create(
-            name="Playbook", type=["ip"], description="test", owner=self.superuser
-        )
-        a = self.Test(PlaybookConfig.objects.filter(pk=pc_not_owned.pk))
-        with self.assertRaises(RuntimeError):
-            a.validate_playbooks(self.user)
-        pc_not_owned.delete()
+        create_and_test_playbook()  # Test with no owner
+        create_and_test_playbook(self.user)  # Test with current user as owner
+        create_and_test_playbook(self.superuser)  # Test with different user as owner

74fc4c999eaa1406470e3f9e7c8129a8ee07aaab
Federico Gibertoni <f.gibertoni@certego.net>
2024-10-17 14:37:22 +0200

    Added every command as absolute

--- integrations/phishing_analyzers/entrypoint.sh
+++ integrations/phishing_analyzers/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo /usr/bin/mkdir -p /var/log/intel_owl/phishing_analyzers
-sudo /usr/bin/touch /var/log/intel_owl/phishing_analyzers/gunicorn_access.log \
+/usr/bin/sudo /usr/bin/mkdir -p /var/log/intel_owl/phishing_analyzers
+/usr/bin/sudo /usr/bin/touch /var/log/intel_owl/phishing_analyzers/gunicorn_access.log \
       /var/log/intel_owl/phishing_analyzers/gunicorn_errors.log
-sudo /usr/bin/chown -R phishing-user:phishing-user \
+/usr/bin/sudo /usr/bin/chown -R phishing-user:phishing-user \
       /opt/deploy/phishing_analyzers /var/log/intel_owl/phishing_analyzers
 
 /usr/local/bin/gunicorn 'app:app' \

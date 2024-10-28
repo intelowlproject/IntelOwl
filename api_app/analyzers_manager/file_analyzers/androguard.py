302a6feae1edc4960d31751fdc3cfa1f9c327497
spoiicy <spoiicy@spoiicys-Laptop.local>
2024-10-28 17:58:08 +0530

    added capabilities to androguard analyzer

--- api_app/analyzers_manager/file_analyzers/androguard.py
+++ api_app/analyzers_manager/file_analyzers/androguard.py
@@ -1,5 +1,7 @@
 import androguard
-import androguard.misc
+import androguard.core
+import androguard.core.bytecodes
+import androguard.core.bytecodes.apk
 
 from api_app.analyzers_manager.classes import FileAnalyzer
 
@@ -7,26 +9,23 @@ from api_app.analyzers_manager.classes import FileAnalyzer
 class AndroguardAnalyzer(FileAnalyzer):
     def run(self):
 
-        results = {
-            "app_name": "",
-            "permissions": [],
-            "activities": [],
-            "requested_third_party_permissions": [],
-            "providers": [],
-            "features": [],
-        }
-
         binary = self.read_file_bytes()
-
-        apk = androguard.misc.APK(binary, raw=True)
-
-        results["app_name"] = apk.get_app_name()
-        results["permissions"] = apk.get_permissions()
-        results["activities"] = apk.get_activities()
-        results["requested_third_party_permissions"] = (
-            apk.get_requested_third_party_permissions()
-        )
-        results["providers"] = apk.get_providers()
-        results["features"] = apk.get_features()
+        apk = androguard.core.bytecodes.apk.APK(binary, raw=True)
+        results = {
+            "app_name": apk.get_app_name(),
+            "permissions": apk.get_permissions(),
+            "activities": apk.get_activities(),
+            "requested_third_party_permissions": apk.get_requested_third_party_permissions(),
+            "providers": apk.get_providers(),
+            "features": apk.get_features(),
+            "receivers": apk.get_receivers(),
+            "services": apk.get_services(),
+            "is_valid_apk": apk.is_valid_APK(),
+            "min_sdk_version": apk.get_min_sdk_version(),
+            "max_sdk_version": apk.get_max_sdk_version(),
+            "target_sdk_version": apk.get_target_sdk_version(),
+            "android_version_code": apk.get_androidversion_code(),
+            "android_version_name": apk.get_androidversion_name(),
+        }
 
         return results

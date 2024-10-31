4220e7e28e56d75e0cd7ca3e35e5d773bc54ad0d
Daniele Rosetti <55402684+drosetti@users.noreply.github.com>
2024-10-31 16:22:33 +0100

    Container with Elasticsearch (#2550)
    * updated elastic, removed kibana and fixed
    
    * added elastic conf
    
    * added persistency and tls
    
    * fixed elastic docker
    
    * removed useless setting
    
    * undo
    
    * remove
    
    * moved to index template
    
    * changed comment
    
    * moved doc indexing in task instead of signal
    
    * added log
    
    * template auto create
    
    * removed mock
    
    * fix
    
    * elastic support https
    
    * added test for elastic cronjob and a collection to mark the last updated time
    
    * code quality
    
    * print to debug ci
    
    * debug ci
    
    * fixed ci

--- intel_owl/settings/elasticsearch.py
+++ intel_owl/settings/elasticsearch.py
@@ -16,25 +16,34 @@ if ELASTICSEARCH_BI_ENABLED:
         print("Elasticsearch not correctly configured")
 
     else:
-        ELASTICSEARCH_CLIENT = Elasticsearch(
+        ELASTICSEARCH_BI_CLIENT = Elasticsearch(
             ELASTICSEARCH_BI_HOST,
             maxsize=20,
             max_retries=10,
             retry_on_timeout=True,
             timeout=30,
         )
-        if not ELASTICSEARCH_CLIENT.ping():
-            print("ELASTICSEARCH client configuration did not connect correctly")
+        if not ELASTICSEARCH_BI_CLIENT.ping():
+            print(
+                f"ELASTICSEARCH BI client configuration did not connect correctly: {ELASTICSEARCH_BI_CLIENT.info()}"
+            )
 
-ELASTICSEARCH_DSL_ENABLED = (
-    secrets.get_secret("ELASTICSEARCH_DSL_ENABLED", False) == "True"
-)
-if ELASTICSEARCH_DSL_ENABLED:
-    ELASTICSEARCH_DSL_HOST = secrets.get_secret("ELASTICSEARCH_DSL_HOST")
+ELASTIC_HOST = secrets.get_secret("ELASTIC_HOST")
+if ELASTIC_HOST:
+    elastic_client_settings = {"hosts": ELASTIC_HOST}
 
-    ELASTICSEARCH_DSL = {
-        "default": {"hosts": ELASTICSEARCH_DSL_HOST},
-    }
+    ELASTIC_PASSWORD = secrets.get_secret("ELASTIC_PASSWORD")
+    if ELASTIC_PASSWORD:
+        elastic_client_settings["basic_auth"] = ("elastic", ELASTIC_PASSWORD)
+    ca_path = "/opt/deploy/intel_owl/certs/elastic_ca/ca.crt"
+    cert_path = "/opt/deploy/intel_owl/certs/elastic_instance/elasticsearch.crt"
+    if "elasticsearch:9200" in ELASTIC_HOST:
+        # in case we use Elastic as container we need the generated
+        # in case we use Elastic as external service it should have a valid cert
+        elastic_client_settings["verify_certs"] = cert_path
+        elastic_client_settings["ca_certs"] = ca_path
+    ELASTICSEARCH_DSL = {"default": elastic_client_settings}
+
     ELASTICSEARCH_DSL_INDEX_SETTINGS = {
         "number_of_shards": int(secrets.get_secret("ELASTICSEARCH_DSL_NO_OF_SHARDS")),
         "number_of_replicas": int(

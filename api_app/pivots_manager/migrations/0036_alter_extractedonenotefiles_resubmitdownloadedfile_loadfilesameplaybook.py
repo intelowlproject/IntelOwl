8addae995b1d604dfd92788e86c73ea1bf5cdbc7
Federico Gibertoni <f.gibertoni@certego.net>
2024-11-07 17:07:04 +0100

    Renamed migration

--- api_app/pivots_manager/migrations/0036_alter_extractedonenotefiles_resubmitdownloadedfile_loadfilesameplaybook.py
+++ api_app/pivots_manager/migrations/0036_alter_extractedonenotefiles_resubmitdownloadedfile_loadfilesameplaybook.py
@@ -6,7 +6,9 @@ def migrate(apps, schema_editor):
     Parameter = apps.get_model("api_app", "Parameter")
     PluginConfig = apps.get_model("api_app", "PluginConfig")
     PivotConfig = apps.get_model("pivots_manager", "PivotConfig")
-    pivots_to_update = PivotConfig.objects.filter(name__in=["ExtractedOneNoteFiles", "ResubmitDownloadedFile"])
+    pivots_to_update = PivotConfig.objects.filter(
+        name__in=["ExtractedOneNoteFiles", "ResubmitDownloadedFile"]
+    )
     pm = PythonModule.objects.create(
         health_check_schedule=None,
         update_schedule=None,
@@ -14,30 +16,30 @@ def migrate(apps, schema_editor):
         base_path="api_app.pivots_manager.pivots",
     )
     param1 = Parameter.objects.create(
-          name="field_to_compare",
-          type="str",
-          description="Dotted path to the field",
-          is_secret=False,
-          required=True,
-          python_module=pm,
-     )
+        name="field_to_compare",
+        type="str",
+        description="Dotted path to the field",
+        is_secret=False,
+        required=True,
+        python_module=pm,
+    )
     for pivot_to_update in pivots_to_update:
 
-      PluginConfig.objects.filter(pivot_config=pivot_to_update).delete()
-      pivot_to_update.python_module = pm
-      PluginConfig.objects.create(
-          parameter=param1,
-          value="stored_base64",
-          for_organization=False,
-          updated_at="2024-11-07T10:35:46.217160Z",
-          analyzer_config=None,
-          connector_config=None,
-          visualizer_config=None,
-          ingestor_config=None,
-          pivot_config=pivot_to_update,
-      )
-      pivot_to_update.full_clean()
-      pivot_to_update.save()
+        PluginConfig.objects.filter(pivot_config=pivot_to_update).delete()
+        pivot_to_update.python_module = pm
+        PluginConfig.objects.create(
+            parameter=param1,
+            value="stored_base64",
+            for_organization=False,
+            updated_at="2024-11-07T10:35:46.217160Z",
+            analyzer_config=None,
+            connector_config=None,
+            visualizer_config=None,
+            ingestor_config=None,
+            pivot_config=pivot_to_update,
+        )
+        pivot_to_update.full_clean()
+        pivot_to_update.save()
 
 
 class Migration(migrations.Migration):

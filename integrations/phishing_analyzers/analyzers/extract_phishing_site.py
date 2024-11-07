c2f0f64d8ca0f6f325de18d9aaa338451af0b143
Federico Gibertoni <f.gibertoni@certego.net>
2024-11-07 09:48:46 +0100

    Page dump decoded to utf-8 string

--- integrations/phishing_analyzers/analyzers/extract_phishing_site.py
+++ integrations/phishing_analyzers/analyzers/extract_phishing_site.py
@@ -33,7 +33,7 @@ def extract_driver_result(driver_wrapper: DriverWrapper) -> dict:
     driver_result: {} = {
         "page_source": base64.b64encode(
             driver_wrapper.get_page_source().encode("utf-8")
-        ),
+        ).decode("utf-8"),
         "page_screenshot_base64": driver_wrapper.get_base64_screenshot(),
         "page_http_traffic": [
             dump_seleniumwire_requests(request)

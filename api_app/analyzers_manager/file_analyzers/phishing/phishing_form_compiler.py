6435bae1eda86a1267cb5b409322ff01b9dd4586
Federico Gibertoni <f.gibertoni@certego.net>
2024-12-11 16:55:00 +0100

    Added action extract method with relative url

--- api_app/analyzers_manager/file_analyzers/phishing/phishing_form_compiler.py
+++ api_app/analyzers_manager/file_analyzers/phishing/phishing_form_compiler.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import date, timedelta
 from typing import Dict
+from urllib.parse import urlparse
 
 import requests
 from faker import Faker
@@ -135,15 +136,30 @@ class PhishingFormCompiler(FileAnalyzer):
             if input_name in names:
                 return fake_value
 
-    def compile_form_field(self, form) -> (dict, str):
-        result: {} = {}
-        # setting default to page itself if action is not specified
+    def _extract_action_attribute(self, form) -> str:
         if not (form_action := form.get("action", None)):
             logger.info(
                 f"'action' attribute not found in form. Defaulting to {self.target_site=}"
             )
             form_action = self.target_site
+
+        # if relative url extracted, clean it from '/' and concatenate everything
+        # if action was not extracted in previous step the if should not pass as it is a url
+        if not urlparse(form_action).netloc:
+            logger.info(f"Found relative url in {form_action=}")
+            base_site = self.target_site
+            if base_site.endswith("/"):
+                base_site = base_site[:-1]
+            if form_action.startswith("/"):
+                form_action = form_action.replace("/", "", 1)
+
+            form_action = base_site + "/" + form_action
+
         logger.info(f"Extracted action to post data to: {form_action}")
+        return form_action
+
+    def compile_form_field(self, form) -> dict:
+        result: {} = {}
 
         for element in form.findall(".//input"):
             input_type: str = element.get("type", None)
@@ -174,10 +190,11 @@ class PhishingFormCompiler(FileAnalyzer):
                 f"Job #{self.job_id}: Sending value {value_to_set} for {input_name=}"
             )
             result.setdefault(input_name, value_to_set)
-        return result, form_action
+        return result
 
     def perform_request_to_form(self, form) -> Response:
-        params, dest_url = self.compile_form_field(form)
+        params = self.compile_form_field(form)
+        dest_url = self._extract_action_attribute(form)
         logger.info(f"Job #{self.job_id}: Sending {params=} to submit url {dest_url}")
         return requests.post(
             url=dest_url,

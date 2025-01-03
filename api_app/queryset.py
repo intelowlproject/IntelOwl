e30e49ef06b97926bd96aefe33e588d05accb29f
0ssigeno <s.berni@certego.net>
2025-01-03 11:26:40 +0100

    Retry insert if path already exists

--- api_app/queryset.py
+++ api_app/queryset.py
@@ -21,7 +21,7 @@ if TYPE_CHECKING:
 import logging
 
 from celery.canvas import Signature
-from django.db import models
+from django.db import IntegrityError, models
 from django.db.models import (
     BooleanField,
     Case,
@@ -280,7 +280,16 @@ class JobQuerySet(MP_NodeQuerySet, CleanOnCreateQuerySet, SendToBiQuerySet):
         """
         if parent:
             return parent.add_child(**kwargs)
-        return self.model.add_root(**kwargs)
+        try:
+            return self.model.add_root(**kwargs)
+        except IntegrityError as e:
+            if "path" in str(e):
+                logger.warning(
+                    f"Found race condition for {kwargs['name']}. Trying again to calculate path."
+                )
+                # we try again a second time, hoping for no race condition
+                return self.model.add_root(**kwargs)
+            raise
 
     def delete(self, *args, **kwargs):
         """

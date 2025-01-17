668feeb9591f014c8883a3700d7607103fa36619
srinitha709 <121370607+srinitha709@users.noreply.github.com>
2025-01-17 16:48:11 +0530

    Update _util.py

--- intel_owl/settings/_util.py
+++ intel_owl/settings/_util.py
@@ -13,6 +13,9 @@ logger = logging.getLogger(__name__)
 
 get_secret = os.environ.get
 
+uid = pwd.getpwnam("www-data").pw_uid
+gid = grp.getgrnam("www-data").gr_gid
+
 def set_permissions(directory: Path, force_create: bool = False):
     if not directory.exists():
         # this may happen in case we have added a new directory in the Dockerfile

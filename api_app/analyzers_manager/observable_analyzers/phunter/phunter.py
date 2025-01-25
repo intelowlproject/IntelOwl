56f1b1d4502aac9c2acdd3411f5d04e93182abf1
basedBaba <basedBaba@proton.me>
2025-01-25 22:07:07 +0530

    Phunter Analyzer, Closes #2286

---
+++ api_app/analyzers_manager/observable_analyzers/phunter/phunter.py
@@ -0,0 +1,62 @@
+# This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
+# See the file 'LICENSE' for copying permission.
+
+import logging
+
+from api_app.analyzers_manager.classes import ObservableAnalyzer
+from api_app.analyzers_manager.observable_analyzers.phunter.phunter_base import (
+    PhunterBase,
+)
+from tests.mock_utils import if_mock_connections, patch
+
+logger = logging.getLogger(__name__)
+
+
+class Phunter(ObservableAnalyzer):
+    """
+    This analyzer is a wrapper for the Phunter project.
+    """
+
+    @classmethod
+    def update(cls) -> bool:
+        pass
+
+    def run(self):
+        logger.info(f"Running Phunter Analyzer for {self.observable_name}")
+
+        phunter = PhunterBase()
+
+        results = phunter.phunt(phone_number=self.observable_name)
+
+        return results
+
+    @classmethod
+    def _monkeypatch(cls):
+        patches = [
+            if_mock_connections(
+                patch.object(
+                    Phunter,
+                    "phunt",
+                    return_value={
+                        "Valid": "true",
+                        "Operator": "Not found",
+                        "Possible": "true",
+                        "Line Type": "Not found",
+                        "Spamcalls": "true",
+                        "Free Lookup": {
+                            "Owner": "Not found",
+                            "Carrier": "Not found",
+                            "Country": "United States",
+                            "Location": "Not found",
+                            "National": "(833) 371-2570",
+                            "Line Type": "TOLL FREE",
+                            "Local Time": "02:43:29",
+                            "Views count": "34",
+                            "International": "+1 833-371-2570",
+                        },
+                        "Phone Number": "+18333712570",
+                    },
+                ),
+            )
+        ]
+        return super()._monkeypatch(patches=patches)
---
+++ api_app/analyzers_manager/observable_analyzers/phunter/phunter_base.py
@@ -0,0 +1,76 @@
+import phonenumbers
+import requests
+from bs4 import BeautifulSoup
+from phonenumbers import carrier
+
+
+class PhunterBase:
+    def __init__(self):
+        pass
+
+    def phunt(self, phone_number: str) -> dict:
+
+        # General Information
+
+        parsed = phonenumbers.parse(phone_number)
+
+        possible = phonenumbers.is_possible_number(parsed)
+
+        valid = phonenumbers.is_valid_number(parsed)
+
+        operator = carrier.name_for_number(parsed, "en")
+        if operator != "":
+            operator = operator
+        else:
+            operator = "Not found"
+
+        line = phonenumbers.number_type(parsed)
+        if line == phonenumbers.PhoneNumberType.FIXED_LINE:
+            ligne = "Fixed"
+        elif line == phonenumbers.PhoneNumberType.MOBILE:
+            ligne = "Mobile"
+        else:
+            ligne = "Not found"
+
+        # Free Lookup
+
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0"
+
+        free_lookup_url = f"https://free-lookup.net/{phone_number.replace('+', '')}"
+
+        r = requests.get(free_lookup_url, headers={"user-agent": user_agent})
+
+        html_body = BeautifulSoup(r.text, "html.parser")
+        list_info = html_body.findChild("ul", class_="report-summary__list").findAll(
+            "div"
+        )
+
+        info_dict = {
+            k.text.strip(): info.text.strip() if info.text.strip() else "Not found"
+            for _, (k, info) in enumerate(zip(list_info[::2], list_info[1::2]))
+        }
+
+        # Spamcalls
+
+        spammer = False
+
+        spamcalls_url = f"https://spamcalls.net/en/number/{phone_number}"
+
+        r = requests.get(spamcalls_url, headers={"user-agent": user_agent})
+
+        if r.status_code == 200:
+            spammer = True
+        else:
+            spammer = False
+
+        result = {
+            "Phone Number": phone_number,
+            "Possible": possible,
+            "Valid": valid,
+            "Operator": operator,
+            "Line Type": ligne,
+            "Free Lookup": info_dict,
+            "Spamcalls": spammer,
+        }
+
+        return result

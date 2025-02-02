eed403c0944dc4a88755646071f7bbfb42e0b82e
pranjalg1331 <pranjaloff13@gmail.com>
2025-02-02 23:09:54 +0530

    new version for dockerfile
    Signed-off-by: pranjalg1331 <pranjaloff13@gmail.com>

--- integrations/nuclei_analyzer/entrypoint.sh
+++ integrations/nuclei_analyzer/entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-# Update Nuclei templates
-nuclei -update-template-dir /opt/nuclei-api/nuclei-templates -update-templates
-sleep 60
 TEMPLATES_DIR="/opt/nuclei-api/nuclei-templates"
 
-echo "Waiting for Nuclei templates to be available..."
+echo "Updating Nuclei templates..."
+nuclei -update-template-dir $TEMPLATES_DIR -update-templates
+
+echo "Verifying Nuclei templates..."
 while [ ! -d "$TEMPLATES_DIR" ] || [ -z "$(ls -A $TEMPLATES_DIR)" ]; do
-    echo "Templates not found, retrying in 10 seconds..."
+    echo "Templates not found or empty, retrying update in 10 seconds..."
+    nuclei -update-template-dir $TEMPLATES_DIR -update-templates
     sleep 10
 done
 
-echo "Templates downloaded successfully. Starting Flask app..."
-
-# Start the Flask app
+echo "Templates downloaded successfully. Starting Flask API..."
 exec gunicorn -b 0.0.0.0:4008 --timeout 120 --access-logfile - "app:app"

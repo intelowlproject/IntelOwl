0d89acb87f0b7e3ce9dc30963a2d0c23a68ceb5d
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2025-02-05 17:31:45 +0100

    fixed apivoid + pr template (#2714)

--- api_app/analyzers_manager/observable_analyzers/basic_observable_analyzer.py
+++ api_app/analyzers_manager/observable_analyzers/basic_observable_analyzer.py
@@ -1,3 +1,5 @@
+# This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
+# See the file 'LICENSE' for copying permission.
 import base64
 import logging
 from tempfile import NamedTemporaryFile

f15e002f4fc4264d129bd09425671173247b68d5
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2025-02-18 10:14:07 +0100

    die package is optional

--- api_app/analyzers_manager/file_analyzers/detectiteasy.py
+++ api_app/analyzers_manager/file_analyzers/detectiteasy.py
@@ -1,7 +1,10 @@
 import json
 import logging
 
-import die
+try:
+    import die
+except ImportError:
+    die = None
 
 from api_app.analyzers_manager.classes import FileAnalyzer
 from tests.mock_utils import MockUpResponse
@@ -17,11 +20,19 @@ class DetectItEasy(FileAnalyzer):
     def run(self):
         logger.info(f"Running DIE on {self.filepath} for {self.md5}")
 
-        json_report = die.scan_file(
-            self.filepath, die.ScanFlags.RESULT_AS_JSON, str(die.database_path / "db")
-        )
+        if die:
+            json_report = die.scan_file(
+                self.filepath,
+                die.ScanFlags.RESULT_AS_JSON,
+                str(die.database_path / "db"),
+            )
+            result = json.loads(json_report)
+        else:
+            message = "DIE package not imported because incompatible in ARM"
+            self.report.errors(message)
+            result = {"errors": message}
 
-        return json.loads(json_report)
+        return result
 
     @staticmethod
     def mocked_docker_analyzer_get(*args, **kwargs):

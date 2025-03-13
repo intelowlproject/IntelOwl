e03e633ededa4105bab66515352a0282037c3889
pranjalg1331 <pranjaloff13@gmail.com>
2025-03-13 10:28:50 +0530

    watchman script change
    Signed-off-by: pranjalg1331 <pranjaloff13@gmail.com>

--- docker/scripts/watchman_install.sh
+++ docker/scripts/watchman_install.sh
@@ -11,9 +11,9 @@ pip3 install --compile -r requirements/django-server-requirements.txt
 # install Watchman to enhance performance on the Django development Server
 # https://docs.djangoproject.com/en/3.2/ref/django-admin/#runserver
 cd /tmp || exit  
-wget https://github.com/facebook/watchman/releases/download/v2024.05.13.00/watchman-v2024.05.13.00-linux.zip
-unzip watchman-*-linux.zip
-cd watchman-*-linux/ || exit 
+wget https://github.com/facebook/watchman/archive/refs/tags/v2024.05.13.00.zip
+unzip v2024.05.13.00.zip*
+cd watchman-2024.05.13.00* || exit
 mkdir -p /usr/local/{bin,lib} /usr/local/var/run/watchman
 cp bin/* /usr/local/bin
 cp lib/* /usr/local/lib
@@ -21,4 +21,4 @@ chmod 755 /usr/local/bin/watchman
 chmod 2777 /usr/local/var/run/watchman
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
 dpkg -i libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-rm -rf watchman-*-linux*
+rm -rf v2024.05.13.00.zip*

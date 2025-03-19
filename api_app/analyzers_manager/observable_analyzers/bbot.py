8505eb08f0c55a0bc9ae9ff144b2661aeb1e4b01
AnshSinghal <potterheadansh@gmail.com>
2025-03-19 06:19:59 +0000

    fixed monkeypatch method

--- api_app/analyzers_manager/observable_analyzers/bbot.py
+++ api_app/analyzers_manager/observable_analyzers/bbot.py
@@ -64,7 +64,7 @@ class BBOT(ObservableAnalyzer, DockerBasedAnalyzer):
         pass
 
     @classmethod
-    def _monkeypatch(cls):
+    def _monkeypatch(cls, patches=None):
         """Mock BBOT's HTTP API response for testing."""
         mock_response = {
             "report": {
@@ -105,4 +105,4 @@ class BBOT(ObservableAnalyzer, DockerBasedAnalyzer):
                 patch("requests.post", return_value=MockUpResponse(mock_response, 200))
             )
         ]
-        return super()._monkeypatch(patches=patches)
+        return super(BBOT, cls)._monkeypatch(patches=patches)

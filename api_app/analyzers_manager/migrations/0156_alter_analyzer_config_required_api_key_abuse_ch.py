085f821cc549a149c146df173f6a99a70ea3e69a
Federico Gibertoni <f.gibertoni@certego.net>
2025-04-30 09:25:59 +0200

    Removed reverse migrate

--- api_app/analyzers_manager/migrations/0156_alter_analyzer_config_required_api_key_abuse_ch.py
+++ api_app/analyzers_manager/migrations/0156_alter_analyzer_config_required_api_key_abuse_ch.py
@@ -45,50 +45,6 @@ def migrate(apps, schema_editor):
     )
 
 
-def reverse_migrate(apps, schema_editor):
-    Parameter = apps.get_model("api_app", "Parameter")
-    PythonModule = apps.get_model("api_app", "PythonModule")
-
-    # observables
-    observable_analyzers = [
-        "urlhaus.URLHaus",
-        "yaraify.YARAify",
-        "feodo_tracker.Feodo_Tracker",
-        "threatfox.ThreatFox",
-        "mb_get.MB_GET",
-        "mb_google.MB_GOOGLE",
-    ]
-    for observable_analyzer in observable_analyzers:
-        module = PythonModule.objects.get(
-            module=observable_analyzer,
-            base_path="api_app.analyzers_manager.observable_analyzers",
-        )
-        Parameter.objects.filter(
-            name="service_api_key",
-            type="str",
-            is_secret=True,
-            python_module=module,
-        ).update(
-            description="Optional API key to connect to abuse.ch services.",
-            required=False,
-        )
-
-    # files
-    yaraify_scan_module = PythonModule.objects.get(
-        module="yaraify_file_scan.YARAifyFileScan",
-        base_path="api_app.analyzers_manager.file_analyzers",
-    )
-    Parameter.objects.filter(
-        name="service_api_key",
-        type="str",
-        is_secret=True,
-        python_module=yaraify_scan_module,
-    ).update(
-        description="Optional API key to connect to abuse.ch services.",
-        required=False,
-    )
-
-
 class Migration(migrations.Migration):
     atomic = False
     dependencies = [
@@ -99,4 +55,4 @@ class Migration(migrations.Migration):
         ),
     ]
 
-    operations = [migrations.RunPython(migrate, reverse_migrate)]
+    operations = [migrations.RunPython(migrate, migrations.RunPython.noop)]

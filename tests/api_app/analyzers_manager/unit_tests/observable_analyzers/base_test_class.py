2203d905b2323ead7bec9ef853c53065189f365f
pranjalg1331 <pranjaloff13@gmail.com>
2025-06-05 14:36:53 +0530

    making class method
    Signed-off-by: pranjalg1331 <pranjaloff13@gmail.com>

--- tests/api_app/analyzers_manager/unit_tests/observable_analyzers/base_test_class.py
+++ tests/api_app/analyzers_manager/unit_tests/observable_analyzers/base_test_class.py
@@ -8,9 +8,8 @@ from tests.api_app.analyzers_manager.unit_tests.analyzer_mocks import ANALYZER_P
 class BaseAnalyzerTest(TestCase):
     analyzer_class = None
     mock_patch_key = None
-    config = AnalyzerConfig.objects.get(python_module=analyzer_class.python_module)
 
-    @staticmethod
+    @classmethod
     def get_sample_observable(self, observable_type):
         mapping = {
             "domain": "example.com",
@@ -24,17 +23,21 @@ class BaseAnalyzerTest(TestCase):
         if self.analyzer_class is None or self.mock_patch_key is None:
             self.skipTest("analyzer_class or mock_patch_key is not set")
 
+        config = AnalyzerConfig.objects.get(
+            python_module=self.analyzer_class.python_module
+        )
+
         patch_context = ANALYZER_PATCHES.get(self.mock_patch_key)
         assert patch_context is not None, f"Patch for {self.mock_patch_key} not found"
 
         with patch_context():
-            for observable_type in self.config.observable_supported:
+            for observable_type in config.observable_supported:
                 if observable_type == "generic":
                     continue
                 with self.subTest(observable_type=observable_type):
                     print(f"Testing observable_type: {observable_type}")
                     observable_value = self.get_sample_observable(observable_type)
-                    analyzer = self.analyzer_class(self.config)
+                    analyzer = self.analyzer_class(config)
                     analyzer.observable_name = observable_value
                     analyzer.observable_classification = observable_type
                     try:

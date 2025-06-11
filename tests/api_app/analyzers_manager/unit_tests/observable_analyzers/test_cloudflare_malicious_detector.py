868db6462a522df3ecbe732191d21d0a98d932ab
pranjalg1331 <pranjaloff13@gmail.com>
2025-06-11 14:04:43 +0530

    mock location changed
    Signed-off-by: pranjalg1331 <pranjaloff13@gmail.com>

--- tests/api_app/analyzers_manager/unit_tests/observable_analyzers/test_cloudflare_malicious_detector.py
+++ tests/api_app/analyzers_manager/unit_tests/observable_analyzers/test_cloudflare_malicious_detector.py
@@ -1,12 +1,20 @@
+from unittest.mock import patch
+
 from api_app.analyzers_manager.observable_analyzers.dns.dns_malicious_detectors.cloudflare_malicious_detector import (
     CloudFlareMaliciousDetector,
 )
 from tests.api_app.analyzers_manager.unit_tests.observable_analyzers.base_test_class import (
     BaseAnalyzerTest,
 )
+from tests.mock_utils import MockUpResponse
 
 
 class CloudFlareMalicioudDetectorTestCase(BaseAnalyzerTest):
 
     analyzer_class = CloudFlareMaliciousDetector
-    mock_patch_key = "cloudflare_malicious_detector"
+
+    def get_mocked_response(self):
+        return patch(
+            "requests.get",
+            return_value=MockUpResponse({"Answer": [{"data": "0.0.0.0"}]}, 200),
+        )

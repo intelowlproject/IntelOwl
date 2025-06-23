e7f11ec93765dbac58ded2706b4a18e138603d35
Akshit Maheshwary <spoiicy@Akshits-Laptop.local>
2025-06-23 13:41:52 +0530

    added CommandSequenceAPI to greedybear analyzer

--- api_app/analyzers_manager/observable_analyzers/greedybear.py
+++ api_app/analyzers_manager/observable_analyzers/greedybear.py
@@ -1,31 +1,81 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
 # See the file 'LICENSE' for copying permission.
 
+import logging
+
 import requests
 
 from api_app.analyzers_manager.classes import ObservableAnalyzer
+from api_app.analyzers_manager.exceptions import AnalyzerRunException
+from api_app.choices import Classification
+from api_app.helpers import get_hash_type
 from tests.mock_utils import MockUpResponse, if_mock_connections, patch
+
+logger = logging.getLogger(__name__)
 
 
 class GreedyBear(ObservableAnalyzer):
     _api_key_name: str
     url: str
+    command_sequence_toggle: bool
+    same_cluster_commands: bool = False
+
+    @classmethod
+    def update(cls):
+        pass
 
     def run(self):
         headers = {
             "Authorization": "Token " + self._api_key_name,
             "Accept": "application/json",
         }
-        params_ = {
-            "query": self.observable_name,
-        }
-        uri = "/api/enrichment"
-        response = requests.get(self.url + uri, params=params_, headers=headers)
-        response.raise_for_status()
 
-        result = response.json()
+        params_ = {"query": self.observable_name, "include_similar": False}
 
-        return result
+        enrichment_uri = "/api/enrichment"
+        command_sequence_uri = "/api/command_sequence"
+
+        result = {}
+        try:
+            if get_hash_type(self.observable_name) == "sha-256":
+                params_["include_similar"] = True
+
+                command_sequence_response = requests.get(
+                    self.url + command_sequence_uri, params=params_, headers=headers
+                )
+                result = {"command_sequence_results": command_sequence_response.json()}
+
+            elif (
+                self.observable_classification == Classification.IP
+                or self.observable_classification == Classification.DOMAIN
+            ):
+                if self.command_sequence_toggle:
+                    if self.same_cluster_commands:
+                        params_["include_similar"] = True
+                    command_sequence_response = requests.get(
+                        self.url + command_sequence_uri, params=params_, headers=headers
+                    )
+                    result["command_sequence_results"] = (
+                        command_sequence_response.json()
+                    )
+
+                enrichment_response = requests.get(
+                    self.url + enrichment_uri, params=params_, headers=headers
+                )
+                result["enrichment_results"] = enrichment_response.json()
+
+            else:
+                raise ValueError("Invalid observale type. Supported are: sha256, IP")
+
+            return result
+
+        except ValueError as e:
+            logger.error(f"Error during GreedyBear analyzer run: {e}")
+            raise AnalyzerRunException(f"GreedyBear analyzer failed: {e}")
+
+        except Exception as e:
+            logger.error(f"Unexpected error during GreedyBear analyzer run: {e}")
+            raise AnalyzerRunException(f"Unexpected error: {e}")
 
     @classmethod
     def _monkeypatch(cls):

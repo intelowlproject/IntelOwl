59cb7b1efafbc1ec577ce84588e953731209151e
Federico Gibertoni <f.gibertoni@certego.net>
2025-06-25 18:12:15 +0200

    Adapted Quad9 DNS malicious resolver to DoH

--- api_app/analyzers_manager/observable_analyzers/dns/dns_malicious_detectors/quad9_malicious_detector.py
+++ api_app/analyzers_manager/observable_analyzers/dns/dns_malicious_detectors/quad9_malicious_detector.py
@@ -2,9 +2,12 @@
 # See the file 'LICENSE' for copying permission.
 
 """Check if the domains is reported as malicious in Quad9 database"""
-
+import base64
+import logging
 from urllib.parse import urlparse
 
+import dns.message
+import httpx
 import requests
 
 from api_app.analyzers_manager import classes
@@ -12,6 +15,8 @@ from api_app.choices import Classification
 from tests.mock_utils import MockUpResponse, if_mock_connections, patch
 
 from ..dns_responses import malicious_detector_response
+
+logger = logging.getLogger(__name__)
 
 
 class Quad9MaliciousDetector(classes.ObservableAnalyzer):
@@ -24,9 +29,24 @@ class Quad9MaliciousDetector(classes.ObservableAnalyzer):
     we can guess that the domain was in the Quad9 blacklist.
     """
 
-    headers: dict = {"Accept": "application/dns-json"}
-    url: str = "https://dns.quad9.net:5053/dns-query"
+    headers: dict = {"Accept": "application/dns-message"}
+    url: str = "https://dns.quad9.net/dns-query"
     google_url: str = "https://dns.google.com/resolve"
+
+    @staticmethod
+    def encode_query(observable: str) -> str:
+        """
+        Constructs a DNS query for the given observable (domain) for an A record,
+        converts it to wire format, and encodes it in URL-safe base64.
+        """
+        logger.info(f"Encoding DNS query for {observable}")
+        query = dns.message.make_query(observable, dns.rdatatype.A)
+        wire_query = query.to_wire()
+        encoded_query = (
+            base64.urlsafe_b64encode(wire_query).rstrip(b"=").decode("ascii")
+        )
+        logger.info(f"Quad9_DNS encoded query for {observable}: {encoded_query}")
+        return encoded_query
 
     def update(self) -> bool:
         pass
@@ -56,18 +76,20 @@ class Quad9MaliciousDetector(classes.ObservableAnalyzer):
         :param observable: domain to resolve
         :type observable: str
         """
-        params = {"name": observable}
+        encoded_query = self.encode_query(observable)
+        complete_url = f"{self.url}?dns={encoded_query}"
 
         # sometimes it can respond with 503, I suppose to avoid DoS.
         # In 1k requests just 20 fails and at least with 30 requests between 2 failures
         # with 2 or 3 attemps the analyzer should get the data
         attempt_number = 3
+        quad9_response = None
         for attempt in range(0, attempt_number):
             try:
-                quad9_response = requests.get(
-                    self.url, headers=self.headers, params=params, timeout=10
+                quad9_response = httpx.Client(http2=True).get(
+                    complete_url, headers=self.headers, timeout=10
                 )
-            except requests.exceptions.ConnectionError as exception:
+            except httpx.ConnectionError as exception:
                 # if the last attempt fails, raise an error
                 if attempt == attempt_number - 1:
                     raise exception
@@ -75,7 +97,12 @@ class Quad9MaliciousDetector(classes.ObservableAnalyzer):
                 quad9_response.raise_for_status()
                 break
 
-        return bool(quad9_response.json().get("Answer", None))
+        dns_response = dns.message.from_wire(quad9_response.content)
+        resolutions: list[str] = []
+        for answer in dns_response.answer:
+            resolutions.extend([resolution.address for resolution in answer])
+
+        return bool(resolutions)
 
     def _google_dns_query(self, observable) -> bool:
         """Perform a DNS query with Google service, return True if Google answer the

92f91ae7ffb40644b4b37697918bb98f0b8ab2da
Akshit Maheshwary <spoiicy@Akshits-Laptop.local>
2025-07-18 18:34:48 +0530

    fixed migration issues

--- api_app/analyzers_manager/migrations/0162_analyzer_config_guarddoggeneric.py
+++ api_app/analyzers_manager/migrations/0162_analyzer_config_guarddoggeneric.py
@@ -131,7 +131,7 @@ class Migration(migrations.Migration):
     atomic = False
     dependencies = [
         ("api_app", "0071_delete_last_elastic_report"),
-        ("analyzers_manager", "0160_update_greedybear"),
+        ("analyzers_manager", "0161_analyzer_config_guarddogfile"),
     ]
 
     operations = [migrations.RunPython(migrate, reverse_migrate)]

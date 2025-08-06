3fed64315c4517e5a41d36809b7060d1a0c996d1
pranjalg1331 <pranjaloff13@gmail.com>
2025-08-06 17:39:02 +0530

    modify class to remove mock response in docker analyzers
    Signed-off-by: pranjalg1331 <pranjaloff13@gmail.com>

--- api_app/analyzers_manager/classes.py
+++ api_app/analyzers_manager/classes.py
@@ -12,7 +12,6 @@ import requests
 from django.conf import settings
 
 from certego_saas.apps.user.models import User
-from tests.mock_utils import MockUpResponse
 
 from ..choices import Classification, PythonModuleBasePaths
 from ..classes import Plugin
@@ -486,43 +485,6 @@ class DockerBasedAnalyzer(BaseAnalyzerMixin, metaclass=ABCMeta):
         if not self.__raise_in_case_bad_request(self.name, resp, params_to_check=[]):
             raise AssertionError
         return resp
-
-    @staticmethod
-    def mocked_docker_analyzer_get(*args, **kwargs):
-        return MockUpResponse(
-            {"key": "test", "returncode": 0, "report": {"test": "This is a test."}}, 200
-        )
-
-    @staticmethod
-    def mocked_docker_analyzer_post(*args, **kwargs):
-        return MockUpResponse({"key": "test", "status": "running"}, 202)
-
-    # def _monkeypatch(self, patches: list = None):
-    #     """
-    #     Here, `_monkeypatch` is an instance method and not a class method.
-    #     This is because when defined with `@classmethod`, we were getting the error
-    #     ```
-    #     '_patch' object has no attribute 'is_local'
-    #     ```
-    #     whenever multiple analyzers with same parent class were being called.
-    #     """
-    #     if patches is None:
-    #         patches = []
-    #     # no need to sleep during tests
-    #     self.poll_distance = 0
-    #     patches.append(
-    #         if_mock_connections(
-    #             patch(
-    #                 "requests.get",
-    #                 side_effect=self.mocked_docker_analyzer_get,
-    #             ),
-    #             patch(
-    #                 "requests.post",
-    #                 side_effect=self.mocked_docker_analyzer_post,
-    #             ),
-    #         )
-    #     )
-    #     return super()._monkeypatch(patches)
 
     def health_check(self, user: User = None) -> bool:
         """

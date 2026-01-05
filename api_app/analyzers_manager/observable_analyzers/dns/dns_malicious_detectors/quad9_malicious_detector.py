9a811adbd751cc91b06b4fa33fca199240c00ca3
ManaswibRane <manaswirane1947@gmail.com>
2026-01-06 00:15:59 +0530

    Fix Quad9MaliciousDetector crash on CNAME records

--- api_app/analyzers_manager/observable_analyzers/dns/dns_malicious_detectors/quad9_malicious_detector.py
+++ api_app/analyzers_manager/observable_analyzers/dns/dns_malicious_detectors/quad9_malicious_detector.py
@@ -80,7 +80,11 @@ class Quad9MaliciousDetector(DoHMixin, classes.ObservableAnalyzer):
         dns_response = dns.message.from_wire(quad9_response.content)
         resolutions: list[str] = []
         for answer in dns_response.answer:
-            resolutions.extend([resolution.address for resolution in answer])
+            for record in answer:
+             if hasattr(record, "address"):  
+              resolutions.append(record.address)
+             elif hasattr(record, "target"): 
+              resolutions.append(record.target)
 
         return bool(resolutions)
 

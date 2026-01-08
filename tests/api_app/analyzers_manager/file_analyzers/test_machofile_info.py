37c84f8066cafd9fc76510b4511ea4057920eac2
Yasvi Tank <yasvitank@gmail.com>
2026-01-08 12:08:43 +0300

    Add MachoFile analyzer to FREE_TO_USE_ANALYZERS

--- tests/api_app/analyzers_manager/file_analyzers/test_machofile_info.py
+++ tests/api_app/analyzers_manager/file_analyzers/test_machofile_info.py
@@ -1,3 +1,7 @@
+# This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
+# See the file 'LICENSE' for copying permission.
+
+
 from api_app.analyzers_manager.file_analyzers.machofile_info import MachoFileInfo
 from tests.api_app.analyzers_manager.file_analyzers.base_test_class import (
     BaseFileAnalyzerTest,

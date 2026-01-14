8fcd33a95c469b52673779055cd581b46d7ee971
Jay-Dosi <jayofficial158@gmail.com>
2026-01-14 16:19:40 +0530

    removed the comment

--- api_app/analyzers_manager/repo_downloader.sh
+++ api_app/analyzers_manager/repo_downloader.sh
@@ -19,9 +19,6 @@ chown -R www-data:www-data ~/.quark-engine
 # Clone dictionaries for dnstwist analyzer
 cd /opt/deploy || exit
 
-#changing this url
-#svn export https://github.com/elceef/dnstwist/tags/20230402/dictionaries dnstwist-dictionaries
-#fix: Download DNSTwist dictionaries from upstream repository
 DNSTWIST_RAW_BASE="https://raw.githubusercontent.com/elceef/dnstwist/master/dictionaries"
 
 mkdir -p dnstwist-dictionaries

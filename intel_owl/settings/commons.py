bff38e58216c9e1a51642dbf4514f1c1ea804c37
Lavanya Bhargava <lavanyabhargava05@gmail.com>
2026-01-31 00:40:12 +0530

    Apply Black formatting after rebase

--- intel_owl/settings/commons.py
+++ intel_owl/settings/commons.py
@@ -41,7 +41,9 @@ BASE_ANALYZER_OBSERVABLE_PYTHON_PATH = PosixPath(
     )
 )
 BASE_ANALYZER_FILE_PYTHON_PATH = PosixPath(
-    get_secret("BASE_ANALYZER_FILE_PYTHON_PATH", "api_app.analyzers_manager.file_analyzers")
+    get_secret(
+        "BASE_ANALYZER_FILE_PYTHON_PATH", "api_app.analyzers_manager.file_analyzers"
+    )
 )
 BASE_ENGINE_MODULES_PYTHON_PATH = PosixPath(
     get_secret("BASE_ENGINE_MODULES_PYTHON_PATH", "api_app.engines_manager.engines")

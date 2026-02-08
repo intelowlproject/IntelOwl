75d18fc838800221e9b1c5b486c26e9e0bfeb96c
Rambatla Venkat Rao <68921481+RamboV@users.noreply.github.com>
2026-02-09 03:35:30 +0530

    Refactor IPQualityScore file scanning logic

--- api_app/analyzers_manager/file_analyzers/ipqsfile.py
+++ api_app/analyzers_manager/file_analyzers/ipqsfile.py
@@ -14,34 +14,34 @@ class IPQSFileScan(FileAnalyzer, IPQualityScoreMixin):
     Scan a binary file using IPQualityScore malware detection service.
     """
 
-    scan_url = "/malware/scan/"
-    lookup_url = "/malware/lookup/"
-    postback_url = "/postback/"
+    @classmethod
+    def update(cls):
+        pass
 
     def run(self):
-        try:
-            binary = self.read_file_bytes()
-            files = {"files": (self.filename, binary)}
-
-            # lookup endpoint check for cached result
-            lookup_result = self._make_request(
-                self.lookup_url, method="POST", _api_key=self._ipqs_api_key, files=files
-            )
-            if lookup_result.get("status", False) == "cached":
-                lookup_result.pop("update_url", None)
-                return lookup_result
-            # sending file to ipqs for scan
-            scan_result = self._make_request(
-                self.scan_url, method="POST", _api_key=self._ipqs_api_key, files=files
-            )
-            # waiting for scan result with help of request id
-            result = self._poll_for_report(
-                endpoint=self.postback_url,
-                _api_key=self._ipqs_api_key,
-                request_id=scan_result.get("request_id"),
-            )
-            result.pop("update_url", None)
-            return result
-        except Exception as e:
-            logger.error(f"Error occurred while scanning file with IPQS: {e}")
-            raise
+        binary = self.read_file_bytes()
+        files = {"files": (self.filename, binary)}
+        # lookup endpoint check for cached result
+        lookup_result = self._make_request(
+            self.lookup_endpoint,
+            method="POST",
+            _api_key=self._ipqs_api_key,
+            files=files,
+        )
+        if lookup_result.get("status", False) == "cached":
+            lookup_result.pop("update_url", None)
+            return lookup_result
+        # sending file to ipqs for scan
+        scan_result = self._make_request(
+            self.scan_endpoint, method="POST",
+            _api_key=self._ipqs_api_key,
+            files=files
+        )
+        # waiting for scan result with help of request id
+        result = self._poll_for_report(
+            endpoint=self.postback_endpoint,
+            _api_key=self._ipqs_api_key,
+            request_id=scan_result.get("request_id"),
+        )
+        result.pop("update_url", None)
+        return result

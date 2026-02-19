47ff07b3ad5764f5d84ce9d98f5692695d870651
AnshSinghal <potterheadansh@gmail.com>
2026-02-19 03:08:39 +0000

    optmized decay method

--- api_app/user_events_manager/queryset.py
+++ api_app/user_events_manager/queryset.py
@@ -11,7 +11,9 @@ from api_app.user_events_manager.choices import DecayProgressionEnum
 
 class UserEventQuerySet(QuerySet):
     def decay(self):
-        from api_app.user_events_manager.models import UserEvent
+        from collections import defaultdict
+
+        from django.db import transaction
 
         objects = (
             self.exclude(decay_progression=DecayProgressionEnum.FIXED.value)
@@ -20,12 +22,26 @@ class UserEventQuerySet(QuerySet):
                 next_decay__lte=now(),
             )
         )
-        # TODO we can probably translate all of this in sql query
-        for obj in objects:
-            obj: UserEvent
+        model_fields = {field.name for field in self.model._meta.fields}
+        if "data_model" in model_fields:
+            objects = objects.select_related("data_model")
+        else:
+            objects = objects.prefetch_related("data_model")
+
+        events = list(objects)
+        if not events:
+            return 0
+
+        data_models_by_class = defaultdict(list)
+
+        for obj in events:
             obj.decay_times += 1
-            obj.data_model.reliability -= 1
-            if obj.data_model.reliability == 0:
+            data_model = obj.data_model
+
+            if data_model is not None:
+                data_model.reliability -= 1
+
+            if data_model is None or data_model.reliability == 0:
                 obj.next_decay = None
             else:
                 if obj.decay_progression == DecayProgressionEnum.LINEAR.value:
@@ -34,9 +50,16 @@ class UserEventQuerySet(QuerySet):
                     obj.next_decay += datetime.timedelta(
                         days=obj.decay_timedelta_days ** (obj.decay_times + 1)
                     )
-            obj.data_model.save()
-            obj.save()
-        return objects.count()
+
+            if data_model is not None:
+                data_models_by_class[data_model.__class__].append(data_model)
+
+        with transaction.atomic():
+            for model_class, models_list in data_models_by_class.items():
+                model_class.objects.bulk_update(models_list, ["reliability"])
+            self.model.objects.bulk_update(events, ["decay_times", "next_decay"])
+
+        return len(events)
 
     def visible_for_user(self, user):
         if user.has_membership():

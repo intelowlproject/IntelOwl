8636c185f95d0e273480498dd86df41904d6cb56
AdityaGupta716 <itzaditya.gupta0716@gmail.com>
2026-02-20 16:43:21 +0530

    Update queryset.py

--- api_app/user_events_manager/queryset.py
+++ api_app/user_events_manager/queryset.py
@@ -1,6 +1,9 @@
 import datetime
+from collections import defaultdict
 
-from django.db.models import F, Q, QuerySet, Value
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Case, DurationField, ExpressionWrapper, F, Q, QuerySet, Value, When
+from django.db.models.functions import Power
 from django.db.models.lookups import IRegex, Range
 from django.utils.timezone import now
 
@@ -11,39 +14,70 @@ from api_app.user_events_manager.choices import DecayProgressionEnum
 
 class UserEventQuerySet(QuerySet):
     def decay(self):
-        from api_app.user_events_manager.models import UserEvent
-
         objects = (
             self.exclude(decay_progression=DecayProgressionEnum.FIXED.value)
             .exclude(next_decay__isnull=True)
-            .filter(
-                next_decay__lte=now(),
-            )
+            .filter(next_decay__lte=now())
         )
-        # TODO we can probably translate all of this in sql query
-        for obj in objects:
-            obj: UserEvent
-            obj.decay_times += 1
-            obj.data_model.reliability -= 1
-            if obj.data_model.reliability == 0:
-                obj.next_decay = None
-            else:
-                if obj.decay_progression == DecayProgressionEnum.LINEAR.value:
-                    obj.next_decay += datetime.timedelta(days=obj.decay_timedelta_days)
-                elif obj.decay_progression == DecayProgressionEnum.INVERSE_EXPONENTIAL.value:
-                    obj.next_decay += datetime.timedelta(
-                        days=obj.decay_timedelta_days ** (obj.decay_times + 1)
-                    )
-            obj.data_model.save()
-            obj.save()
-        return objects.count()
+
+        count = objects.count()
+        if not count:
+            return 0
+
+        # Step 1: Bulk update decay_times and next_decay on UserEvent
+        objects.update(
+            decay_times=F("decay_times") + 1,
+            next_decay=Case(
+                When(
+                    data_model__reliability=1,
+                    then=None,
+                ),
+                When(
+                    decay_progression=DecayProgressionEnum.LINEAR.value,
+                    then=F("next_decay") + ExpressionWrapper(
+                        F("decay_timedelta_days") * datetime.timedelta(days=1),
+                        output_field=DurationField(),
+                    ),
+                ),
+                When(
+                    decay_progression=DecayProgressionEnum.INVERSE_EXPONENTIAL.value,
+                    then=F("next_decay") + ExpressionWrapper(
+                        Power(
+                            F("decay_timedelta_days"),
+                            F("decay_times") + 1,
+                        ) * datetime.timedelta(days=1),
+                        output_field=DurationField(),
+                    ),
+                ),
+                default=None,
+            ),
+        )
+
+        # Step 2: Bulk update reliability on data_model
+        # Group by content_type to handle GenericForeignKey correctly
+        content_type_ids = defaultdict(list)
+        for obj in objects.values("data_model_content_type_id", "data_model_object_id"):
+            content_type_ids[obj["data_model_content_type_id"]].append(
+                obj["data_model_object_id"]
+            )
+
+        for ct_id, obj_ids in content_type_ids.items():
+            ct = ContentType.objects.get_for_id(ct_id)
+            model = ct.model_class()
+            model.objects.filter(
+                pk__in=obj_ids,
+                reliability__gt=0,
+            ).update(reliability=F("reliability") - 1)
+
+        return count
 
     def visible_for_user(self, user):
         if user.has_membership():
-            user_query = Q(user=user) | Q(user__membership__organization_id=user.membership.organization_id)
+            user_query = Q(user=user) | Q(
+                user__membership__organization_id=user.membership.organization_id
+            )
         else:
             user_query = Q(user=user)
-
         return self.filter(user_query)
 
     def create(self, **kwargs):
@@ -61,7 +95,9 @@ class UserDomainWildCardEventQuerySet(UserEventQuerySet):
             Classification.DOMAIN.value,
             Classification.URL.value,
         ]:
-            return self.annotate(matches=IRegex(Value(analyzable.name), F("query"))).filter(matches=True)
+            return self.annotate(
+                matches=IRegex(Value(analyzable.name), F("query"))
+            ).filter(matches=True)
         return self.none()
 
     def create(self, **kwargs):
@@ -73,9 +109,9 @@ class UserDomainWildCardEventQuerySet(UserEventQuerySet):
 class UserIPWildCardEventQuerySet(UserEventQuerySet):
     def matches(self, analyzable: Analyzable) -> "UserIPWildCardEventQuerySet":
         if analyzable.classification == Classification.IP.value:
-            return self.annotate(matches=Range(Value(analyzable.name), (F("start_ip"), F("end_ip")))).filter(
-                matches=True
-            )
+            return self.annotate(
+                matches=Range(Value(analyzable.name), (F("start_ip"), F("end_ip")))
+            ).filter(matches=True)
         return self.none()
 
     def create(self, **kwargs):

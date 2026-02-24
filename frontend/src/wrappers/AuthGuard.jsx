2e7a0ddbcf9e2f541f28db51f591c3c8260d3ef7
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2026-02-25 01:18:20 +0530

    fix/prettier-failure

--- frontend/src/wrappers/AuthGuard.jsx
+++ frontend/src/wrappers/AuthGuard.jsx
@@ -33,8 +33,8 @@ export default function AuthGuard({ children }) {
     } else {
       setInitialCheckDone(true);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     // intentionally run only once on mount to restore session from cookie
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // side effects

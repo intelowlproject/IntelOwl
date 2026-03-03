20fbb89705269c0959c1859d9222e649cadec898
rootp1 <arnav.iitr@email.com>
2026-03-03 20:35:06 +0530

    watchman to watchfiles

--- intel_owl/settings/__init__.py
+++ intel_owl/settings/__init__.py
@@ -58,6 +58,11 @@ INSTALLED_APPS = [
     "django_extensions",
 ]
 
+from .commons import DEBUG  # noqa: E402
+
+if DEBUG:
+    INSTALLED_APPS.append("django_watchfiles")
+
 from .a_secrets import *  # lgtm [py/polluting-import]
 from .auth import *  # lgtm [py/polluting-import]
 from .aws import *  # lgtm [py/polluting-import]

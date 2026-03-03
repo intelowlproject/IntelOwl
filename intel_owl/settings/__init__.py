c4f37c28306491de2648596f1b60dc4ffed097e1
rootp1 <arnav.iitr@email.com>
2026-03-03 20:47:24 +0530

    init py

--- intel_owl/settings/__init__.py
+++ intel_owl/settings/__init__.py
@@ -63,7 +63,7 @@ from .commons import DEBUG  # noqa: E402
 if DEBUG:
     INSTALLED_APPS.append("django_watchfiles")
 
-from .a_secRrets import *  # lgtm [py/polluting-import]
+from .a_secrets import *  # lgtm [py/polluting-import]
 from .auth import *  # lgtm [py/polluting-import]
 from .aws import *  # lgtm [py/polluting-import]
 from .cache import *  # lgtm [py/polluting-import]

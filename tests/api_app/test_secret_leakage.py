70d17c3f7426f55205a355449e0e84e24efd56a7
chauhan-varun <varunchauhan097@gmail.com>
2026-03-15 16:42:16 +0530

    style: Condense multi-line dictionaries and lists in secret leakage tests.

--- tests/api_app/test_secret_leakage.py
+++ tests/api_app/test_secret_leakage.py
@@ -15,19 +15,13 @@ class SecretLeakageTests(TestCase):
             "api_key": "secret_key",
             "username": "user1",
             "password": "secret_password",
-            "nested": {
-                "token": "secret_token",
-                "normal": "value"
-            }
+            "nested": {"token": "secret_token", "normal": "value"},
         }
         expected = {
             "api_key": "<redacted>",
             "username": "user1",
             "password": "<redacted>",
-            "nested": {
-                "token": "<redacted>",
-                "normal": "value"
-            }
+            "nested": {"token": "<redacted>", "normal": "value"},
         }
         self.assertEqual(mask_recursive(data), expected)
 
@@ -36,34 +30,22 @@ class SecretLeakageTests(TestCase):
             "_api_key": "secret",
             "my_password_field": "secret",
             "authentication_token": "secret",
-            "normal_field": "safe"
+            "normal_field": "safe",
         }
         expected = {
             "_api_key": "<redacted>",
             "my_password_field": "<redacted>",
             "authentication_token": "<redacted>",
-            "normal_field": "safe"
+            "normal_field": "safe",
         }
         self.assertEqual(mask_recursive(data), expected)
 
     def test_mask_recursive_list(self):
-        data = [
-            {"key": "secret"},
-            {"other": "safe"}
-        ]
-        expected = [
-            {"key": "<redacted>"},
-            {"other": "safe"}
-        ]
+        data = [{"key": "secret"}, {"other": "safe"}]
+        expected = [{"key": "<redacted>"}, {"other": "safe"}]
         self.assertEqual(mask_recursive(data), expected)
 
     def test_mask_recursive_case_insensitive(self):
-        data = {
-            "API_KEY": "secret",
-            "Password": "secret"
-        }
-        expected = {
-            "API_KEY": "<redacted>",
-            "Password": "<redacted>"
-        }
+        data = {"API_KEY": "secret", "Password": "secret"}
+        expected = {"API_KEY": "<redacted>", "Password": "<redacted>"}
         self.assertEqual(mask_recursive(data), expected)

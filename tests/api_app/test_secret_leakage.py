d927454a20b23f98f8e2fbb36c7d0bde7bba59b7
chauhan-varun <varunchauhan097@gmail.com>
2026-03-15 16:34:54 +0530

    chore: Add license header to `test_secret_leakage.py`.

--- tests/api_app/test_secret_leakage.py
+++ tests/api_app/test_secret_leakage.py
@@ -1,3 +1,5 @@
+# This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
+# See the file 'LICENSE' for copying permission.
 from django.test import TestCase
 from api_app.helpers import mask_recursive, mask_sensitive_data
 

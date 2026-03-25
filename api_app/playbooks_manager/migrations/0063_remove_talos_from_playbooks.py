d0f6e99fa0f9447dfd57bd1e7617f5ae4c2ce7d5
Adekilekun Fatiu <fatwadek00@gmail.com>
2026-03-25 13:14:19 +0100

    Remove TalosReputation from playbook configurations

---
+++ api_app/playbooks_manager/migrations/0063_remove_talos_from_playbooks.py
@@ -0,0 +1,40 @@
+# This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
+# See the file 'LICENSE' for copying permission.
+
+
+from django.db import migrations
+
+
+def remove_talos_from_playbooks(apps, schema_editor):
+    PlaybookConfig = apps.get_model("playbooks_manager", "PlaybookConfig")
+    AnalyzerConfig = apps.get_model("analyzers_manager", "AnalyzerConfig")
+
+    try:
+        talos_analyzer = AnalyzerConfig.objects.get(name="TalosReputation")
+    except AnalyzerConfig.DoesNotExist:
+        talos_analyzer = None
+
+    if talos_analyzer is not None:
+        for playbook in PlaybookConfig.objects.filter(analyzers=talos_analyzer):
+            playbook.analyzers.remove(talos_analyzer)
+
+    # Also scrub runtime_configuration if present
+    for playbook in PlaybookConfig.objects.all():
+        rc = playbook.runtime_configuration
+        if isinstance(rc, dict) and "analyzers" in rc and "TalosReputation" in rc["analyzers"]:
+            del rc["analyzers"]["TalosReputation"]
+            playbook.runtime_configuration = rc
+            playbook.save(update_fields=["runtime_configuration"])
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("playbooks_manager", "0062_add_cleanbrowsing_to_free_to_use"),
+        ("analyzers_manager", "0175_analyzer_config_cleanbrowsing_malicious_detector"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            remove_talos_from_playbooks, reverse_code=migrations.RunPython.noop
+        ),
+    ]

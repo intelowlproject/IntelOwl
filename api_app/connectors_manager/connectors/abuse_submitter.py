5b5414e9bb0181c7d2121f665a70026acb45d0d6
chauhan-varun <varunchauhan097@gmail.com>
2026-03-27 09:58:44 +0530

    fixed the AttributeError and incorrect exception handling in the AbuseSubmitter connector

--- api_app/connectors_manager/connectors/abuse_submitter.py
+++ api_app/connectors_manager/connectors/abuse_submitter.py
@@ -1,19 +1,23 @@
-from api_app.analyzers_manager.exceptions import AnalyzerRunException
 from api_app.connectors_manager.connectors.email_sender import EmailSender
+from api_app.connectors_manager.exceptions import ConnectorRunException
 
 
 class AbuseSubmitter(EmailSender):
+    def _validate_job_hierarchy(self):
+        if not (self._job.parent_job and self._job.parent_job.parent_job):
+            raise ConnectorRunException(
+                "Job hierarchy is invalid. This connector requires a 'Job -> Parent -> Grandparent' structure. "
+                "Ensure it's run within the 'Takedown_Request' playbook."
+            )
+
     @property
     def subject(self) -> str:
+        self._validate_job_hierarchy()
         return f"Takedown domain request for {self._job.parent_job.parent_job.analyzable.name}"
 
     @property
     def body(self) -> str:
-        if not self._job.parent_job:
-            raise AnalyzerRunException(
-                "Parent job does not exist. "
-                "This analyzer must be run only with the playbook Takedown_Request to work properly"
-            )
+        self._validate_job_hierarchy()
         return (
             f"Domain {self._job.parent_job.parent_job.analyzable.name} "
             "has been detected as malicious by our team. We kindly request you to take "

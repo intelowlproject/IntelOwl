06ba5d4c168746679a6daefc4571f71f7ba01bf8
Rambatla Venkat Rao <68921481+RamboV@users.noreply.github.com>
2026-04-09 11:55:58 +0530

    Refactor import statement in test_ipqs.py
    Reformatted import statement for better readability.

--- tests/api_app/analyzers_manager/unit_tests/observable_analyzers/test_ipqs.py
+++ tests/api_app/analyzers_manager/unit_tests/observable_analyzers/test_ipqs.py
@@ -3,8 +3,9 @@
 from unittest.mock import patch
 
 from api_app.analyzers_manager.observable_analyzers import ipqs
-from tests.api_app.analyzers_manager.unit_tests.observable_analyzers import \
-    base_test_class
+from tests.api_app.analyzers_manager.unit_tests.observable_analyzers import (
+    base_test_class,
+)
 from tests.mock_utils import MockUpResponse
 
 

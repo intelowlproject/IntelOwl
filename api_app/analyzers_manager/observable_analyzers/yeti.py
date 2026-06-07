1277b1136d5f7bc6281d175a1750f373bdf18e46
Sanjib Behera <178703817+sanjib2006@users.noreply.github.com>
2026-06-07 15:42:33 +0530

    Refactor YETI analyzer to implement API v2 auth flow

--- api_app/analyzers_manager/observable_analyzers/yeti.py
+++ api_app/analyzers_manager/observable_analyzers/yeti.py
@@ -4,6 +4,7 @@
 import requests
 
 from api_app.analyzers_manager import classes
+from api_app.analyzers_manager.exceptions import AnalyzerRunException
 
 
 class YETI(classes.ObservableAnalyzer):
@@ -19,10 +20,30 @@ class YETI(classes.ObservableAnalyzer):
             "query": {"value": self._job.analyzable.name},
             "count": self.results_count,
         }
-        headers = {"Accept": "application/json", "X-Api-Key": self._api_key_name}
+
+        # auth
+        auth_url = f"{self._url_key_name}/api/v2/auth/api-token"
+        auth_headers = {"x-yeti-apikey": self._api_key_name, "User-Agent": "IntelOwl"}
+
+        try:
+            auth_resp = requests.post(
+                url=auth_url,
+                headers=auth_headers,
+                verify=self.verify_ssl,
+                timeout=60,
+            )
+            auth_resp.raise_for_status()
+            access_token = auth_resp.json().get("access_token")
+
+            if not access_token:
+                raise AnalyzerRunException("Failed to obtain access token from YETI.")
+        except requests.RequestException as e:
+            raise AnalyzerRunException(f"YETI Auth Request failed: {e}")
+
+        headers = {"Accept": "application/json", "Authorization": f"Bearer {access_token}"}
         if self._url_key_name and self._url_key_name.endswith("/"):
             self._url_key_name = self._url_key_name[:-1]
-        url = f"{self._url_key_name}/api/v2/observables/search/"
+        url = f"{self._url_key_name}/api/v2/observables/search"
 
         # search for observables
         resp = requests.post(
